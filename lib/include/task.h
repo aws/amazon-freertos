453f965b7fc84cdb3600f1e55422d21bf10918be
jsrmalvarez <jsrmalvarez@ninjatic.engineer>
2018-11-29 18:43:08 +0100

    Conditional compilation missing.

--- lib/include/task.h
+++ lib/include/task.h
@@ -2132,10 +2132,11 @@ void vTaskNotifyGiveFromISR( TaskHandle_t xTaskToNotify, BaseType_t *pxHigherPri
  */
 uint32_t ulTaskNotifyTake( BaseType_t xClearCountOnExit, TickType_t xTicksToWait ) PRIVILEGED_FUNCTION;
 
+#if( configUSE_TASK_NOTIFICATION_MESSAGES == 1 )
 void const* pvTaskNotifyWaitMessage(size_t* xNotifiedMessageSize, TickType_t xTicksToWait );
 void vTaskNotifySendMessage( TaskHandle_t xTaskToNotify, void const* pvNotifiedMessage, size_t xNotifiedMessageSize);
 void vTaskNotifySendMessageFromISR( TaskHandle_t xTaskToNotify, void const* pvNotifiedMessage, size_t xNotifiedMessageSize, BaseType_t *pxHigherPriorityTaskWoken );
-
+#endif
 
 /**
  * task. h
