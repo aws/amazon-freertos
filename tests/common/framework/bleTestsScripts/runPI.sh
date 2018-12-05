6078b2d102fd433d3252cf57747d1e0dec165679
hbouvier <33438817+huguesBouvier@users.noreply.github.com>
2018-12-04 18:32:27 -0800

    Improvement: Adding stop advertisement test to test framework
    Adding stop advertisement test to test framework

--- tests/common/framework/bleTestsScripts/runPI.sh
+++ tests/common/framework/bleTestsScripts/runPI.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-scp * pi@192.168.1.7: 
-ssh -t -t 192.168.1.7 -l pi << 'ENDSSH'
+scp * pi@192.168.1.4: 
+ssh -t -t 192.168.1.4 -l pi << 'ENDSSH'
 sudo rm -rf "/var/lib/bluetooth/*"
 sudo hciconfig hci0 reset
 python test1.py
