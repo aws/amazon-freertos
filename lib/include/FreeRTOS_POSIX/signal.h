e077694854fab8845dfcf596ffc08aa977717bb2
Sarena Meas <6563840+sarenameas@users.noreply.github.com>
2018-12-12 15:38:26 -0800

    docs: Update file versions for Amazon FreeRTOS 1.4.5.

--- lib/include/FreeRTOS_POSIX/signal.h
+++ lib/include/FreeRTOS_POSIX/signal.h
@@ -1,5 +1,5 @@
 /*
- * Amazon FreeRTOS+POSIX V1.0.0
+ * Amazon FreeRTOS+POSIX V1.0.1
  * Copyright (C) 2018 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
--- tests/common/test_runner/aws_test_runner.c
+++ tests/common/test_runner/aws_test_runner.c
@@ -1,5 +1,5 @@
 /*
- * Amazon FreeRTOS Test Runner V1.1.2  
+ * Amazon FreeRTOS Test Runner V1.1.3
  * Copyright (C) 2018 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
