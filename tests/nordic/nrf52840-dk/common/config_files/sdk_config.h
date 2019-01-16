a428e326747da6bb1b02d5e724ba0730b7da51cb
Konstantin Meshcheryakov <kmeshcheryakov@klika-tech.com>
2019-01-16 12:37:22 +0300

    Icrease the maximum MTU size to ensure tests passing

--- tests/nordic/nrf52840-dk/common/config_files/sdk_config.h
+++ tests/nordic/nrf52840-dk/common/config_files/sdk_config.h
@@ -12147,7 +12147,7 @@
 
 // <o> NRF_SDH_BLE_GATT_MAX_MTU_SIZE - Static maximum MTU size.
 #ifndef NRF_SDH_BLE_GATT_MAX_MTU_SIZE
-#define NRF_SDH_BLE_GATT_MAX_MTU_SIZE 180
+#define NRF_SDH_BLE_GATT_MAX_MTU_SIZE 200
 #endif
 
 // <o> NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE - Attribute Table size in bytes. The size must be a multiple of 4.
