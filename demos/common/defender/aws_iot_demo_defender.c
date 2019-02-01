e46a8e390e5d9ae53a71f97f7392039cbc29d579
qiutongs <qiutongs@amazon.com>
2019-02-01 15:37:25 -0800

    add tests/demo for multiple socket connections (#273)

--- demos/common/defender/aws_iot_demo_defender.c
+++ demos/common/defender/aws_iot_demo_defender.c
@@ -40,11 +40,21 @@
 /* Defender includes. */
 #include "aws_iot_defender.h"
 
+/* Secure Socket includes. */
+#include "aws_secure_sockets.h"
+
 /* POSIX sleep include. */
 #include "FreeRTOS_POSIX/unistd.h"
 
 /* Cbor includes. */
 #include "cbor.h"
+
+/* Rx and Tx time outs are used to ensure the sockets do not wait too long for
+ * missing data. */
+static const TickType_t xReceiveTimeOut = pdMS_TO_TICKS( 2000 );
+static const TickType_t xSendTimeOut = pdMS_TO_TICKS( 2000 );
+
+static Socket_t _createSocketToEchoServer();
 
 static void _defenderTask( void * param );
 
@@ -111,20 +121,33 @@ static void _defenderTask( void * param )
 
     AwsIotLogInfo( "----Device Defender Demo Start----.\r\n" );
 
-    /* provide metrics: all metrics in "tcp connections" group */
+    /* Create a socket connected to echo server. */
+    Socket_t socket = _createSocketToEchoServer();
+
+    /* Specify all metrics in "tcp connections" group */
     AwsIotDefender_SetMetrics( AWS_IOT_DEFENDER_METRICS_TCP_CONNECTIONS,
                                AWS_IOT_DEFENDER_METRICS_ALL );
 
-    /* set metrics report period to 5 minutes */
+    /* Set metrics report period to 5 minutes(300 seconds) */
     AwsIotDefender_SetPeriod( 300 );
 
+    /* Start the defender agent. */
     _startDefender();
 
-    /* let it run for 3 seconds */
+    uint32_t ip = SOCKETS_GetHostByName(clientcredentialMQTT_BROKER_ENDPOINT);
+    char buffer[16];
+    SOCKETS_inet_ntoa(ip, buffer);
+    AwsIotLogInfo("expected ip: %s",buffer);
+
+    /* Let it run for 3 seconds */
     sleep( 3 );
 
-    /* stop the defender*/
+    /* Stop the defender agent. */
     AwsIotDefender_Stop();
+
+    /* Clean up the socket. */
+    SOCKETS_Shutdown( socket, SOCKETS_SHUT_RDWR );
+    SOCKETS_Close( socket );
 
     AwsIotLogInfo( "----Device Defender Demo End----.\r\n" );
 }
@@ -135,7 +158,6 @@ static void _startDefender()
 {
     const AwsIotDefenderCallback_t callback = { .function = _defenderCallback, .param1 = NULL };
 
-    /* start the defender */
     AwsIotDefenderStartInfo_t startInfo =
     {
         .tlsInfo         = AWS_IOT_NETWORK_TLS_INFO_INITIALIZER,
@@ -162,7 +184,38 @@ static void _startDefender()
         startInfo.tlsInfo.pAlpnProtos = NULL;
     }
 
+    /* Invoke defender start API. */
     AwsIotDefender_Start( &startInfo );
+}
+
+/*-----------------------------------------------------------*/
+
+static Socket_t _createSocketToEchoServer()
+{
+    Socket_t socket;
+    SocketsSockaddr_t echoServerAddress;
+    int32_t error = 0;
+
+    /* Echo requests are sent to the echo server.  The address of the echo
+     * server is configured by the constants configECHO_SERVER_ADDR0 to
+     * configECHO_SERVER_ADDR3 in FreeRTOSConfig.h. */
+    echoServerAddress.usPort = SOCKETS_htons( configTCP_ECHO_CLIENT_PORT );
+    echoServerAddress.ulAddress = SOCKETS_inet_addr_quick( configECHO_SERVER_ADDR0,
+                                                           configECHO_SERVER_ADDR1,
+                                                           configECHO_SERVER_ADDR2,
+                                                           configECHO_SERVER_ADDR3 );
+
+    socket = SOCKETS_Socket( SOCKETS_AF_INET, SOCKETS_SOCK_STREAM, SOCKETS_IPPROTO_TCP );
+    AwsIotDemo_Assert( socket != SOCKETS_INVALID_SOCKET );
+
+    /* Set a time out so a missing reply does not cause the task to block indefinitely. */
+    SOCKETS_SetSockOpt( socket, 0, SOCKETS_SO_RCVTIMEO, &xReceiveTimeOut, sizeof( xReceiveTimeOut ) );
+    SOCKETS_SetSockOpt( socket, 0, SOCKETS_SO_SNDTIMEO, &xSendTimeOut, sizeof( xSendTimeOut ) );
+
+    error = SOCKETS_Connect( socket, &echoServerAddress, sizeof( echoServerAddress ) );
+    AwsIotDemo_Assert( error == 0 );
+
+    return socket;
 }
 
 /*-----------------------------------------------------------*/
