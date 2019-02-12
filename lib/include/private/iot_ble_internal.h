af4c276276fc3ef75326ba2027e2fcc38aa32f5e
hugues <33438817+huguesBouvier@users.noreply.github.com>
2019-02-11 17:12:51 -0800

    Remove iot header

--- lib/include/private/iot_ble_internal.h
+++ lib/include/private/iot_ble_internal.h
@@ -31,11 +31,6 @@
 #ifndef _AWS_BLE_INTERNALS_H_
 #define _AWS_BLE_INTERNALS_H_
 
-/* Build using a config header, if provided. */
-#ifdef AWS_IOT_CONFIG_FILE
-    #include AWS_IOT_CONFIG_FILE /* Lines between #ifdef/#endif are indented. */
-#endif
-
 #include "iot_linear_containers.h"
 
 /* Place holder for saving Char descriptors into memory. */
