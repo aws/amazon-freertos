6b8cc2b77de94349d23bd1bc800377f3bad33e8b
Gaurav Aggarwal <aggarg@amazon.com>
2019-02-21 13:25:15 -0800

    fix: Fix uninitialized variable in TCP tests
    xEhoServerAddress was created on stack and some of the member variables
    were not initialized. As a result those variables would end up having
    garbage values - this did not create any problem because we did not use
    those member variables. This commit initializes those member variables
    to correct values.
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- tests/common/secure_sockets/aws_test_tcp.c
+++ tests/common/secure_sockets/aws_test_tcp.c
@@ -1892,6 +1892,9 @@ static void prvConnect_InvalidAddressLength( Server_t xConn,
     {
         if( xConn == eNonsecure )
         {
+            xEchoServerAddress.ucLength = ( uint8_t ) sizeof( SocketsSockaddr_t );
+            xEchoServerAddress.ucSocketDomain = ( uint8_t ) SOCKETS_AF_INET;
+
             /* Populate the non-secure echo server address. */
             xEchoServerAddress.ulAddress = SOCKETS_inet_addr_quick( tcptestECHO_SERVER_ADDR0,
                                                                     tcptestECHO_SERVER_ADDR1,
