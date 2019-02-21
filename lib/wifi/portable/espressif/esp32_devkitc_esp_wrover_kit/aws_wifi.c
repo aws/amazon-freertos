987764b5168026dae3ee174fea566a031ffadcbd
Mahavir Jain <mahavir@espressif.com>
2019-02-21 13:09:29 +0530

    lib/wifif: espressif: fix scan failure under certain disconnect scenarios

--- lib/wifi/portable/espressif/esp32_devkitc_esp_wrover_kit/aws_wifi.c
+++ lib/wifi/portable/espressif/esp32_devkitc_esp_wrover_kit/aws_wifi.c
@@ -47,7 +47,7 @@ const int AP_STOPPED_BIT = BIT4;
 const int ESPTOUCH_DONE_BIT = BIT5;
 static bool wifi_conn_state;
 static bool wifi_ap_state;
-static bool wifi_ap_not_found;
+static bool wifi_auth_failure;
 
 /**
  * @brief Semaphore for WiFI module.
@@ -81,21 +81,24 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
             break;
         case SYSTEM_EVENT_STA_DISCONNECTED:
             ESP_LOGI(TAG, "SYSTEM_EVENT_STA_DISCONNECTED: %d", info->disconnected.reason);
-            wifi_ap_not_found = false;
+            wifi_auth_failure = false;
 
             /* Set code corresponding to the reason for disconnection */
             switch (info->disconnected.reason) {
                 case WIFI_REASON_AUTH_EXPIRE:
+                case WIFI_REASON_ASSOC_EXPIRE:
+                case WIFI_REASON_AUTH_LEAVE:
                 case WIFI_REASON_4WAY_HANDSHAKE_TIMEOUT:
                 case WIFI_REASON_BEACON_TIMEOUT:
                 case WIFI_REASON_AUTH_FAIL:
                 case WIFI_REASON_ASSOC_FAIL:
                 case WIFI_REASON_HANDSHAKE_TIMEOUT:
                     ESP_LOGD(TAG, "STA Auth Error");
+                    wifi_auth_failure = true;
                     break;
                 case WIFI_REASON_NO_AP_FOUND:
                     ESP_LOGD(TAG, "STA AP Not found");
-                    wifi_ap_not_found = true;
+                    wifi_auth_failure = true;
                     break;
                 default:
                     break;
@@ -415,7 +418,7 @@ WIFIReturnCode_t WIFI_Scan( WIFIScanResult_t * pxBuffer,
     /* Try to acquire the semaphore. */
     if( xSemaphoreTake( xWiFiSem, xSemaphoreWaitTicks ) == pdTRUE )
     {
-        if ( wifi_conn_state == false && wifi_ap_not_found == true )
+        if ( wifi_conn_state == false && wifi_auth_failure == true )
         {
             /* It seems that WiFi needs explicit disassoc before scan request post
              * attempt to connect to invalid network name or SSID.
