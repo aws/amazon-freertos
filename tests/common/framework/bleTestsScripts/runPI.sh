2893302d38e86d766f07e2809c64b8043092c346
hbouvier <33438817+huguesBouvier@users.noreply.github.com>
2019-03-06 16:18:28 -0800

    Changing script for new bridge keeper image

--- tests/common/framework/bleTestsScripts/runPI.sh
+++ tests/common/framework/bleTestsScripts/runPI.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-scp * pi@192.168.1.4: 
-ssh -t -t 192.168.1.4 -l pi << 'ENDSSH'
-sudo rm -rf "/var/lib/bluetooth/*"
-sudo hciconfig hci0 reset
+scp * root@192.168.1.4: 
+ssh -t -t 192.168.1.4 -l root << 'ENDSSH'
+rm -rf "/var/lib/bluetooth/*"
+hciconfig hci0 reset
 python test1.py
 sleep 1
 ENDSSH
