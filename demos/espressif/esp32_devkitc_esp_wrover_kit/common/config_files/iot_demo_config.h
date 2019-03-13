0bd90105408fa3d91e9e3e9fe3aa4ccf5f6a0470
hbouvier <33438817+huguesBouvier@users.noreply.github.com>
2019-03-12 17:57:18 -0700

    Increasing reponse time for robustness on faraway broker

--- demos/espressif/esp32_devkitc_esp_wrover_kit/common/config_files/iot_demo_config.h
+++ demos/espressif/esp32_devkitc_esp_wrover_kit/common/config_files/iot_demo_config.h
@@ -31,6 +31,8 @@
 #define IOT_DEMO_MQTT_PUBLISH_BURST_COUNT    ( 10 )
 #define IOT_DEMO_MQTT_PUBLISH_BURST_SIZE     ( 2 )
 
+#define IOT_MQTT_RESPONSE_WAIT_MS (10000)
+
 /* Global logging configuration. */
 #define IOT_LOG_LEVEL_DEMO                   IOT_LOG_INFO
 #define IOT_LOG_LEVEL_GLOBAL                 IOT_LOG_INFO
