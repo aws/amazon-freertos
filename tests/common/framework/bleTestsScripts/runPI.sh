101ae67246b18629a043a5c59e88000d3d40f200
hbouvier <33438817+huguesBouvier@users.noreply.github.com>
2019-03-12 18:21:35 -0700

    Changing log to PI

--- tests/common/framework/bleTestsScripts/runPI.sh
+++ tests/common/framework/bleTestsScripts/runPI.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-scp * pi@192.168.1.4: 
-ssh -t -t 192.168.1.4 -l pi << 'ENDSSH'
+scp * root@192.168.1.4: 
+ssh -t -t 192.168.1.4 -l root << 'ENDSSH'
 sudo rm -rf "/var/lib/bluetooth/*"
 sudo hciconfig hci0 reset
 python test1.py
