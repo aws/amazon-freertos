df48aa5e31c4c8d6843680f72a59e83cbd17f2cd
Cobus van Eeden <35851496+cobusve@users.noreply.github.com>
2019-04-16 14:30:12 -0700

    Missed this file in the checkin, it contains the Microchip CMAKE Metadata

---
+++ vendors/microchip/boards/curiosity_pic32mzef/curiosity_pic32mzef.cmake
@@ -0,0 +1,21 @@
+# -------------------------------------------------------------------------------------------------
+# Amazon FreeRTOS Console metadata
+# -------------------------------------------------------------------------------------------------
+afr_set_board_metadata(ID "CURIOSITY_PIC32MZ-EF")
+afr_set_board_metadata(DISPLAY_NAME "CURIOSITY_PIC32MZ-EF")
+afr_set_board_metadata(DESCRIPTION "Curiosity PIC32MZ-EF Development board produced by Microchip Technology Inc")
+afr_set_board_metadata(VENDOR_NAME "Microchip Technology Inc")
+afr_set_board_metadata(FAMILY_NAME "PIC32MZ-EF")
+afr_set_board_metadata(IS_ACTIVE "TRUE")
+afr_set_board_metadata(DATA_RAM_MEMORY "512KB")
+afr_set_board_metadata(PROGRAM_MEMORY "2MB")
+afr_set_board_metadata(CODE_SIGNER "AmazonFreeRTOS-Default")
+afr_set_board_metadata(SUPPORTED_IDE "CMakeBuildSystem")
+afr_set_board_metadata(RECOMMENDED_IDE "CMakeBuildSystem")
+afr_set_board_metadata(IDE_CMakeBuildSystem_NAME "CMakeBuildSystem")
+afr_set_board_metadata(IDE_CMakeBuildSystem_COMPILER "Microchip XC32")
+afr_set_board_metadata(IDE_CMakeBuildSystem_RECOMMENDED "TRUE")
+
+afr_set_board_metadata(IDE_CMakeBuildSystem_PROJECT_LOCATION "${AFR_ROOT_DIR}/demos/microchip/curiosity_pic32mzef/make")
+afr_set_board_metadata(DEMO_COMMON_LOCATION "${AFR_ROOT_DIR}/demos/microchip/curiosity_pic32mzef/common")
+afr_set_board_metadata(THIRD_PARTY_LIB_LOCATION "${AFR_ROOT_DIR}/lib/third_party/mcu_vendor/microchip")
