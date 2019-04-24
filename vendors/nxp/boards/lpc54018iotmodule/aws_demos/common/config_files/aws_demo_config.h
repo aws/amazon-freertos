6d6d6948cb0e8030696f4f6dc5ddb9e23d813111
qiutongs <qiutongs@amazon.com>
2019-04-24 15:58:16 -0700

    fix demo project for nxp (#553)

--- vendors/nxp/boards/lpc54018iotmodule/aws_demos/common/config_files/aws_demo_config.h
+++ vendors/nxp/boards/lpc54018iotmodule/aws_demos/common/config_files/aws_demo_config.h
@@ -29,52 +29,52 @@
 #include "iot_demo_runner.h"
 
 /* Default configuration for all demos. Individual demos can override these below */
-#define democonfigDEMO_STACKSIZE               ( configMINIMAL_STACK_SIZE * 5 )
-#define democonfigDEMO_PRIORITY                ( tskIDLE_PRIORITY + 1 )
-#define democonfigNETWORK_TYPES                ( AWSIOT_NETWORK_TYPE_WIFI )
+#define democonfigDEMO_STACKSIZE    ( configMINIMAL_STACK_SIZE * 15 )
+#define democonfigDEMO_PRIORITY     ( tskIDLE_PRIORITY + 1 )
+#define democonfigNETWORK_TYPES     ( AWSIOT_NETWORK_TYPE_WIFI )
 
 /* Some individual demos want to override these defaults, that is done in this section */
-#if defined(democonfigTCP_ECHO_TASKS_SEPARATE_ENABLED)
+#if defined( democonfigTCP_ECHO_TASKS_SEPARATE_ENABLED )
     #undef democonfigDEMO_STACKSIZE
-    #define democonfigDEMO_STACKSIZE                        ( configMINIMAL_STACK_SIZE * 4 )
+    #define democonfigDEMO_STACKSIZE    ( configMINIMAL_STACK_SIZE * 4 )
     #undef democonfigDEMO_PRIORITY
-    #define democonfigDEMO_PRIORITY                         ( tskIDLE_PRIORITY + 5 ) 
+    #define democonfigDEMO_PRIORITY     ( tskIDLE_PRIORITY + 5 )
 #endif
 
-#if defined(democonfigTCP_ECHO_TASKS_SINGLE_ENABLED)
+#if defined( democonfigTCP_ECHO_TASKS_SINGLE_ENABLED )
     #undef democonfigDEMO_STACKSIZE
-    #define democonfigDEMO_STACKSIZE                        ( configMINIMAL_STACK_SIZE * 4 )
+    #define democonfigDEMO_STACKSIZE                   ( configMINIMAL_STACK_SIZE * 4 )
     #undef democonfigDEMO_PRIORITY
-    #define democonfigDEMO_PRIORITY                         ( tskIDLE_PRIORITY + 1 ) 
+    #define democonfigDEMO_PRIORITY                    ( tskIDLE_PRIORITY + 1 )
 #endif
 
-#define democonfigSHADOW_DEMO_NUM_TASKS             ( 2 )
-#define democonfigSHADOW_DEMO_TASK_STACK_SIZE       ( configMINIMAL_STACK_SIZE * 4 )
-#define democonfigSHADOW_DEMO_TASK_PRIORITY         ( tskIDLE_PRIORITY + 1 )
-#define shadowDemoUPDATE_TASK_STACK_SIZE            ( configMINIMAL_STACK_SIZE * 5 )
+#define democonfigSHADOW_DEMO_NUM_TASKS                ( 2 )
+#define democonfigSHADOW_DEMO_TASK_STACK_SIZE          ( configMINIMAL_STACK_SIZE * 4 )
+#define democonfigSHADOW_DEMO_TASK_PRIORITY            ( tskIDLE_PRIORITY + 1 )
+#define shadowDemoUPDATE_TASK_STACK_SIZE               ( configMINIMAL_STACK_SIZE * 5 )
 
-#define democonfigMQTT_ECHO_TLS_NEGOTIATION_TIMEOUT pdMS_TO_TICKS( 12000 )
-#define democonfigMQTT_ECHO_TASK_STACK_SIZE         ( configMINIMAL_STACK_SIZE * 4 )
-#define democonfigMQTT_ECHO_TASK_PRIORITY           ( tskIDLE_PRIORITY )
+#define democonfigMQTT_ECHO_TLS_NEGOTIATION_TIMEOUT    pdMS_TO_TICKS( 12000 )
+#define democonfigMQTT_ECHO_TASK_STACK_SIZE            ( configMINIMAL_STACK_SIZE * 4 )
+#define democonfigMQTT_ECHO_TASK_PRIORITY              ( tskIDLE_PRIORITY )
 
 /* Number of sub pub tasks that connect to a broker that is not using TLS. */
-#define democonfigMQTT_SUB_PUB_NUM_UNSECURE_TASKS            ( 0 )
+#define democonfigMQTT_SUB_PUB_NUM_UNSECURE_TASKS      ( 0 )
 /* Number of sub pub tasks that connect to a broker that is using TLS. */
-#define democonfigMQTT_SUB_PUB_NUM_SECURE_TASKS              ( 1 )
+#define democonfigMQTT_SUB_PUB_NUM_SECURE_TASKS        ( 1 )
 
-#define democonfigMQTT_SUB_PUB_TASK_STACK_SIZE      ( configMINIMAL_STACK_SIZE * 5 )
-#define democonfigMQTT_SUB_PUB_TASK_PRIORITY        ( tskIDLE_PRIORITY + 1 )
+#define democonfigMQTT_SUB_PUB_TASK_STACK_SIZE         ( configMINIMAL_STACK_SIZE * 5 )
+#define democonfigMQTT_SUB_PUB_TASK_PRIORITY           ( tskIDLE_PRIORITY + 1 )
 
 
 /* Greengrass discovery example task parameters. */
-#define democonfigGREENGRASS_DISCOVERY_TASK_STACK_SIZE     ( configMINIMAL_STACK_SIZE * 22 )
-#define democonfigGREENGRASS_DISCOVERY_TASK_PRIORITY       ( tskIDLE_PRIORITY + 1 )
+#define democonfigGREENGRASS_DISCOVERY_TASK_STACK_SIZE    ( configMINIMAL_STACK_SIZE * 22 )
+#define democonfigGREENGRASS_DISCOVERY_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1 )
 
 /* Timeout used when performing MQTT operations that do not need extra time
-to perform a TLS negotiation. */
-#define democonfigMQTT_TIMEOUT			           pdMS_TO_TICKS( 2500 )
+ * to perform a TLS negotiation. */
+#define democonfigMQTT_TIMEOUT                            pdMS_TO_TICKS( 2500 )
 
 /* Send AWS IoT MQTT traffic encrypted. */
-#define democonfigMQTT_AGENT_CONNECT_FLAGS          	   ( mqttagentREQUIRE_TLS )
+#define democonfigMQTT_AGENT_CONNECT_FLAGS                ( mqttagentREQUIRE_TLS )
 
 #endif /* _AWS_DEMO_CONFIG_H_ */
