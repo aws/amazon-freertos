c8916542fde79bde6f6b6cd85edf2a64604cd2b6
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-05-01 13:15:40 -0700

    Increase size of FreeRTOS heap on ST

--- vendors/st/boards/stm32l475_discovery/aws_tests/common/config_files/aws_test_runner_config.h
+++ vendors/st/boards/stm32l475_discovery/aws_tests/common/config_files/aws_test_runner_config.h
@@ -36,16 +36,15 @@
 #define testrunnerFULL_OTA_AGENT_ENABLED           testrunnerUNSUPPORTED
 #define testrunnerFULL_OTA_PAL_ENABLED             testrunnerUNSUPPORTED
 #define testrunnerFULL_MQTT_ALPN_ENABLED           testrunnerUNSUPPORTED
+#define testrunnerFULL_MQTT_STRESS_TEST_ENABLED    testrunnerUNSUPPORTED
 
 /* Supported tests. 0 = Disabled, 1 = Enabled */
-#define testrunnerFULL_MQTT_STRESS_TEST_ENABLED    0
 #define testrunnerFULL_MQTT_AGENT_ENABLED          0
 #define testrunnerFULL_TCP_ENABLED                 1
 #define testrunnerFULL_GGD_ENABLED                 0
 #define testrunnerFULL_GGD_HELPER_ENABLED          0
 #define testrunnerFULL_SHADOW_ENABLED              0
 #define testrunnerFULL_SHADOWv4_ENABLED            0
-#define testrunnerFULL_MQTT_ENABLED                0
 #define testrunnerFULL_MQTTv4_ENABLED              0
 #define testrunnerFULL_PKCS11_ENABLED              0
 #define testrunnerFULL_CRYPTO_ENABLED              0
