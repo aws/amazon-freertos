42d74368e703d5ead689759afb32be5d540233f0
Qiutong Song <qiutongs@amazon.com>
2019-05-08 14:07:45 -0700

    update PR

--- vendors/renesas/amazon_freertos_common/compiler_support/ccrx/exit.c
+++ vendors/renesas/amazon_freertos_common/compiler_support/ccrx/exit.c
@@ -1,6 +1,3 @@
-/* Not sure why "abort" implementation was disabled. Enable it because tinycbor uses "assert" which calls abort. */
-
-#if 1 /* This file will be removed later because abort() is not used any more. */
 /* CC-RX's user define function */
 void abort( void );
 void abort( void )
