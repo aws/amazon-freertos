718d37b585180f3076e72d365ac69f54f333416c
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-05-16 10:59:08 -0700

    Increase heap size on TI

--- vendors/ti/boards/cc3220_launchpad/aws_tests/common/config_files/FreeRTOSConfig.h
+++ vendors/ti/boards/cc3220_launchpad/aws_tests/common/config_files/FreeRTOSConfig.h
@@ -55,7 +55,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define configUSE_TICK_HOOK                      0
 #define configTICK_RATE_HZ                       ( ( TickType_t ) 1000 )
 #define configMINIMAL_STACK_SIZE                 ( ( unsigned short ) 256 )
-#define configTOTAL_HEAP_SIZE                    ( ( size_t ) ( 0x10000 ) )
+#define configTOTAL_HEAP_SIZE                    ( ( size_t ) ( 85000 ) )
 #define configMAX_TASK_NAME_LEN                  ( 12 )
 #define configUSE_TRACE_FACILITY                 1
 #define configUSE_16_BIT_TICKS                   0
