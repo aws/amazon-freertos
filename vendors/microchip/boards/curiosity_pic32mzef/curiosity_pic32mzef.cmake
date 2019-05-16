75818a93b61ea0697055ffb3ff10b6c915c768f7
Manasa <smanas@amazon.com>
2019-05-16 11:42:57 -0700

    Fixing microchip metadata

--- vendors/microchip/boards/curiosity_pic32mzef/curiosity_pic32mzef.cmake
+++ vendors/microchip/boards/curiosity_pic32mzef/curiosity_pic32mzef.cmake
@@ -1,11 +1,11 @@
 # -------------------------------------------------------------------------------------------------
 # Amazon FreeRTOS Console metadata
 # -------------------------------------------------------------------------------------------------
-afr_set_board_metadata(ID "CURIOSITY_PIC32MZ-EF")
-afr_set_board_metadata(DISPLAY_NAME "CURIOSITY_PIC32MZ-EF")
-afr_set_board_metadata(DESCRIPTION "Curiosity PIC32MZ-EF Development board produced by Microchip Technology Inc")
-afr_set_board_metadata(VENDOR_NAME "Microchip Technology Inc")
-afr_set_board_metadata(FAMILY_NAME "PIC32MZ-EF")
+afr_set_board_metadata(ID "MCHP-Curiosity-PIC32MZEF")
+afr_set_board_metadata(DISPLAY_NAME "Curiosity PIC32MZEF")
+afr_set_board_metadata(DESCRIPTION "Development board for the PIC32MZ EF Series MCU")
+afr_set_board_metadata(VENDOR_NAME "Microchip")
+afr_set_board_metadata(FAMILY_NAME "PIC32MZ")
 afr_set_board_metadata(IS_ACTIVE "TRUE")
 afr_set_board_metadata(DATA_RAM_MEMORY "512KB")
 afr_set_board_metadata(PROGRAM_MEMORY "2MB")
