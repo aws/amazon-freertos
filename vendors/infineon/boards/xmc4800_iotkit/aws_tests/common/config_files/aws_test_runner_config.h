913d6b4505df5d19872ebf8888e6008f3450924f
Bhagavandas <bhagavar@amazon.com>
2019-05-16 18:10:46 -0700

    Reduce task pool stack size for infineon

--- vendors/infineon/boards/xmc4800_iotkit/aws_tests/common/config_files/aws_test_runner_config.h
+++ vendors/infineon/boards/xmc4800_iotkit/aws_tests/common/config_files/aws_test_runner_config.h
@@ -36,16 +36,18 @@
 #define testrunnerFULL_OTA_PAL_ENABLED             testrunnerUNSUPPORTED
 
 /* Enable tests by setting defines to 1 */
-#define testrunnerFULL_MQTT_ALPN_ENABLED           0
 #define testrunnerFULL_PKCS11_ENABLED              0
 #define testrunnerFULL_CRYPTO_ENABLED              0
-#define testrunnerFULL_MQTT_STRESS_TEST_ENABLED    0
-#define testrunnerFULL_MQTT_AGENT_ENABLED          0
-#define testrunnerFULL_TCP_ENABLED                 1
+#define testrunnerFULL_TCP_ENABLED                 0
 #define testrunnerFULL_GGD_ENABLED                 0
 #define testrunnerFULL_GGD_HELPER_ENABLED          0
+#define testrunnerFULL_SHADOWv4_ENABLED            0
 #define testrunnerFULL_SHADOW_ENABLED              0
+#define testrunnerFULL_MQTTv4_ENABLED              1
+#define testrunnerFULL_MQTT_STRESS_TEST_ENABLED    0
+#define testrunnerFULL_MQTT_AGENT_ENABLED          0
 #define testrunnerFULL_MQTT_ENABLED                0
+#define testrunnerFULL_MQTT_ALPN_ENABLED           0
 #define testrunnerFULL_WIFI_ENABLED                0
 #define testrunnerFULL_MEMORYLEAK_ENABLED          0
 #define testrunnerFULL_TLS_ENABLED                 0
