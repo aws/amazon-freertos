1a6be4a1a3fe10432699ce060616a530e4cc7624
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-05-17 09:33:30 -0700

    Add taskpool tests to ST

--- vendors/st/boards/stm32l475_discovery/aws_tests/common/config_files/aws_test_runner_config.h
+++ vendors/st/boards/stm32l475_discovery/aws_tests/common/config_files/aws_test_runner_config.h
@@ -39,6 +39,7 @@
 #define testrunnerFULL_MQTT_STRESS_TEST_ENABLED    testrunnerUNSUPPORTED
 
 /* Supported tests. 0 = Disabled, 1 = Enabled */
+#define testrunnerFULL_TASKPOOL_ENABLED            0
 #define testrunnerFULL_MQTT_AGENT_ENABLED          0
 #define testrunnerFULL_TCP_ENABLED                 1
 #define testrunnerFULL_GGD_ENABLED                 0
