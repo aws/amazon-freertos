7b1f6efa47ad10ef03ead4df86ce67dfe0c06027
qiutongs <qiutongs@amazon.com>
2019-05-21 16:27:59 -0700

    update demo project for Renesas (#647)

--- vendors/renesas/boards/rx65n-rsk/aws_demos/common/config_files/iot_config.h
+++ vendors/renesas/boards/rx65n-rsk/aws_demos/common/config_files/iot_config.h
@@ -31,7 +31,7 @@
 #define IOT_DEMO_MQTT_PUBLISH_BURST_COUNT    ( 10 )
 #define IOT_DEMO_MQTT_PUBLISH_BURST_SIZE     ( 2 )
 
-#define IOT_MQTT_RESPONSE_WAIT_MS (1000)
+#define IOT_MQTT_RESPONSE_WAIT_MS            ( 1000 )
 
 /* Global logging configuration. */
 #define IOT_LOG_LEVEL_GLOBAL                 IOT_LOG_INFO
@@ -42,10 +42,10 @@
 /*#define IOT_LOG_LEVEL_NETWORK                IOT_LOG_INFO */
 /*#define IOT_LOG_LEVEL_MQTT                   IOT_LOG_INFO */
 /*#define AWS_IOT_LOG_LEVEL_SHADOW             IOT_LOG_INFO */
-#define IOT_LOG_LEVEL_DEMO                   IOT_LOG_INFO
+#define IOT_LOG_LEVEL_DEMO               IOT_LOG_INFO
 
 /* Set the task pool stack size and priority */
-#define IOT_THREAD_DEFAULT_STACK_SIZE    2048
+#define IOT_THREAD_DEFAULT_STACK_SIZE    1024
 #define IOT_THREAD_DEFAULT_PRIORITY      5
 
 /* Include the common configuration file for FreeRTOS. */
