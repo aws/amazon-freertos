e500bdc4dbe8264dd51e4238d6870f4aafc36b44
Cobus van Eeden <35851496+cobusve@users.noreply.github.com>
2019-05-23 14:57:04 -0700

    Update Xilinx demos to work with new structure and update Xilinx CMakeLists

--- vendors/xilinx/boards/microzed/aws_demos/common/application_code/main.c
+++ vendors/xilinx/boards/microzed/aws_demos/common/application_code/main.c
@@ -24,18 +24,13 @@
  * http://www.FreeRTOS.org
  */
 
-
-#include <stdio.h>
 #include <time.h>
-#include <string.h>
-#include <stdlib.h>
+#include <stdio.h>
 
 /* FreeRTOS includes. */
 #include "FreeRTOS.h"
 #include "task.h"
-#include "timers.h"
-#include "queue.h"
-#include "semphr.h"
+
 #include "FreeRTOS_IP.h"
 #include "FreeRTOS_Sockets.h"
 #include "FreeRTOS_DHCP.h"
@@ -43,12 +38,19 @@
 /* Demo includes */
 #include "aws_demo.h"
 
-/* AWS library includes. */
+/* AWS System includes. */
+#include "aws_application_version.h"
 #include "aws_system_init.h"
-#include "aws_logging_task.h"
 #include "aws_clientcredential.h"
-#include "platform_config.h"
 #include "aws_dev_mode_key_provisioning.h"
+
+/* Demo application includes. */
+#include "aws_demo_config.h"
+#include "aws_demo.h"
+#include "aws_logging_task.h"
+
+/* Application version info. */
+#include "aws_application_version.h"
 
 /* Logging Task Defines. */
 #define mainLOGGING_MESSAGE_QUEUE_LENGTH    ( 15 )
@@ -61,58 +63,6 @@
 #define mainDEVICE_NICK_NAME				"XilinxDemo"
 
 
-/* Static arrays for FreeRTOS-Plus-TCP stack initialization for Ethernet network
- * connections are declared below. If you are using an Ethernet connection on your MCU
- * device it is recommended to use the FreeRTOS+TCP stack. The default values are
- * defined in FreeRTOSConfig.h. */
-
-/* Default MAC address configuration.  The application creates a virtual network
- * connection that uses this MAC address by accessing the raw Ethernet data
- * to and from a real network connection on the host PC.  See the
- * configNETWORK_INTERFACE_TO_USE definition for information on how to configure
- * the real network connection to use. */
-const uint8_t ucMACAddress[ 6 ] =
-{
-    configMAC_ADDR0,
-    configMAC_ADDR1,
-    configMAC_ADDR2,
-    configMAC_ADDR3,
-    configMAC_ADDR4,
-    configMAC_ADDR5
-};
-
-/* The default IP and MAC address used by the application.  The address configuration
- * defined here will be used if ipconfigUSE_DHCP is 0, or if ipconfigUSE_DHCP is
- * 1 but a DHCP server could not be contacted.  See the online documentation for
- * more information. */
-static const uint8_t ucIPAddress[ 4 ] =
-{
-    configIP_ADDR0,
-    configIP_ADDR1,
-    configIP_ADDR2,
-    configIP_ADDR3
-};
-static const uint8_t ucNetMask[ 4 ] =
-{
-    configNET_MASK0,
-    configNET_MASK1,
-    configNET_MASK2,
-    configNET_MASK3
-};
-static const uint8_t ucGatewayAddress[ 4 ] =
-{
-    configGATEWAY_ADDR0,
-    configGATEWAY_ADDR1,
-    configGATEWAY_ADDR2,
-    configGATEWAY_ADDR3
-};
-static const uint8_t ucDNSServerAddress[ 4 ] =
-{
-    configDNS_SERVER_ADDR0,
-    configDNS_SERVER_ADDR1,
-    configDNS_SERVER_ADDR2,
-    configDNS_SERVER_ADDR3
-};
 
 /**
  * @brief Application task startup hook for applications using Wi-Fi. If you are not
@@ -148,23 +98,15 @@ static UBaseType_t ulNextRand;
 /**
  * @brief Application runtime entry point.
  */
+extern void  vApplicationIPInit( void );
 int main( void )
 {
     /* Perform any hardware initialization that does not require the RTOS to be
      * running.  */
     prvMiscInitialization();
 
-    /* Create tasks that are not dependent on the Wi-Fi being initialized. */
-    xLoggingTaskInitialize( mainLOGGING_TASK_STACK_SIZE,
-                            tskIDLE_PRIORITY,
-                            mainLOGGING_MESSAGE_QUEUE_LENGTH );
+    vApplicationIPInit();
 
-    /* FreeRTOS TCP IP initialization function */
-    FreeRTOS_IPInit( ucIPAddress,
-                     ucNetMask,
-                     ucGatewayAddress,
-                     ucDNSServerAddress,
-                     ucMACAddress );
     /* Start the scheduler.  Initialization that requires the OS to be running,
      * including the Wi-Fi initialization, is performed in the RTOS daemon task
      * startup hook. */
@@ -202,6 +144,12 @@ static void prvMiscInitialization( void )
 	 * running, here.
 	 */
 
+    /* Start logging task. */
+    xLoggingTaskInitialize( mainLOGGING_TASK_STACK_SIZE,
+                            tskIDLE_PRIORITY,
+                            mainLOGGING_MESSAGE_QUEUE_LENGTH );
+
+
 	/* Seed the random number generator. */
 	time( &xTimeNow );
 	xil_printf("Seed for randomiser: %lu \n\r", xTimeNow);
@@ -210,18 +158,14 @@ static void prvMiscInitialization( void )
 	vPortInstallFreeRTOSVectorTable();
 	platform_init_fs();
 }
-/*-----------------------------------------------------------*/
 
-void vApplicationDaemonTaskStartupHook( void )
-{
-    /* Perform any hardware initialization, that require the RTOS to be
-     * running, here. NOTHING to be done here for now */
-}
 /*-----------------------------------------------------------*/
 
 void vApplicationIPNetworkEventHook( eIPCallbackEvent_t eNetworkEvent )
 {
     static BaseType_t xTasksAlreadyCreated = pdFALSE;
+    uint32_t ulIPAddress, ulNetMask, ulGatewayAddress, ulDNSServerAddress;
+    char cBuffer[ 16 ];
 
     /* If the network has just come up...*/
     if( eNetworkEvent == eNetworkUp )
@@ -232,113 +176,31 @@ void vApplicationIPNetworkEventHook( eIPCallbackEvent_t eNetworkEvent )
         	vDevModeKeyProvisioning();
             DEMO_RUNNER_RunDemos();
             xTasksAlreadyCreated = pdTRUE;
+
         }
+
+        /* Print out the network configuration, which may have come from a DHCP
+        * server. */
+        FreeRTOS_GetAddressConfiguration(
+            &ulIPAddress,
+            &ulNetMask,
+            &ulGatewayAddress,
+            &ulDNSServerAddress );
+        FreeRTOS_inet_ntoa( ulIPAddress, cBuffer );
+        FreeRTOS_printf( ( "\r\n\r\nIP Address: %s\r\n", cBuffer ) );
+
+        FreeRTOS_inet_ntoa( ulNetMask, cBuffer );
+        FreeRTOS_printf( ( "Subnet Mask: %s\r\n", cBuffer ) );
+
+        FreeRTOS_inet_ntoa( ulGatewayAddress, cBuffer );
+        FreeRTOS_printf( ( "Gateway Address: %s\r\n", cBuffer ) );
+
+        FreeRTOS_inet_ntoa( ulDNSServerAddress, cBuffer );
+        FreeRTOS_printf( ( "DNS Server Address: %s\r\n\r\n\r\n", cBuffer ) );
+    
     }
 }
 
-/*-----------------------------------------------------------*/
-
-/**
- * @brief This is to provide memory that is used by the Idle task.
- *
- * If configUSE_STATIC_ALLOCATION is set to 1, then the application must provide an
- * implementation of vApplicationGetIdleTaskMemory() in order to provide memory to
- * the Idle task.
- */
-void vApplicationGetIdleTaskMemory( StaticTask_t ** ppxIdleTaskTCBBuffer,
-                                    StackType_t ** ppxIdleTaskStackBuffer,
-                                    uint32_t * pulIdleTaskStackSize )
-{
-    /* If the buffers to be provided to the Idle task are declared inside this
-     * function then they must be declared static - otherwise they will be allocated on
-     * the stack and so not exists after this function exits. */
-    static StaticTask_t xIdleTaskTCB;
-    static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
-
-    /* Pass out a pointer to the StaticTask_t structure in which the Idle
-     * task's state will be stored. */
-    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
-
-    /* Pass out the array that will be used as the Idle task's stack. */
-    *ppxIdleTaskStackBuffer = uxIdleTaskStack;
-
-    /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
-     * Note that, as the array is necessarily of type StackType_t,
-     * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
-}
-/*-----------------------------------------------------------*/
-
-/**
- * @brief This is to provide the memory that is used by the RTOS daemon/time task.
- *
- * If configUSE_STATIC_ALLOCATION is set to 1, then application must provide an
- * implementation of vApplicationGetTimerTaskMemory() in order to provide memory
- * to the RTOS daemon/time task.
- */
-void vApplicationGetTimerTaskMemory( StaticTask_t ** ppxTimerTaskTCBBuffer,
-                                     StackType_t ** ppxTimerTaskStackBuffer,
-                                     uint32_t * pulTimerTaskStackSize )
-{
-    /* If the buffers to be provided to the Timer task are declared inside this
-     * function then they must be declared static - otherwise they will be allocated on
-     * the stack and so not exists after this function exits. */
-    static StaticTask_t xTimerTaskTCB;
-    static StackType_t uxTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
-
-    /* Pass out a pointer to the StaticTask_t structure in which the Idle
-     * task's state will be stored. */
-    *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
-
-    /* Pass out the array that will be used as the Timer task's stack. */
-    *ppxTimerTaskStackBuffer = uxTimerTaskStack;
-
-    /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
-     * Note that, as the array is necessarily of type StackType_t,
-     * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-    *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
-}
-/*-----------------------------------------------------------*/
-
-/**
- * @brief Warn user if pvPortMalloc fails.
- *
- * Called if a call to pvPortMalloc() fails because there is insufficient
- * free memory available in the FreeRTOS heap.  pvPortMalloc() is called
- * internally by FreeRTOS API functions that create tasks, queues, software
- * timers, and semaphores.  The size of the FreeRTOS heap is set by the
- * configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h.
- *
- */
-void vApplicationMallocFailedHook()
-{
-    taskDISABLE_INTERRUPTS();
-	configPRINTF( ("In vApplicationMallocFailedHook \n\r") );
-    for( ;; );
-}
-/*-----------------------------------------------------------*/
-
-/**
- * @brief Loop forever if stack overflow is detected.
- *
- * If configCHECK_FOR_STACK_OVERFLOW is set to 1,
- * this hook provides a location for applications to
- * define a response to a stack overflow.
- *
- * Use this hook to help identify that a stack overflow
- * has occurred.
- *
- */
-void vApplicationStackOverflowHook( TaskHandle_t xTask,
-                                    char * pcTaskName )
-{
-    portDISABLE_INTERRUPTS();
-
-    /* Loop forever */
-    for( ; ; )
-    {
-    }
-}
 /*-----------------------------------------------------------*/
 
 /**
@@ -494,19 +356,11 @@ void vAssertCalled(const char * pcFile,
 	}
 	taskENABLE_INTERRUPTS();
 }
+
+
 /*-----------------------------------------------------------*/
 
-/**
- * @brief User defined application hook need by the FreeRTOS-Plus-TCP library.
- */
-#if ( ipconfigUSE_LLMNR != 0 ) || ( ipconfigUSE_NBNS != 0 ) || ( ipconfigDHCP_REGISTER_HOSTNAME == 1 )
-    const char * pcApplicationHostnameHook(void)
-    {
+void vApplicationDaemonTaskStartupHook( void )
+{
 
-        /* This function will be called during the DHCP: the machine will be registered
-         * with an IP address plus this name. */
-        return clientcredentialIOT_THING_NAME;
-    }
-
-#endif
-
+}
