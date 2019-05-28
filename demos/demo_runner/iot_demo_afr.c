1d3802852d2150a19f73250984125d444420ade1
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-05-28 12:29:51 -0700

    Consistent const-ness of demo server info

--- demos/demo_runner/iot_demo_afr.c
+++ demos/demo_runner/iot_demo_afr.c
@@ -39,7 +39,7 @@
 #include "iot_mqtt.h"
 
 /* Server info, End point and port #. */
-static const IotNetworkServerInfoAfr_t serverInfo = AWS_IOT_NETWORK_SERVER_INFO_AFR_INITIALIZER;
+static IotNetworkServerInfoAfr_t serverInfo = AWS_IOT_NETWORK_SERVER_INFO_AFR_INITIALIZER;
 
 /* Secure connection info. */
 static IotNetworkCredentialsAfr_t credentials = AWS_IOT_NETWORK_CREDENTIALS_AFR_INITIALIZER;
@@ -132,13 +132,9 @@ static void _onNetworkStateChangeCallback( uint32_t network,
                 credentials.pAlpnProtos = NULL;
             }
 
-            /* According to C99 standard, it should transfer from any pointer to void pointer.
-             * In this case, it is "IotNetworkServerInfoAfr_t const *".
-             * But IAR compiler considers this is an incomplatible type, thus explictly casting to void *
-             */
             pDemoContext->networkConnectedCallback( true,
                                                     clientcredentialIOT_THING_NAME,
-                                                    ( void * ) &serverInfo,
+                                                    &serverInfo,
                                                     &credentials,
                                                     pNetworkInterface );
         }
@@ -165,13 +161,9 @@ static void _onNetworkStateChangeCallback( uint32_t network,
                     credentials.pAlpnProtos = NULL;
                 }
 
-                /* According to C99 standard, it should transfer from any pointer to void pointer.
-                 * In this case, it is "IotNetworkServerInfoAfr_t const *".
-                 * But IAR compiler considers this is an incomplatible type, thus explictly casting to void *
-                 */
                 pDemoContext->networkConnectedCallback( true,
                                                         clientcredentialIOT_THING_NAME,
-                                                        ( void * ) &serverInfo,
+                                                        &serverInfo,
                                                         &credentials,
                                                         pNetworkInterface );
             }
