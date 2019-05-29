ef1bfa786d0fef29d27d9ae11a955ef2701a12e9
Tiangang Song <ts.whu@outlook.com>
2019-05-29 15:36:35 -0700

    Use universal names in cmake toolchains files

--- tools/cmake/toolchains/xc32.cmake
+++ tools/cmake/toolchains/xc32.cmake
@@ -3,21 +3,22 @@ include("${CMAKE_CURRENT_LIST_DIR}/find_compiler.cmake")
 set(CMAKE_SYSTEM_NAME Generic)
 
 # Find Microchip XC32.
-afr_find_compiler(XC32_C xc32-gcc)
-afr_find_compiler(XC32_CXX xc32-g++)
+afr_find_compiler(AFR_COMPILER_CC xc32-gcc)
+afr_find_compiler(AFR_COMPILER_CXX xc32-g++)
+set(AFR_COMPILER_ASM "${AFR_COMPILER_CC}" CACHE INTERNAL "")
 
 # Specify the cross compiler.
-set(CMAKE_C_COMPILER ${XC32_C} CACHE FILEPATH "C compiler")
-set(CMAKE_CXX_COMPILER ${XC32_CXX} CACHE FILEPATH "C++ compiler")
-set(CMAKE_ASM_COMPILER ${XC32_C} CACHE FILEPATH "ASM compiler")
+set(CMAKE_C_COMPILER ${AFR_COMPILER_CC} CACHE FILEPATH "C compiler")
+set(CMAKE_CXX_COMPILER ${AFR_COMPILER_CXX} CACHE FILEPATH "C++ compiler")
+set(CMAKE_ASM_COMPILER ${AFR_COMPILER_ASM} CACHE FILEPATH "ASM compiler")
 
 # Disable compiler checks.
 set(CMAKE_C_COMPILER_FORCED TRUE)
 set(CMAKE_CXX_COMPILER_FORCED TRUE)
 
 # Add target system root to cmake find path.
-get_filename_component(XC32_BIN "${XC32_C}" DIRECTORY)
-get_filename_component(CMAKE_FIND_ROOT_PATH "${XC32_BIN}" DIRECTORY)
+get_filename_component(AFR_COMPILER_DIR "${AFR_COMPILER_CC}" DIRECTORY)
+get_filename_component(CMAKE_FIND_ROOT_PATH "${AFR_COMPILER_DIR}" DIRECTORY)
 
 # Don't look for executable in target system prefix.
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
@@ -25,3 +26,6 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # Look for includes and libraries only in the target system prefix.
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+
+# Helper variables to abstracts some common compiler flags.
+set(AFR_COMPILER_NO_WARNINGS "-w" CACHE INTERNAL "")
