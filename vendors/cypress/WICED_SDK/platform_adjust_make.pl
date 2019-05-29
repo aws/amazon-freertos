2b5d9e99626e06db98a035923e2933dd74aced04
Mradula Nayak <nmradula@amazon.com>
2019-05-29 15:37:41 -0700

    Modify WICED Studio IDE files for release-1.5

--- vendors/cypress/WICED_SDK/platform_adjust_make.pl
+++ vendors/cypress/WICED_SDK/platform_adjust_make.pl
@@ -42,9 +42,9 @@ use warnings;
 # 3. If any new .cproject file added with new app or with new platform, that need to be
 #    included in @projfiles array with full path.
 
-my @projfiles=("../../../../../tests/cypress/CYW954907AEVAL1F/wicedstudio/.cproject",
-               "../../../../../demos/cypress/CYW954907AEVAL1F/wicedstudio/.cproject",
-               "../../../../../tests/cypress/CYW943907AEVAL1F/wicedstudio/.cproject", "../../../../../demos/cypress/CYW943907AEVAL1F/wicedstudio/.cproject");
+my @projfiles=("../boards/CYW943907AEVAL1F/aws_demos/wicedstudio/.cproject",
+               "../boards/CYW943907AEVAL1F/aws_tests/wicedstudio/.cproject",
+               "../boards/CYW954907AEVAL1F/aws_demos/wicedstudio/.cproject", "../boards/CYW954907AEVAL1F/aws_tests/wicedstudio/.cproject");
 my @execfiles;
 my ($cmd, $status);
 if ($^O =~ m/darwin/i) {
