a70a4edf7ca7118363c94a25925e3841bfc02576
Yuhui Zheng <10982575+yuhui-zheng@users.noreply.github.com>
2019-06-04 14:49:14 -0700

    Marvell manifest.
    Signed-off-by: Yuhui Zheng <10982575+yuhui-zheng@users.noreply.github.com>

--- vendors/marvell/manifest.cmake
+++ vendors/marvell/manifest.cmake
@@ -1,7 +1,10 @@
 set(
     AFR_MANIFEST_SUPPORTED_BOARDS
-    "mw320_rd"
+    mw300_rd
+    mw320_rd
     CACHE INTERNAL "Supported boards list."
 )
 
 set(AFR_MANIFEST_BOARD_DIR "boards")
+set(AFR_MANIFEST_BOARD_DIR_mw300_rd "boards/mw320_rd")
+set(AFR_MANIFEST_BOARD_DIR_mw320_rd "boards/mw320_rd")
