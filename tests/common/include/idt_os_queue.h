4a718acbd6c86f1ec0e46ee1ec29d418895ea46e
Bora Cukurova <bcukuro@amazon.com>
2019-06-06 09:50:15 -0700

    ble test cases os abstraction

---
+++ tests/common/include/idt_os_queue.h
@@ -0,0 +1,54 @@
+/*
+ * Amazon FreeRTOS
+ * Copyright (C) 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * http://aws.amazon.com/freertos
+ * http://www.FreeRTOS.org
+ */
+
+#ifndef IDT_QUEUE_H
+#define IDT_QUEUE_H
+
+#include "idt_os_common.h"
+#include "FreeRTOS.h"
+#include "queue.h"
+// ======================================================= //
+
+#define idt_QueueReceive(Queue, Buffer, TimeToWait) \
+	xQueueReceive( Queue, Buffer, pdMS_TO_TICKS(TimeToWait) )
+
+// ======================================================= //
+
+#define idt_QueueCreate(QueueLength, ItemSize) \
+	xQueueCreate( QueueLength, ItemSize )
+
+// ======================================================= //
+
+#define idt_QueueSend(Queue, ItemTpQueue, TimeToWait) \
+	xQueueSend( Queue, ItemTpQueue, pdMS_TO_TICKS( TimeToWait))
+
+// ======================================================= //
+
+#define idt_QueueDelete(Queue) \
+	vQueueDelete( Queue )
+
+// ======================================================= //
+
+#endif /* IDT_QUEUE_H */
