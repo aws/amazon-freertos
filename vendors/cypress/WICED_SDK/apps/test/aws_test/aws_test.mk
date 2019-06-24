8d945012e09fe373202e5194be2e7d78e924362c
Hugues Bouvier <33438817+huguesBouvier@users.noreply.github.com>
2019-06-24 13:30:38 -0700

    renaming aws_test_tcp to iot_test_tcp

--- vendors/cypress/WICED_SDK/apps/test/aws_test/aws_test.mk
+++ vendors/cypress/WICED_SDK/apps/test/aws_test/aws_test.mk
@@ -123,7 +123,7 @@ $(NAME)_SOURCES    := $(AMAZON_FREERTOS_PATH)vendors/cypress/boards/$(PLATFORM)/
                       $(AFR_THIRDPARTY_PATH)tinycbor/cborpretty.c \
                       $(AFR_ABSTRACTIONS_PATH)wifi/test/iot_test_wifi.c \
                       $(AFR_FREERTOS_PLUS_STANDARD_PATH)tls/test/iot_test_tls.c \
-                      $(AFR_ABSTRACTIONS_PATH)secure_sockets/test/aws_test_tcp.c \
+                      $(AFR_ABSTRACTIONS_PATH)secure_sockets/test/iot_test_tcp.c \
                       $(AFR_ABSTRACTIONS_PATH)pkcs11/test/aws_test_pkcs11.c \
                       $(AFR_C_SDK_STANDARD_PATH)mqtt/test/iot_test_mqtt_agent.c \
                       $(AFR_C_SDK_STANDARD_PATH)mqtt/test/system/iot_tests_mqtt_system.c \
