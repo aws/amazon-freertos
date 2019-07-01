e99b1e7502baf6b5ea0f514adfb7259c27bb0122
Abhijit Dixit <44424462+abhidixi11@users.noreply.github.com>
2019-07-01 16:24:04 -0700

    feat: UDP network abstraction layer
    UDP abstraction layer for applications which use UDP (eg NTP)
    
    Current network abstraction layer support only TCP, this PR will
    support UDP

--- tests/common/aws_test_runner.c
+++ tests/common/aws_test_runner.c
@@ -217,6 +217,10 @@ static void RunTests( void )
             RUN_TEST_GROUP( Full_UDP );
         #endif
     #endif
+
+    #if ( testrunnerUTIL_PLATFORM_NETWORK_UDP_ENABLED == 1 )
+        RUN_TEST_GROUP( UTIL_Platform_Network_UDP );
+    #endif
 }
 /*-----------------------------------------------------------*/
 
