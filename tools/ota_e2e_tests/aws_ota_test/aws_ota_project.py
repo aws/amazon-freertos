4a01cd710ef6066739bbe6089b8af6ac7abeaeec
HarnishaA <gevariah@amazon.com>
2019-07-09 14:34:35 -0700

    Added two new precor test cases for OTA E2E.

--- tools/ota_e2e_tests/aws_ota_test/aws_ota_project.py
+++ tools/ota_e2e_tests/aws_ota_test/aws_ota_project.py
@@ -241,7 +241,7 @@ class OtaAfrProject:
                 prefix = next(prefix for prefix in prefixToValue.keys() if line.startswith(prefix))
                 if prefixToValue[prefix] != None:
                     line = '{} {}\n'.format(prefix, prefixToValue[prefix])
-            # print(line) will place an extra newline character in the file.
+            # print(line) # will place an extra newline character in the file.
             sys.stdout.write(line)
 
     def setMqttLogsOn(self):
@@ -293,6 +293,14 @@ class OtaAfrProject:
                 '#define APP_VERSION_BUILD': bugfix
             },
             os.path.join(self._projectRootDir, OtaAfrProject.APPLICATION_VERSION_PATH)
+        )
+
+    def setOtaBlockSize(self, blockSize):
+        """Set aws_application_version.h with the input version.
+        """
+        self.__setIdentifierInFile(
+            {' #define otaconfigMAX_NUM_BLOCKS_REQUEST': str(blockSize) + 'U'},
+            os.path.join(self._projectRootDir, self._boardProjectPath, 'config_files', 'aws_ota_agent_config.h')
         )
 
     def setCodesignerCertificate(self, certificate):
