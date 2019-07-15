0f6a0f35c4a605ef4aa74b4cfa4a5d2f77661755
Paul Butler <butler.paul@gmail.com>
2019-07-15 14:43:30 -0700

    update tracealyzer recorder library to 4.3.5
    Signed-off-by: Paul Butler <butler.paul@gmail.com>

--- libraries/3rdparty/tracealyzer_recorder/streamports/File/include/trcStreamingPort.h
+++ libraries/3rdparty/tracealyzer_recorder/streamports/File/include/trcStreamingPort.h
@@ -5,8 +5,7 @@
  * trcStreamingPort.h
  *
  * The interface definitions for trace streaming ("stream ports").
- * This "stream port" sets up the recorder to use TCP/IP as streaming channel.
- * The example is for lwIP.
+ * This "stream port" sets up the recorder to stream the trace to file.
  *
  * Terms of Use
  * This file is part of the trace recorder library (RECORDER), which is the 
@@ -47,19 +46,39 @@
 #ifndef TRC_STREAMING_PORT_H
 #define TRC_STREAMING_PORT_H
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+int32_t writeToFile(void* data, uint32_t size, int32_t *ptrBytesWritten);
+
+void closeFile(void);
+
+void openFile(char* fileName);
+
+/* This define will determine whether to use the internal PagedEventBuffer or not.
+If file writing creates additional trace events (i.e. it uses semaphores or mutexes), 
+then the paged event buffer must be enabled to avoid infinite recursion. */
 #define TRC_STREAM_PORT_USE_INTERNAL_BUFFER 1
 
-int32_t trcTcpRead(void* data, uint32_t size, int32_t *ptrBytesRead);
+#define TRC_STREAM_PORT_READ_DATA(_ptrData, _size, _ptrBytesRead) 0 /* Does not read commands from Tz (yet) */
 
-int32_t trcTcpWrite(void* data, uint32_t size, int32_t *ptrBytesWritten);
+#define TRC_STREAM_PORT_WRITE_DATA(_ptrData, _size, _ptrBytesSent) writeToFile(_ptrData, _size, _ptrBytesSent)
 
-#define TRC_STREAM_PORT_READ_DATA(_ptrData, _size, _ptrBytesRead) trcTcpRead(_ptrData, _size, _ptrBytesRead)
+#if (TRC_CFG_RECORDER_BUFFER_ALLOCATION == TRC_RECORDER_BUFFER_ALLOCATION_DYNAMIC)
+#define TRC_STREAM_PORT_MALLOC() \
+			_TzTraceData = TRC_PORT_MALLOC((TRC_CFG_PAGED_EVENT_BUFFER_PAGE_COUNT) * (TRC_CFG_PAGED_EVENT_BUFFER_PAGE_SIZE));
+extern char* _TzTraceData;
+#else
+#define TRC_STREAM_PORT_MALLOC()  /* Custom or static allocation. Not used. */
+#endif
+#define TRC_STREAM_PORT_INIT() \
+		TRC_STREAM_PORT_MALLOC(); \
+		openFile("trace.psf")
 
-#define TRC_STREAM_PORT_WRITE_DATA(_ptrData, _size, _ptrBytesSent) trcTcpWrite(_ptrData, _size, _ptrBytesSent)
+#define TRC_STREAM_PORT_ON_TRACE_END() closeFile()
 
 #ifdef __cplusplus
 }
--- libraries/3rdparty/tracealyzer_recorder/streamports/TCPIP_Win32/trcStreamingPort.c
+++ libraries/3rdparty/tracealyzer_recorder/streamports/TCPIP_Win32/trcStreamingPort.c
@@ -9,7 +9,7 @@
  * Existing ports can easily be modified to fit another setup, e.g., a 
  * different TCP/IP stack, or to define your own stream port.
  *
- * Terms of Use
+  * Terms of Use
  * This file is part of the trace recorder library (RECORDER), which is the 
  * intellectual property of Percepio AB (PERCEPIO) and provided under a
  * license as follows.
@@ -45,138 +45,165 @@
  * www.percepio.com
  ******************************************************************************/
 
+#include<stdio.h>
+#include<winsock2.h>
+
 #include "trcRecorder.h"
 
 #if (TRC_CFG_RECORDER_MODE == TRC_RECORDER_MODE_STREAMING)  
 #if (TRC_USE_TRACEALYZER_RECORDER == 1)
+
+
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+SOCKET server_socket = (UINT_PTR)NULL, trace_socket = (UINT_PTR)NULL;
+struct sockaddr_in server, client;
+
+int initServerSocketIfNeeded(void);
+int initWinsockIfNeeded(void);
+
+int initWinsockIfNeeded(void)
+{
+	WSADATA wsa;
 	
-/* TCP/IP includes - for lwIP in this case */
-#include "lwip/tcpip.h"
-#include "lwip/sockets.h"
+	if (server_socket)
+		return 0;
 
-int errno;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		return -1;
+	}
 
-#define TRC_TCPIP_PORT 12000
-
-int sock = -1, new_sd = -1;
-int flags = 0;
-int remoteSize;
-struct sockaddr_in address, remote;
-
-int32_t trcSocketSend( void* data, int32_t size, int32_t* bytesWritten )
-{
-  if (new_sd < 0)
-    return -1;
-  
-  if (bytesWritten == NULL)
-	return -1;
-  
-  *bytesWritten = send( new_sd, data, size, 0 );
-  if (*bytesWritten < 0)
-  {
-    /* EWOULDBLOCK may be expected when buffers are full */
-    if (errno != 0 && errno != EWOULDBLOCK)
-    {
-      closesocket(new_sd);
-      new_sd = -1;
-      return -1;
-    }
-    else
-        *bytesWritten = 0;
-  }
-  
-  return 0;
-}
-
-int32_t trcSocketReceive( void* data, int32_t size, int32_t* bytesRead )
-{
-  if (new_sd < 0)
-    return -1;
-
-  *bytesRead = recv( new_sd, data, size, 0 );
-  /* EWOULDBLOCK may be expected when there is no data to receive */
-  if (errno != 0 && errno != EWOULDBLOCK)
-  {
-    closesocket(new_sd);
-    new_sd = -1;
-    return -1;
-  }
-
-  return 0;
-}
-
-int32_t trcSocketInitializeListener()
-{
-  if (sock >= 0)
 	return 0;
-  
-  errno = 0;
-
-  sock = lwip_socket(AF_INET, SOCK_STREAM, 0);
-
-  if (sock < 0)
-    return -1;
-
-  address.sin_family = AF_INET;
-  address.sin_port = htons( TRC_TCPIP_PORT );
-  address.sin_addr.s_addr = INADDR_ANY;
-
-  if (bind(sock, (struct sockaddr *)&address, sizeof (address)) < 0)
-  {
-    closesocket(sock);
-    sock = -1;
-    return -1;
-  }
-
-  if (lwip_listen(sock, 5) < 0)
-  {
-    closesocket(sock);
-    sock = -1;
-    return -1;
-  }
-
-  return 0;
 }
 
-int32_t trcSocketAccept()
+
+int initServerSocketIfNeeded(void)
 {
-  if (sock < 0)
-      return -1;
-  
-  if (new_sd >= 0)
-      return 0;
-  
-  remoteSize = sizeof( remote );
-  new_sd = accept( sock, (struct sockaddr *)&remote, (socklen_t*)&remoteSize );
+	if (initWinsockIfNeeded() < 0)
+	{
+		return -1;
+	}
 
-  if( new_sd < 0 )
-  {
-   	closesocket(new_sd);
-    new_sd = -1;
-   	closesocket(sock);
-    sock = -1;
-    return -1;
-  }
+	if (server_socket)
+		return 0;
 
-  flags = fcntl( new_sd, F_GETFL, 0 );
-  fcntl( new_sd, F_SETFL, flags | O_NONBLOCK );
+	if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+	{
+		return -1;
+	}
 
-  return 0;
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(8888);
+
+	if (bind(server_socket, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
+	{
+		closesocket(server_socket);
+		WSACleanup();
+		server_socket = (UINT_PTR)NULL;
+		return -1;
+	}
+
+	if (listen(server_socket, 3) < 0)
+	{
+		closesocket(server_socket);
+		WSACleanup();
+		server_socket = (UINT_PTR)NULL;
+		return -1;
+	}
+
+	return 0;
 }
-/************** MODIFY THE ABOVE PART TO USE YOUR TPC/IP STACK ****************/
 
-int32_t trcTcpWrite(void* data, uint32_t size, int32_t *ptrBytesWritten)
+int initTraceSocketIfNeeded(void)
 {
-    return trcSocketSend(data, size, ptrBytesWritten);
+	int c;
+
+	if (!server_socket)
+		return -1;
+
+	if (trace_socket)
+		return 0;
+
+	c = sizeof(struct sockaddr_in);
+	trace_socket = accept(server_socket, (struct sockaddr *)&client, &c);
+	if (trace_socket == INVALID_SOCKET)
+	{
+		trace_socket = (UINT_PTR)NULL;
+		
+		closesocket(server_socket);
+		WSACleanup();
+		server_socket = (UINT_PTR)NULL;
+		
+		return -1;
+	}
+
+	return 0;
 }
 
-int32_t trcTcpRead(void* data, uint32_t size, int32_t *ptrBytesRead)
+int32_t writeToSocket(void* data, uint32_t size, int32_t *ptrBytesWritten)
 {
-    trcSocketInitializeListener();
-        
-    trcSocketAccept();
-      
-    return trcSocketReceive(data, size, ptrBytesRead);
+	int ret;
+
+	if (!trace_socket)
+	{
+		if (ptrBytesWritten != NULL)
+		{
+			*ptrBytesWritten = 0;
+		}
+		return -1;
+	}
+	ret = send(trace_socket, data, size, 0);
+	if (ret <= 0)
+	{
+		if (ptrBytesWritten != NULL)
+		{
+			*ptrBytesWritten = 0;
+		}
+
+		closesocket(trace_socket);
+		trace_socket = (UINT_PTR)NULL;
+		return ret;
+	}
+
+	if (ptrBytesWritten != NULL)
+	{
+		*ptrBytesWritten = ret;
+	}
+	
+	return 0;
+}
+
+int32_t readFromSocket(void* data, uint32_t bufsize, int32_t *ptrBytesRead)
+{
+	unsigned long bytesAvailable = 0;
+
+	if (initServerSocketIfNeeded() < 0)
+		return -1;
+	
+	if (initTraceSocketIfNeeded() < 0)
+		return -1;
+
+	if (ioctlsocket(trace_socket, FIONREAD, &bytesAvailable) != NO_ERROR)
+	{
+		closesocket(trace_socket);
+		trace_socket = (UINT_PTR)NULL;
+		return -1;
+	}
+
+	if (bytesAvailable > 0)
+	{
+		*ptrBytesRead = recv(trace_socket, data, bufsize, 0);
+		if (*ptrBytesRead == SOCKET_ERROR)
+		{
+			closesocket(trace_socket);
+			trace_socket = (UINT_PTR)NULL;
+			return -1;
+		}
+	}
+
+	return 0;
 }
 
 #endif /*(TRC_USE_TRACEALYZER_RECORDER == 1)*/
