3b7c128c0a8e02c755a896cbffcb286bd9dad2ba
Gaurav Aggarwal <aggarg@amazon.com>
2019-07-16 12:31:03 -0700

    Make the driver for Inventek WiFi module interrupt driven
    The driver for the Inventek WiFi module provided by ST had support for
    interrupts but it was written for CMSIS-RTOS. So far we were using the
    driver in the polling mode thereby wasting unnecessary CPU cycles while
    the module was busy working.
    
    This change uses the CMSIS wrapper for FreeRTOS provided by ST to enable
    the interrupt mode of the driver. As a result, we no longer will waste
    any unnecessary CPU cycles while the module is busy working.
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- vendors/st/boards/stm32l475_discovery/aws_tests/config_files/FreeRTOSConfig.h
+++ vendors/st/boards/stm32l475_discovery/aws_tests/config_files/FreeRTOSConfig.h
@@ -111,7 +111,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * routine that makes calls to interrupt safe FreeRTOS API functions.  DO NOT CALL
  * INTERRUPT SAFE FREERTOS API FUNCTIONS FROM ANY INTERRUPT THAT HAS A HIGHER
  * PRIORITY THAN THIS! (higher priorities are lower numeric values. */
-#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY    5
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY    1
 
 /* Interrupt priorities used by the kernel port layer itself.  These are generic
 * to all Cortex-M ports, and do not rely on any particular library functions. */
