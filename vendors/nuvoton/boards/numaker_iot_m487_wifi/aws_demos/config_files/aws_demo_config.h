69af492f548722e3ad6199b653681726f1ebcbb4
Qiutong Song <qiutongs@amazon.com>
2019-07-18 14:24:56 -0700

    add back iot_demo_freertos.c; remove --split_sections in cmake compile flags

--- vendors/nuvoton/boards/numaker_iot_m487_wifi/aws_demos/config_files/aws_demo_config.h
+++ vendors/nuvoton/boards/numaker_iot_m487_wifi/aws_demos/config_files/aws_demo_config.h
@@ -26,10 +26,22 @@
 #ifndef _AWS_DEMO_CONFIG_H_
 #define _AWS_DEMO_CONFIG_H_
 
+/* To run a particular demo you need to define one of these. 
+   Only one demo can be configured at a time
+            CONFIG_MQTT_DEMO_ENABLED
+            CONFIG_SHADOW_DEMO_ENABLED
+            CONFIG_GREENGRASS_DISCOVERY_DEMO_ENABLED
+            CONFIG_TCP_ECHO_CLIENT_DEMO_ENABLED
+            CONFIG_DEFENDER_DEMO_ENABLED
+            CONFIG_POSIX_DEMO_ENABLED
+            CONFIG_OTA_UPDATE_DEMO_ENABLED
+            
+    These defines are used in iot_demo_runner.h for demo selection */
+
 #define CONFIG_MQTT_DEMO_ENABLED 
 
 /* Default configuration for all demos. Individual demos can override these below */
-#define democonfigDEMO_STACKSIZE               ( configMINIMAL_STACK_SIZE * 8 )
+#define democonfigDEMO_STACKSIZE               ( configMINIMAL_STACK_SIZE * 20 )
 #define democonfigDEMO_PRIORITY                ( tskIDLE_PRIORITY + 5 )
 #define democonfigNETWORK_TYPES                ( AWSIOT_NETWORK_TYPE_WIFI )
 
