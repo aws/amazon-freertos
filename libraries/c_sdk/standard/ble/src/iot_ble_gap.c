7ef4f245e61a3aee1b62ee970504296b358b011b
Ravishankar Bhagavandas <bhagavar@amazon.com>
2019-07-18 15:13:21 -0700

    Enable/disable BLE gatt services through config (#952)
    * Make FreeRTOS GATT services optional in config
    
    * Uncrustify
    
    * Review Comments

--- libraries/c_sdk/standard/ble/src/iot_ble_gap.c
+++ libraries/c_sdk/standard/ble/src/iot_ble_gap.c
@@ -336,29 +336,30 @@ void _bondedCb( BTStatus_t status,
 
 BTStatus_t _startAllServices()
 {
-    BTStatus_t status = eBTStatusSuccess;
-    BaseType_t error;
+    BTStatus_t ret = eBTStatusSuccess;
+    bool status = true;
 
-    error = IotBleDeviceInfo_Init();
+    #if ( IOT_BLE_ENABLE_DEVICE_INFO_SERVICE == 1 )
+        status = IotBleDeviceInfo_Init();
+    #endif
 
-    if( error == pdPASS )
-    {
-        if( IotBleDataTransfer_Init() == false )
+    #if ( IOT_BLE_ENABLE_DATA_TRANSFER_SERVICE == 1 )
+        if( status == true )
         {
-            error = pdFAIL;
+            status = IotBleDataTransfer_Init();
         }
-    }
+    #endif
 
-    if( error != pdPASS )
+    if( status == false )
     {
-        status = eBTStatusFail;
+        ret = eBTStatusFail;
     }
 
     #if ( IOT_BLE_ADD_CUSTOM_SERVICES == 1 )
         IotBle_AddCustomServicesCb();
     #endif
 
-    return status;
+    return ret;
 }
 /*-----------------------------------------------------------*/
 
