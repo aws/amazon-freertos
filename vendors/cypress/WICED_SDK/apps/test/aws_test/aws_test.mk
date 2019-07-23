57fd38e6cc38b388fbf01f8d05b35caf0b84ac69
Alexa Noxon <33439348+alexa-noxon@users.noreply.github.com>
2019-07-23 08:35:20 -0700

    Update Cypress makefiles

--- vendors/cypress/WICED_SDK/apps/test/aws_test/aws_test.mk
+++ vendors/cypress/WICED_SDK/apps/test/aws_test/aws_test.mk
@@ -145,7 +145,6 @@ $(NAME)_SOURCES    := $(AMAZON_FREERTOS_PATH)vendors/cypress/boards/$(PLATFORM)/
                       $(AFR_C_SDK_AWS_PATH)defender/test/aws_iot_tests_defender_api.c \
                       $(AFR_FREERTOS_PLUS_STANDARD_PATH)crypto/test/iot_test_crypto.c \
                       $(AMAZON_FREERTOS_PATH)demos/dev_mode_key_provisioning/src/aws_dev_mode_key_provisioning.c \
-                      $(AMAZON_FREERTOS_PATH)demos/dev_mode_key_provisioning/src/pem2der.c \
                       $(AFR_C_SDK_STANDARD_PATH)common/taskpool/iot_taskpool.c \
                       $(AFR_ABSTRACTIONS_PATH)platform/freertos/iot_clock_freertos.c \
                       $(AFR_ABSTRACTIONS_PATH)platform/freertos/iot_network_freertos.c \
