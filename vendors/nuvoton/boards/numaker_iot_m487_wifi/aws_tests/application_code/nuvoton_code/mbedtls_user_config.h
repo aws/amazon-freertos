e3a85d4bf1595d4db2100c50d66e767c02970237
cyliangtw <cyliang@nuvoton.com>
2019-07-23 12:04:21 +0800

    To use default MBEDTLS_SSL_MAX_CONTENT_LEN for non-TLS ext

--- vendors/nuvoton/boards/numaker_iot_m487_wifi/aws_tests/application_code/nuvoton_code/mbedtls_user_config.h
+++ vendors/nuvoton/boards/numaker_iot_m487_wifi/aws_tests/application_code/nuvoton_code/mbedtls_user_config.h
@@ -46,9 +46,10 @@
 
  
 /* SSL options */
-#if defined(MBEDTLS_SSL_MAX_CONTENT_LEN)
-#undef MBEDTLS_SSL_MAX_CONTENT_LEN
-#endif
-#define MBEDTLS_SSL_MAX_CONTENT_LEN             6144 //6144 //5120 //8192 /**< Maximum fragment length in bytes, determines the size of each of the two internal I/O buffers */ 
+/* Below configuration could be enabled as while Server & client support TLS ext */
+//#if defined(MBEDTLS_SSL_MAX_CONTENT_LEN)
+//#undef MBEDTLS_SSL_MAX_CONTENT_LEN
+//#endif
+//#define MBEDTLS_SSL_MAX_CONTENT_LEN             4096 /**< Maximum fragment length in bytes, determines the size of each of the two internal I/O buffers. */ 
 
 #endif /* MBEDTLS_USER_CONFIG_H */
