49a9fd3e108edc69bed76ec6ae52f7d55c6f6673
Debasmita Lohar <dlohar2009@gmail.com>
2019-07-24 14:50:34 -0400

    Inserting two elements in the reqQ to get coverage

--- tools/cbmc/proofs/HTTP/IotHttpsClient_Disconnect/IotHttpsClient_Disconnect_harness.c
+++ tools/cbmc/proofs/HTTP/IotHttpsClient_Disconnect/IotHttpsClient_Disconnect_harness.c
@@ -12,11 +12,14 @@
 void harness() {
   IotHttpsConnectionHandle_t connHandle = newIotConnectionHandle();
   if(connHandle) {
-  /* This is done to insert an element in reqQ and get coverage in IotHttpsClient_Disconnect. */
-  	_httpsRequest_t * pReq = malloc(sizeof(_httpsRequest_t));
-  	pReq->pHttpsResponse = malloc(sizeof(_httpsResponse_t));
-  /* This function inserts the element in the reqQ.*/
-  	IotDeQueue_EnqueueTail(&(connHandle->reqQ), &(pReq->link));
+  /* This is done to insert two elements in reqQ and get coverage in IotHttpsClient_Disconnect. */
+  	_httpsRequest_t * p1Req = malloc(sizeof(_httpsRequest_t));
+  	p1Req->pHttpsResponse = malloc(sizeof(_httpsResponse_t));
+  	_httpsRequest_t * p2Req = malloc(sizeof(_httpsRequest_t));
+  	p2Req->pHttpsResponse = malloc(sizeof(_httpsResponse_t));
+  /* This function inserts an element in the reqQ.*/
+  	IotDeQueue_EnqueueTail(&(connHandle->reqQ), &(p1Req->link));
+  	IotDeQueue_EnqueueTail(&(connHandle->reqQ), &(p2Req->link));
   }
   IotHttpsClient_Disconnect(connHandle);
 }
