ef423f0b7866d025bbb13dfb3cb962e1f7a05567
Debasmita Lohar <dlohar@amazon.com>
2019-07-25 12:07:15 -0400

    CBMC proofs for various HTTPS Client API:
    * IotHttpsClient_Connect
    * IotHttpsClient_ReadContentLength
    * IotHttpsClient_ReadHeader
    *IotHttpsClient_ReadResponseStatus

---
+++ tools/cbmc/proofs/HTTP/IotHttpsClient_ReadContentLength/IotHttpsClient_ReadContentLength_harness.c
@@ -0,0 +1,16 @@
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "FreeRTOS_Sockets.h"
+
+/* FreeRTOS+TCP includes. */
+#include "iot_https_client.h"
+#include "iot_https_internal.h"
+
+#include "../global_state_HTTP.c"
+
+void harness() {
+  IotHttpsResponseHandle_t respHandle = newIotResponseHandle();
+  uint32_t pContentLength;
+  IotHttpsClient_ReadContentLength(respHandle, &pContentLength);
+}
