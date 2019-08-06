7623217b070bc0bf1341a2443bd91e2c80926040
Yuhui Zheng <10982575+yuhui-zheng@users.noreply.github.com>
2019-08-05 17:21:19 -0700

    Defender test mode support.
    In test mode, we are publishing to non-reserved topic, so that user can check what is published.
    Shorter duration is used, to help aid testing.

--- libraries/c_sdk/aws/defender/src/aws_iot_defender_mqtt.c
+++ libraries/c_sdk/aws/defender/src/aws_iot_defender_mqtt.c
@@ -28,8 +28,11 @@
 /* Defender internal include. */
 #include "private/aws_iot_defender_internal.h"
 
-/* Define topics segments used by defender. */
-#define TOPIC_PREFIX             "$aws/things/"
+#if ( DEVICE_DEFENDER_TEST_MODE==1 )
+    #define TOPIC_PREFIX             "iotdemo/"
+#else 
+    #define TOPIC_PREFIX             "$aws/things/"
+#endif
 
 #define TOPIC_SUFFIX_PUBLISH     "/defender/metrics/" DEFENDER_FORMAT
 
