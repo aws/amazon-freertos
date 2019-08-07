d06314c07ec918453badc79c279b21e25d320319
Sarena Meas <sarem@amazon.com>
2019-08-07 00:55:19 -0700

    Add testrunnerFULL_HTTPS_CLIENT_ENABLED to all board's aws_test_runner_config.h

--- vendors/microchip/boards/curiosity_pic32mzef/aws_tests/config_files/aws_test_runner_config.h
+++ vendors/microchip/boards/curiosity_pic32mzef/aws_tests/config_files/aws_test_runner_config.h
@@ -60,5 +60,6 @@
 #define testrunnerFULL_TLS_ENABLED                 0
 #define testrunnerFULL_OTA_AGENT_ENABLED           0
 #define testrunnerFULL_OTA_PAL_ENABLED             0
+#define testrunnerFULL_HTTPS_CLIENT_ENABLED        0
 
 #endif /* AWS_TEST_RUNNER_CONFIG_H */
