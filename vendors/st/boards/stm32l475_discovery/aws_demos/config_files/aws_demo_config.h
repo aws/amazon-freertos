090a23158352a28f9fa7c61eb747ec007815dbad
Gordon Wang <36049150+gordonwang0@users.noreply.github.com>
2019-08-14 17:12:39 -0700

    Increase stack size of ST Shadow demo

--- vendors/st/boards/stm32l475_discovery/aws_demos/config_files/aws_demo_config.h
+++ vendors/st/boards/stm32l475_discovery/aws_demos/config_files/aws_demo_config.h
@@ -49,7 +49,7 @@
 #define democonfigNETWORK_TYPES                           ( AWSIOT_NETWORK_TYPE_WIFI )
 
 #define democonfigSHADOW_DEMO_NUM_TASKS                   ( 2 )
-#define democonfigSHADOW_DEMO_TASK_STACK_SIZE             ( configMINIMAL_STACK_SIZE * 4 )
+#define democonfigSHADOW_DEMO_TASK_STACK_SIZE             ( 1024 )
 #define democonfigSHADOW_DEMO_TASK_PRIORITY               ( tskIDLE_PRIORITY + 5 )
 #define shadowDemoUPDATE_TASK_STACK_SIZE                  ( configMINIMAL_STACK_SIZE * 5 )
 
