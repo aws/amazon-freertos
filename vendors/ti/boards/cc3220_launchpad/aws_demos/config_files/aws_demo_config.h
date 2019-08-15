066728b22db7fdf83dcbf03c4f9b8d6ed0183c9a
Sarena Meas <sarem@amazon.com>
2019-08-14 17:38:20 -0700

    Undo accidential TI change.

--- vendors/ti/boards/cc3220_launchpad/aws_demos/config_files/aws_demo_config.h
+++ vendors/ti/boards/cc3220_launchpad/aws_demos/config_files/aws_demo_config.h
@@ -40,7 +40,7 @@
  *
  *  These defines are used in iot_demo_runner.h for demo selection */
 
-#define CONFIG_HTTPS_ASYNC_DOWNLOAD_DEMO_ENABLED
+#define CONFIG_MQTT_DEMO_ENABLED
 
 /* Default configuration for all demos. Individual demos can override these below */
 #define democonfigDEMO_STACKSIZE    ( configMINIMAL_STACK_SIZE * 8 )
