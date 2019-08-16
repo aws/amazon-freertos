3df1912eaae2b22760cc0cb64efac5ea644c02f2
Lee George Thomas <ltham@amazon.com>
2019-08-16 16:05:04 -0700

    File versions update for release 201908.00

--- libraries/abstractions/ble_hal/include/bt_hal_manager_adapter_classic.h
+++ libraries/abstractions/ble_hal/include/bt_hal_manager_adapter_classic.h
@@ -1,5 +1,5 @@
 /*
- * Amazon FreeRTOS BLE HAL V1.0.0
+ * Amazon FreeRTOS BLE HAL V2.0.0
  * Copyright (C) 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
