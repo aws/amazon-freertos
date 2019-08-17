90dce2274ee5f040a180b9f4b0cd8566fdf3fa75
leegeth <51681119+leegeth@users.noreply.github.com>
2019-08-16 17:35:28 -0700

    File versions update for release 201908.00 (#1100)
    * File versions update for release 201908.00
    
    * Fixing the version for PKCS #11

--- libraries/abstractions/platform/include/platform/iot_network.h
+++ libraries/abstractions/platform/include/platform/iot_network.h
@@ -1,5 +1,5 @@
 /*
- * Amazon FreeRTOS Platform V1.0.0
+ * Amazon FreeRTOS Platform V1.1.0
  * Copyright (C) 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
