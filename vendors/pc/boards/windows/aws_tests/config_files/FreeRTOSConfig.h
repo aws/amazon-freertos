4b07f636397c3a475bb6bbf688226973c05ace54
bauer-matthews <bauer.matthews@gmail.com>
2019-08-28 15:39:15 -0400

    Update FreeRTOSConfig.h

--- vendors/pc/boards/windows/aws_tests/config_files/FreeRTOSConfig.h
+++ vendors/pc/boards/windows/aws_tests/config_files/FreeRTOSConfig.h
@@ -168,7 +168,7 @@ extern void vLoggingPrint( const char * pcMessage );
  * results in the wired network being used, while setting
  * configNETWORK_INTERFACE_TO_USE to 2 results in the wireless network being
  * used. */
-#define configNETWORK_INTERFACE_TO_USE       ( 1L )
+#define configNETWORK_INTERFACE_TO_USE       ( 3L )
 
 /* The address of an echo server that will be used by the two demo echo client
  * tasks:
