5f04fa49ddf1af22b8428104fca2285d5be7e0af
qiutongs <songqt01@gmail,com>
2019-09-12 15:02:43 -0700

    update common io files with Amazon FreeRTOS licenses

--- libraries/abstractions/common_io/test/iot_test_i2c.c
+++ libraries/abstractions/common_io/test/iot_test_i2c.c
@@ -1,5 +1,5 @@
 /*
- * Amazon FreeRTOS (don't know what to name here)
+ * Amazon FreeRTOS Common IO V1.0.0
  * Copyright (C) 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
