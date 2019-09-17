95b880858839d6b817f984ffb96f441308f9a2c0
qiutongs <songqt01@gmail,com>
2019-09-17 14:22:51 -0700

    add detailed API document to i2c

--- libraries/abstractions/common_io/include/iot_i2c.h
+++ libraries/abstractions/common_io/include/iot_i2c.h
@@ -42,32 +42,32 @@
 /**
  * The speeds supported by I2C bus.
  */
-#define IOT_I2C_STANDARD_MODE_BPS         ( 100000 )
-#define IOT_I2C_FAST_MODE_BPS             ( 400000 )
-#define IOT_I2C_FAST_MODE_PLUS_BPS        ( 1000000 )
-#define IOT_I2C_HIGH_SPEED_BPS            ( 3400000 )
+#define IOT_I2C_STANDARD_MODE_BPS         ( 100000 )  /*!< Standard mode bits per second. */
+#define IOT_I2C_FAST_MODE_BPS             ( 400000 )  /*!< Fast mode bits per second. */
+#define IOT_I2C_FAST_MODE_PLUS_BPS        ( 1000000 ) /*!< Fast plus mode bits per second. */
+#define IOT_I2C_HIGH_SPEED_BPS            ( 3400000 ) /*!< High speed mode bits per second. */
 
 /**
  * The return codes for the functions in I2C.
  */
-#define IOT_I2C_SUCCESS                       ( 0 )
-#define IOT_I2C_INVALID_VALUE                 ( 1 )
-#define IOT_I2C_BUSY                          ( 2 )
-#define IOT_I2C_WRITE_FAILED                  ( 3 )
-#define IOT_I2C_READ_FAILED                   ( 4 )
-#define IOT_I2C_NACK                          ( 5 )
-#define IOT_I2C_BUS_TIMEOUT                   ( 6 )
-#define IOT_I2C_NOTHING_TO_CANCEL             ( 7 )
-#define IOT_I2C_FUNCTION_NOT_SUPPORTED        ( 8 )
-#define IOT_I2C_SLAVE_ADDRESS_NOT_SET         ( 9 )
+#define IOT_I2C_SUCCESS                   ( 0 )     /*!< I2C operation completed successfully. */
+#define IOT_I2C_INVALID_VALUE             ( 1 )     /*!< At least one parameter is invalid. */
+#define IOT_I2C_BUSY                      ( 2 )     /*!< I2C bus is busy at current time. */
+#define IOT_I2C_WRITE_FAILED              ( 3 )     /*!< I2C driver returns error when performing write operation. */
+#define IOT_I2C_READ_FAILED               ( 4 )     /*!< I2C driver returns error when performing read operation. */
+#define IOT_I2C_NACK                      ( 5 )     /*!< Unexpected NACK is caught. */
+#define IOT_I2C_BUS_TIMEOUT               ( 6 )     /*!< I2C operation not completed within specified timeout. */
+#define IOT_I2C_NOTHING_TO_CANCEL         ( 7 )     /*!< No ongoing transaction when cancel operation is performed. */
+#define IOT_I2C_FUNCTION_NOT_SUPPORTED    ( 8 )     /*!< I2C operation is not supported. */
+#define IOT_I2C_SLAVE_ADDRESS_NOT_SET     ( 9 )     /*!< Slave address is not set before calling I2C read or write operation. */
 
 /**
  * @brief I2C Bus status
  */
 typedef enum
 {
-    eI2CBusIdle = 0,
-    eI2cBusBusy = IOT_I2C_BUSY,
+    eI2CBusIdle = 0,            /*!< I2C bus is idle. */
+    eI2cBusBusy = IOT_I2C_BUSY, /*!< I2C bus is busy. */
 } IotI2CBusStatus_t;
 
 /**
@@ -75,10 +75,10 @@ typedef enum
  */
 typedef enum
 {
-    eI2CCompleted = IOT_I2C_SUCCESS,
-    eI2CDriverFailed,
-    eI2CNackFromSlave = IOT_I2C_NACK,
-    eI2CMasterTimeout = IOT_I2C_BUS_TIMEOUT,
+    eI2CCompleted = IOT_I2C_SUCCESS,         /*!< I2C operation completed successfully. */
+    eI2CDriverFailed,                        /*!< I2C driver returns error during last operation. */
+    eI2CNackFromSlave = IOT_I2C_NACK,        /*!< Unexpected NACK is caught. */
+    eI2CMasterTimeout = IOT_I2C_BUS_TIMEOUT, /*!< I2C operation not completed within specified timeout. */
 } IotI2COperationStatus_t;
 
 /**
@@ -86,8 +86,8 @@ typedef enum
  */
 typedef struct IotI2CConfig
 {
-    uint32_t ulMasterTimeout;   /**<! Master timeout value in msec, to relinquish the bus if slave does not respond. This is an extended feature of SMBus which is not part of I2C protocol. */
-    uint32_t ulBusFreq;         /**<! Bus frequency/baud rate */
+    uint32_t ulMasterTimeout; /**<! Master timeout value in msec, to relinquish the bus if slave does not respond. This is an extended feature of SMBus which is not part of I2C protocol. */
+    uint32_t ulBusFreq;       /**<! Bus frequency/baud rate */
 } IotI2CConfig_t;
 
 /**
@@ -95,17 +95,16 @@ typedef struct IotI2CConfig
  */
 typedef enum
 {
-    eI2CSendNoStopFlag,     /**<! Set flag to not send stop after transaction */
-                            /**<! Default is always stop for every transaction */
-                            /**<! Flag will auto reset to stop after one transaction if you set no stop */
-    eI2CSetSlaveAddr,       /**<! All the operations use this slave address after it is set. */
-    eI2CSetMasterConfig,    /**<! Sets the I2C bus frequency and timeout using the struct IotI2CConfig_t,
-                             *   default speed is Standard */
-    eI2CGetMasterConfig,    /**<! Gets the I2C bus frequency and timeout set for the I2C master. */
-    eI2CGetBusState,        /**<! Get the current I2C bus status. Returns eI2CBusIdle or eI2CBusy */
-    eI2CBusReset,           /**<! Master resets the bus. */
-    eI2CGetTxNoOfbytes,     /**<! Get the number of bytes sent in write operation. */
-    eI2CGetRxNoOfbytes,     /**<! Get the number of bytes received in read operation. */
+    eI2CSendNoStopFlag,  /**<! Set flag to not send stop after transaction */
+                         /**<! Default is always stop for every transaction */
+                         /**<! Flag will auto reset to stop after one transaction if you set no stop */
+    eI2CSetSlaveAddr,    /**<! All the operations use this slave address after it is set. */
+    eI2CSetMasterConfig, /**<! Sets the I2C bus frequency and timeout using the struct IotI2CConfig_t, default speed is Standard mode. */
+    eI2CGetMasterConfig, /**<! Gets the I2C bus frequency and timeout set for the I2C master. */
+    eI2CGetBusState,     /**<! Get the current I2C bus status. Returns eI2CBusIdle or eI2CBusy */
+    eI2CBusReset,        /**<! Master resets the bus. */
+    eI2CGetTxNoOfbytes,  /**<! Get the number of bytes sent in write operation. */
+    eI2CGetRxNoOfbytes,  /**<! Get the number of bytes received in read operation. */
 } IotI2CIoctlRequest_t;
 
 /**
@@ -118,7 +117,7 @@ struct                    IotI2CDescriptor;
  *        This is initialized in open and returned to caller. The caller must pass
  *        this pointer to the rest of APIs.
  */
-typedef struct IotI2CDescriptor   * IotI2CHandle_t;
+typedef struct IotI2CDescriptor * IotI2CHandle_t;
 
 /**
  * @brief The callback function for completion of I2C operation.
@@ -130,7 +129,8 @@ typedef struct IotI2CDescriptor   * IotI2CHandle_t;
  *                          passed back to the caller in the callback.
  *
  */
-typedef void (* IotI2CCallback_t) ( IotI2COperationStatus_t xOpStatus, void * pvUserContext );
+typedef void (* IotI2CCallback_t) ( IotI2COperationStatus_t xOpStatus,
+                                    void * pvUserContext );
 
 /**
  * @brief Initiates and reserves an I2C instance as master.
@@ -150,6 +150,7 @@ IotI2CHandle_t iot_i2c_open( int32_t lI2CInstance );
 /**
  * @brief Sets the application callback to be called on completion of an operation.
  *
+ * @note This is only used in asynchronous call.
  * @warning If the input handle is invalid, this function silently takes no action.
  *
  * @param[in] pxI2CPeripheral The I2C peripheral handle returned in the open() call.
@@ -161,9 +162,9 @@ void iot_i2c_set_callback( IotI2CHandle_t const pxI2CPeripheral,
                            void * pvUserContext );
 
 /**
- * @brief Starts the I2C master read operation in blocking mode.
+ * @brief Starts the I2C master read operation in synchronous mode.
  *
- * This function attempts to read certain number of bytes from slave device to a pre-allocated buffer.
+ * This function attempts to read certain number of bytes from slave device to a pre-allocated buffer, in synchronous way.
  * Partial read might happen, e.g. no more data is available.
  * And the number of bytes that have been actually read can be obtained by calling iot_i2c_ioctl.
  *
@@ -178,29 +179,30 @@ void iot_i2c_set_callback( IotI2CHandle_t const pxI2CPeripheral,
  * @param[in] xBytes The number of bytes to read.
  *
  * @return
- * - IOT_I2C_SUCCESS, on success and all the requested bytes have been read
+ * - IOT_I2C_SUCCESS, on success
  * - IOT_I2C_INVALID_VALUE, if
  *     - pxI2CPeripheral is NULL
  *     - pxI2CPeripheral is not opened yet
  *     - pucBuffer is NULL
  *     - xBytes is 0
- * - IOT_I2C_NACK, if
+ * - IOT_I2C_SLAVE_ADDRESS_NOT_SET, if slave address is not set yet
+ * - IOT_I2C_NACK or IOT_I2C_READ_FAILED, if
  *     - no device correspond to the slave address
  *     - the slave is unable to receive or transmit
  *     - the slave gets data or commands that it does not understand
- *     - not all the requested data is available.
- * - IOT_I2C_BUS_TIMEOUT, if this is a SMBus and salve device does not respond within configured timeout.
+ *     - there is some unknown driver error
+ * - IOT_I2C_BUS_TIMEOUT, if timeout is supported and slave device does not respond within configured timeout.
  * - IOT_I2C_BUSY, if the bus is busy which means there is an ongoing transaction.
- * - IOT_I2C_READ_FAILED, if there is some unknown driver error
+ * - IOT_I2C_FUNCTION_NOT_SUPPORTED, if this board doesn't support this operation.
  */
 int32_t iot_i2c_read_sync( IotI2CHandle_t const pxI2CPeripheral,
                            uint8_t * const pucBuffer,
                            size_t xBytes );
 
 /**
- * @brief Starts the I2C master write operation in blocking mode.
+ * @brief Starts the I2C master write operation in synchronous mode.
  *
- * This function attempts to write certain number of bytes from a pre-allocated buffer to a slave device.
+ * This function attempts to write certain number of bytes from a pre-allocated buffer to a slave device, in synchronous way.
  * Partial write might happen, e.g. slave device unable to receive more data.
  * And the number of bytes that have been actually written can be obtained by calling iot_i2c_ioctl.
  *
@@ -215,52 +217,102 @@ int32_t iot_i2c_read_sync( IotI2CHandle_t const pxI2CPeripheral,
  * @param[in] xBytes The number of bytes to write.
  *
  * @return
- * - IOT_I2C_SUCCESS, on success and all the requested bytes have been written
+ * - IOT_I2C_SUCCESS, on success
  * - IOT_I2C_INVALID_VALUE, if
  *     - pxI2CPeripheral is NULL
  *     - pxI2CPeripheral is not opened yet
  *     - pucBuffer is NULL
  *     - xBytes is 0
- * - IOT_I2C_NACK, if
+ * - IOT_I2C_SLAVE_ADDRESS_NOT_SET, if slave address is not set yet
+ * - IOT_I2C_NACK or IOT_I2C_WRITE_FAILED, if
  *     - no device correspond to the slave address
  *     - the slave is unable to receive or transmit
  *     - the slave gets data or commands that it does not understand
- *     - not all the requested data is available.
- * - IOT_I2C_BUS_TIMEOUT, if this is a SMBus and slave device does not respond within configured timeout.
+ *     - there is some unknown driver error
+ * - IOT_I2C_BUS_TIMEOUT, if timeout is supported and slave device does not respond within configured timeout.
  * - IOT_I2C_BUSY, if the bus is busy which means there is an ongoing transaction.
- * - IOT_I2C_WRITE_FAILED, if there is some unknown driver error
+ * - IOT_I2C_FUNCTION_NOT_SUPPORTED, if this board doesn't support this operation.
  */
 int32_t iot_i2c_write_sync( IotI2CHandle_t const pxI2CPeripheral,
                             uint8_t * const pucBuffer,
                             size_t xBytes );
 
 /**
- * @brief Starts the I2C master read operation in non-blocking mode.
- *        You write_sync the address of the register you want to read from first to read data
+ * @brief Starts the I2C master read operation in asynchronous mode.
+ *
+ * This function attempts to read certain number of bytes from a pre-allocated buffer, in asynchronous way.
+ * It returns immediately when the operation is started and the status can be check by calling iot_i2c_ioctl.
+ * Once the operation completes, successful or not, the user callback will be invoked.
+ *
+ * Partial read might happen, e.g. slave device unable to receive more data.
+ * And the number of bytes that have been actually read can be obtained by calling iot_i2c_ioctl.
+ *
+ * @Note Usually, the address of register needs to be written before calling this function.
+ * @Note If eI2CSendNoStopFlag is set and this function returns, whether the actual transaction has been started is undefined. This is board-specific behavior.
+ *
+ * @warning Prior to this function, slave address must be already configured.
+ * @warning pucBuffer must be valid before callback is invoked.
+ * @warning None of other read or write functions shall be called during this function or before user callback.
  *
  * @param[in] pxI2CPeripheral The I2C handle returned in open() call.
  * @param[out] pucBuffer The receive buffer to read the data into
  * @param[in] xBytes The number of bytes to read.
  *
- * @return SUCCESS = IOT_I2C_SUCCESS,
- *         FAILED = IOT_I2C_READ_FAILED
- *         BUSY = IOT_I2C_BUSY
+ * @return
+ * - IOT_I2C_SUCCESS, on success
+ * - IOT_I2C_INVALID_VALUE, if
+ *     - pxI2CPeripheral is NULL
+ *     - pxI2CPeripheral is not opened yet
+ *     - pucBuffer is NULL
+ *     - xBytes is 0
+ * - IOT_I2C_SLAVE_ADDRESS_NOT_SET, if slave address is not set yet
+ * - IOT_I2C_NACK or IOT_I2C_READ_FAILED, if
+ *     - no device correspond to the slave address
+ *     - the slave is unable to receive or transmit
+ *     - the slave gets data or commands that it does not understand
+ *     - there is some unknown driver error
+ * - IOT_I2C_FUNCTION_NOT_SUPPORTED, if this board doesn't support this operation.
  */
 int32_t iot_i2c_read_async( IotI2CHandle_t const pxI2CPeripheral,
                             uint8_t * const pucBuffer,
                             size_t xBytes );
 
 /**
- * @brief Starts the I2C master write operation in non-blocking mode.
+ * @brief Starts the I2C master write operation in asynchronous mode.
+ *
+ * This function attempts to write certain number of bytes from a pre-allocated buffer to a slave device, in asynchronous way.
+ * It returns immediately when the operation is started and the status can be check by calling iot_i2c_ioctl.
+ * Once the operation completes, successful or not, the user callback will be invoked.
+ *
+ * Partial write might happen, e.g. slave device unable to receive more data.
+ * And the number of bytes that have been actually written can be obtained by calling iot_i2c_ioctl.
+ *
+ * @Note Usually, the first byte is treated as the register address and the following bytes are treated as data to be written.
+ * @Note If eI2CSendNoStopFlag is set and this function returns, whether the actual transaction has been started is undefined. This is board-specific behavior.
+ *
+ * @warning Prior to this function, slave address must be already configured.
+ * @warning None of other read or write functions shall be called during this function.
  *
  * @param[in] pxI2CPeripheral The I2C handle returned in open() call.
- * @param[in] pucBuffer The transmit buffer containing the data to be written.
- *                     First byte is always treated as the register address and the following bytes are treated as data to be written.
+ * @param[in] pucBuffer The transmit buffer containing the data to be written. It must stay allocated before this function returns.
  * @param[in] xBytes The number of bytes to write.
  *
- * @return SUCCESS = IOT_I2C_SUCCESS,
- *         FAILED = IOT_I2C_WRITE_FAILED,
- *         BUSY = IOT_I2C_BUSY
+ * @return
+ * - IOT_I2C_SUCCESS, on success
+ * - IOT_I2C_INVALID_VALUE, if
+ *     - pxI2CPeripheral is NULL
+ *     - pxI2CPeripheral is not opened yet
+ *     - pucBuffer is NULL
+ *     - xBytes is 0
+ * - IOT_I2C_SLAVE_ADDRESS_NOT_SET, if slave address is not set yet
+ * - IOT_I2C_NACK or IOT_I2C_WRITE_FAILED, if
+ *     - no device correspond to the slave address
+ *     - the slave is unable to receive or transmit
+ *     - the slave gets data or commands that it does not understand
+ *     - there is some unknown driver error
+ * - IOT_I2C_BUS_TIMEOUT, if timeout is supported and slave device does not respond within configured timeout.
+ * - IOT_I2C_BUSY, if the bus is busy which means there is an ongoing transaction.
+ * - IOT_I2C_FUNCTION_NOT_SUPPORTED, if this board doesn't support this operation.
  */
 int32_t iot_i2c_write_async( IotI2CHandle_t const pxI2CPeripheral,
                              uint8_t * const pucBuffer,
@@ -273,9 +325,17 @@ int32_t iot_i2c_write_async( IotI2CHandle_t const pxI2CPeripheral,
  * @param[in] xI2CRequest Should be one of I2C_Ioctl_Request_t.
  * @param[in,out] pvBuffer The configuration values for the IOCTL request.
  *
- * @return SUCCESS = IOT_I2C_SUCCESS,
- *         FAILED = IOT_I2C_FUNCTION_NOT_SUPPORTED,
- *         BUSY = IOT_I2C_BUSY,
+ * @note SetMasterConfig is expected only called once at beginning.
+ * @note SetSlaveAddr is expected only called once at beginning.
+ * @note SendNoStopFlag is called at every operation you want to not send stop condition.
+ *
+ * @return
+ * - IOT_I2C_SUCCESS, on success
+ * - IOT_I2C_INVALID_VALUE, if
+ *     - pxI2CPeripheral is NULL
+ *     - pxI2CPeripheral is not opened yet
+ *     - pucBuffer is NULL with requests which needs buffer
+ * - IOT_I2C_FUNCTION_NOT_SUPPORTED, if this board doesn't support this operation.
  */
 int32_t iot_i2c_ioctl( IotI2CHandle_t const pxI2CPeripheral,
                        IotI2CIoctlRequest_t xI2CRequest,
@@ -287,20 +347,26 @@ int32_t iot_i2c_ioctl( IotI2CHandle_t const pxI2CPeripheral,
  *
  * @param[in] pxI2CPeripheral The I2C handle returned in open() call.
  *
- * @return SUCCESS = IOT_I2C_SUCCESS,
- *         FAILED = IOT_I2C_INVALID_VALUE,
+ * @return
+ * - IOT_I2C_SUCCESS, on success
+ * - IOT_I2C_INVALID_VALUE, if
+ *     - pxI2CPeripheral is NULL
+ *     - pxI2CPeripheral is not opened yet
  */
 int32_t iot_i2c_close( IotI2CHandle_t const pxI2CPeripheral );
 
 /**
- * @brief This function is used to cancel the current operation in progress
- * if possible.
+ * @brief This function is used to cancel the current operation in progress, if possible.
  *
  * @param[in] pxI2CPeripheral The I2C handle returned in open() call.
  *
- * @return SUCCESS = IOT_I2C_SUCCESS,
- *          FAILED = IOT_I2C_FUNCTION_INVALID_VALUE
- *          or IOT_I2C_FUNCTION_NOT_SUPPORTED, IOT_I2C_NOTHING_TO_CANCEL
+ * @return
+ * - IOT_I2C_SUCCESS, on success
+ * - IOT_I2C_INVALID_VALUE, if
+ *     - pxI2CPeripheral is NULL
+ *     - pxI2CPeripheral is not opened yet
+ * - IOT_I2C_NOTHING_TO_CANCEL, if there is no on-going transaction.
+ * - IOT_I2C_FUNCTION_NOT_SUPPORTED, if this board doesn't support this operation.
  */
 int32_t iot_i2c_cancel( IotI2CHandle_t const pxI2CPeripheral );
 
