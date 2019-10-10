ae193c3c9f5e0c918c64a9cf27242d31cd766c02
Pavan Madehalli Ranganath <rangpava@amazon.com>
2019-10-09 19:35:58 -0700

    Test CMake

---
+++ vendors/microchip/boards/secure_element/secure_element.cmake
@@ -0,0 +1,17 @@
+# -------------------------------------------------------------------------------------------------
+# Amazon FreeRTOS Console metadata
+# -------------------------------------------------------------------------------------------------
+afr_set_board_metadata(ID "ECC608-With-Windows-Simulator")
+afr_set_board_metadata(DISPLAY_NAME "ECC608 With Windows Simulator")
+afr_set_board_metadata(DESCRIPTION "ECC608 With Windows Simulator")
+afr_set_board_metadata(VENDOR_NAME "Microchip")
+afr_set_board_metadata(FAMILY_NAME "ECC608")
+afr_set_board_metadata(CODE_SIGNER "AmazonFreeRTOS-Default")
+afr_set_board_metadata(SUPPORTED_IDE "VisualStudio")
+afr_set_board_metadata(RECOMMENDED_IDE "VisualStudio")
+afr_set_board_metadata(IDE_VisualStudio_NAME "Visual Studio")
+afr_set_board_metadata(IDE_VisualStudio_COMPILER "MSVC")
+afr_set_board_metadata(IS_ACTIVE "TRUE")
+
+afr_set_board_metadata(IDE_VisualStudio_PROJECT_LOCATION "${AFR_ROOT_DIR}/projects/microchip/secure_element/visual_studio/aws_demos")
+afr_set_board_metadata(AWS_DEMOS_CONFIG_FILES_LOCATION "${CMAKE_CURRENT_LIST_DIR}/aws_demos/config_files")
