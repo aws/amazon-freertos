abdedbdb514f36eb51db9b7eecfc61d8f37600bb
Brian Franklin <43479547+brianfr-amazon@users.noreply.github.com>
2019-10-11 13:25:58 -0500

    docs: Improve documentation on iot_timer APIs (#1388)
    Improve the documentation on iot_timer APIs
    
    Why:  Need to clarify parameters going into the APIs and
    what the expected return values are for each.

--- libraries/abstractions/common_io/include/iot_timer.h
+++ libraries/abstractions/common_io/include/iot_timer.h
@@ -46,11 +46,11 @@
 /**
  * @brief Return values used by timer driver
  */
-#define IOT_TIMER_SUCCESS                   ( 0 )
-#define IOT_TIMER_INVALID_VALUE             ( 1 )
-#define IOT_TIMER_FUNCTION_NOT_SUPPORTED    ( 2 )
-#define IOT_TIMER_NOT_RUNNING               ( 3 )
-#define IOT_TIMER_SET_FAILED                ( 4 )
+#define IOT_TIMER_SUCCESS                   ( 0 )    /*!< Timer operation completed successfully. */
+#define IOT_TIMER_INVALID_VALUE             ( 1 )    /*!< At least one parameter is invalid. */
+#define IOT_TIMER_FUNCTION_NOT_SUPPORTED    ( 2 )    /*!< Timer operation not supported. */
+#define IOT_TIMER_NOT_RUNNING               ( 3 )    /*!< Timer not running. */
+#define IOT_TIMER_SET_FAILED                ( 4 )    /*!< Timer set failed. */
 
 /**
  * @brief   timer descriptor type defined in the source file.
@@ -78,7 +78,11 @@ typedef void ( * IotTimerCallback_t)( void * pvUserContext );
  *
  * @param[in]   lTimerInstance  instance of the timer to initialize.
  *
- * @return  Handle to IotTimerHandle_t on SUCCESS or NULL on failure.
+ * @return
+ *   - Handle to IotTimerHandle_t on SUCCESS
+ *   - NULL if
+ *      - lTimerInstance is invalid
+ *      - lTimerInstance is already open.
  */
 IotTimerHandle_t iot_timer_open( int32_t lTimerInstance );
 
@@ -86,6 +90,12 @@ IotTimerHandle_t iot_timer_open( int32_t lTimerInstance );
  * @brief   iot_timer_set_callback is used to set the callback to be called when
  *          the timer reaches the count (delay) set by the caller.
  *          Callers can set the delay using the iot_timer_delay API.
+ *
+ * @note Newly set callback overrides the one previously set
+ * @note This callback is per handle. Each instance has its own callback.
+ *
+ * @warning This function silently does nothing if either pxTimerHandle or
+ *          xCallback handle are NULL.
  *
  * @param[in]   pxTimerHandle   handle to Timer interface returned in
  *                              iot_timer_open()
@@ -104,8 +114,9 @@ void iot_timer_set_callback( IotTimerHandle_t const pxTimerHandle,
  * @param[in]   pxTimerHandle   handle to Timer interface returned in
  *                              iot_timer_open()
  *
- * @return  returns IOT_TIMER_SUCCESS on success or returns
- *          one of IOT_TIMER_INVALID_VALUE on error.
+ * @return
+ *   - IOT_TIMER_SUCCESS on success
+ *   - IOT_TIMER_INVALID_VALUE if pxTimerHandle is NULL
  */
 int32_t iot_timer_start( IotTimerHandle_t const pxTimerHandle );
 
@@ -117,21 +128,28 @@ int32_t iot_timer_start( IotTimerHandle_t const pxTimerHandle );
  *
  * @return  returns IOT_TIMER_SUCCESS on success or returns
  *          one of IOT_TIMER_INVALID_VALUE, IOT_TIMER_FUNCTION_NOT_SUPPORTED on error.
+ * @return
+ *   - IOT_TIMER_SUCCESS on success
+ *   - IOT_TIMER_INVALID_VALUE if pxTimerHandle is NULL.
+ *   - IOT_TIMER_FUNCTION_NOT_SUPPORTED, if the free running timer on SoC cant be stopped.
+ *   - IOT_TIMER_NOT_RUNNING if iot_timer_start has not been called.
  */
 int32_t iot_timer_stop( IotTimerHandle_t const pxTimerHandle );
 
 /*!
  * @brief   iot_timer_get_value is used to get the current timer value in micro seconds.
  *
- * @param[in]   pxTimerHandle   handle to Timer interface returned in
- *                              iot_timer_open()
- * @param[out]  ulMicroSeconds  current timer count in microseconds.
+ * @param[in]   pxTimerHandle    handle to Timer interface returned in
+ *                               iot_timer_open()
+ * @param[out]  ullMicroSeconds  current timer count in microseconds.
  *
- * @return  returns IOT_TIMER_SUCCESS on success or returns
- *          one of IOT_TIMER_INVALID_VALUE, IOT_TIMER_NOT_RUNNING on error.
+ * @return
+ *   - IOT_TIMER_SUCCESS on success
+ *   - IOT_TIMER_INVALID_VALUE if pxTimerHandle or ulMicroSeconds pointers are NULL
+ *   - IOT_TIMER_NOT_RUNNING if timer hasn't been started.
  */
 int32_t iot_timer_get_value( IotTimerHandle_t const pxTimerHandle,
-                             uint64_t * ulMicroSeconds );
+                             uint64_t * ullMicroSeconds );
 
 /*!
  * @brief   iot_timer_delay is used to set up a delay/wake-up time in microseconds.
@@ -145,8 +163,41 @@ int32_t iot_timer_get_value( IotTimerHandle_t const pxTimerHandle,
  *                                  iot_timer_open()
  * @param[in]   ulDelayMicroSeconds delay time in micro seconds
  *
- * @return  returns IOT_TIMER_SUCCESS on success or returns
- *          one of IOT_TIMER_INVALID_VALUE, IOT_TIMER_NOT_RUNNING, IOT_TIMER_SET_FAILED on error.
+ * <b>Example timer delay execution</b>
+ * The callback function is called to signal when the timer reaches the count (delay)
+ * set by the caller.
+ * @code{c}
+
+ * IotTimerHandle_t xTimerHandle;
+ *
+ * xTimerHandle = iot_timer_open( ltestIotTimerInstance);
+ * // assert( xTimerHandle == NULL );
+ *
+ * // Set the callback to call prvTimerCallbackFunction() when delay reached.
+ * iot_timer_set_callback(xTimerHandle, prvTimerCallbackFunction, NULL);
+ *
+ * // Set the timer delay to be TIMER_DEFAULT_DELAY_US
+ * lRetVal = iot_timer_delay(xTimerHandle, TIMER_DEFAULT_DELAY_US );
+ * // assert( lRetVal != IOT_TIMER_SUCCESS );
+ *
+ * //Start the timer
+ * lRetVal = iot_timer_start(xTimerHandle);
+ * // assert ( lRetVal != IOT_TIMER_SUCCESS);
+ *
+ * // Wait for the Delay callback to be called.  Inside of prvTimerCallbackFunction()
+ * // the function will use xSemaphoreGive() to signal completion.
+ * lRetVal = xSemaphoreTake(IotTimerSemaphore, portMAX_DELAY);
+ * // assert( lRetVal != TRUE );
+ *
+ * lRetVal = iot_timer_close(xTimerHandle);
+ * //assert ( lRetVal != IOT_TIMER_SUCCESS);
+ * @endcode
+ *
+ * @return
+ *   - IOT_TIMER_SUCCESS on success
+ *   - IOT_TIMER_INVALID_VALUE if pxTimerHandle or ulMicroSeconds pointers are NULL
+ *   - IOT_TIMER_NOT_RUNNING if timer hasn't been started.
+ *   - IOT_TIMER_SET_FAILED on error.
  */
 int32_t iot_timer_delay( IotTimerHandle_t const pxTimerHandle,
                          uint32_t ulDelayMicroSeconds );
@@ -159,9 +210,11 @@ int32_t iot_timer_delay( IotTimerHandle_t const pxTimerHandle,
  * @param[in]   pxTimerHandle   handle to Timer interface returned in
  *                              iot_timer_open()
  *
- * @return  returns IOT_TIMER_SUCCESS on success or returns
- *          one of IOT_TIMER_INVALID_VALUE on error
- *          or IOT_TIMER_FUNCTION_NOT_SUPPORTED, IOT_TIMER_NOTHING_TO_CANCEL
+ * @return
+ *   - IOT_TIMER_SUCCESS on success
+ *   - IOT_TIMER_INVALID_VALUE if pxTimerHandle is NULL
+ *   - IOT_TIMER_FUNCTION_NOT_SUPPORTED if timer can't be cancelled.
+ *   - IOT_TIMER_NOTHING_TO_CANCEL if there is no timer running to cancel.
  */
 int32_t iot_timer_cancel( IotTimerHandle_t const pxTimerHandle );
 
@@ -172,8 +225,11 @@ int32_t iot_timer_cancel( IotTimerHandle_t const pxTimerHandle );
  * @param[in]   pxTimerHandle   handle to Timer interface returned in
  *                              iot_timer_open()
  *
- * @return  returns IOT_TIMER_SUCCESS on success or returns
- *          one of IOT_TIMER_INVALID_VALUE on error.
+ * @return
+ *   - IOT_TIMER_SUCCESS on success
+ *   - IOT_TIMER_INVALID_VALUE if
+ *      - pxTimerHandle is NULL.
+ *      - pxTimerHandle not open (previously closed).
  */
 int32_t iot_timer_close( IotTimerHandle_t const pxTimerHandle );
 
