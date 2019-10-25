137f9b70e4de3d950fb23df1beae3c14daab327a
Alexa Noxon <33439348+alexa-noxon@users.noreply.github.com>
2019-10-24 18:12:45 -0700

    Move xInitializePkcs11Token from provisioning to iot_pkcs11.c
    Any PKCS #11 invocation that might be the first time
    a token is used requires C_InitializeToken.  Moving this
    to iot_pkcs11.c so that it is accessible to ulRand.

--- libraries/freertos_plus/standard/pkcs11/src/iot_pkcs11.c
+++ libraries/freertos_plus/standard/pkcs11/src/iot_pkcs11.c
@@ -141,6 +141,80 @@ CK_RV xInitializePKCS11( void )
 
 /*-----------------------------------------------------------*/
 
+/* Perform common token initialization as per the PKCS #11 standard. For
+ * compatibility reasons, this may include authentication with a static PIN. */
+CK_RV xInitializePkcs11Token( void )
+{
+    CK_RV xResult;
+
+    CK_FUNCTION_LIST_PTR pxFunctionList;
+    CK_SLOT_ID * pxSlotId = NULL;
+    CK_ULONG xSlotCount;
+    CK_FLAGS xTokenFlags = 0;
+    CK_TOKEN_INFO_PTR pxTokenInfo = NULL;
+
+    xResult = C_GetFunctionList( &pxFunctionList );
+
+    if( xResult == CKR_OK )
+    {
+        xResult = xInitializePKCS11();
+    }
+
+    if( ( xResult == CKR_OK ) || ( xResult == CKR_CRYPTOKI_ALREADY_INITIALIZED ) )
+    {
+        xResult = xGetSlotList( &pxSlotId, &xSlotCount );
+    }
+
+    if( ( xResult == CKR_OK ) &&
+        ( NULL != pxFunctionList->C_GetTokenInfo ) &&
+        ( NULL != pxFunctionList->C_InitToken ) )
+    {
+        /* Check if the token requires further initialization. */
+        pxTokenInfo = pvPortMalloc( sizeof( CK_TOKEN_INFO ) );
+
+        if( pxTokenInfo != NULL )
+        {
+            /* We will take the first slot available. If your application
+             * has multiple slots, insert logic for selecting an appropriate
+             * slot here.
+             */
+            xResult = pxFunctionList->C_GetTokenInfo( pxSlotId[ 0 ], pxTokenInfo );
+        }
+        else
+        {
+            xResult = CKR_HOST_MEMORY;
+        }
+
+        if( CKR_OK == xResult )
+        {
+            xTokenFlags = pxTokenInfo->flags;
+        }
+
+        if( ( CKR_OK == xResult ) && !( CKF_TOKEN_INITIALIZED & xTokenFlags ) )
+        {
+            /* Initialize the token if it is not already. */
+            xResult = pxFunctionList->C_InitToken( pxSlotId[ 0 ],
+                                                   ( CK_UTF8CHAR_PTR ) configPKCS11_DEFAULT_USER_PIN,
+                                                   sizeof( configPKCS11_DEFAULT_USER_PIN ) - 1,
+                                                   ( CK_UTF8CHAR_PTR ) "FreeRTOS" );
+        }
+    }
+
+    if( pxTokenInfo != NULL )
+    {
+        vPortFree( pxTokenInfo );
+    }
+
+    if( pxSlotId != NULL )
+    {
+        vPortFree( pxSlotId );
+    }
+
+    return xResult;
+}
+
+/*-----------------------------------------------------------*/
+
 CK_RV xInitializePkcs11Session( CK_SESSION_HANDLE * pxSession )
 {
     CK_RV xResult;
