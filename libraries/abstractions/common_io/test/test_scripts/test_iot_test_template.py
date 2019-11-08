6afb4985cfa83fb6c66c58bb2e1c0426faa5355e
cmcgcc <54688357+cmcgcc@users.noreply.github.com>
2019-11-08 09:15:58 -0800

    Add assisted tests for usb interrupt type device. (#1511)

--- libraries/abstractions/common_io/test/test_scripts/test_iot_test_template.py
+++ libraries/abstractions/common_io/test/test_scripts/test_iot_test_template.py
@@ -1,22 +1,22 @@
-# Amazon FreeRTOS V1.2.3
+# Amazon FreeRTOS Common IO V1.0.0
 # Copyright (C) 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 # http://aws.amazon.com/freertos
 # http://www.FreeRTOS.org
@@ -24,9 +24,11 @@
 
 import subprocess
 import os
+from abc import ABC, abstractmethod
 
 
-class test_template:
+class test_template(ABC):
+    @abstractmethod
     def __init__(self, serial, ip, login, pwd, csv_handler):
         self._func_list = []
 
@@ -40,8 +42,9 @@ class test_template:
     rpi_output_file = ''
 
     def auto_run(self):
-        print('hardware ready? (y/n)')
-        string = str(input())
+        # print('hardware ready? (y/n)')
+        # string = str(input())
+        string = 'y'
 
         if string == 'y':
             skip_test = False
@@ -54,7 +57,8 @@ class test_template:
                 self._cr.writerow({'test name': f.__name__, 'test result': 'skipped'})
         else:
             print("tests start")
-            subprocess.call([self.shell_script, self._ip, self._login, self._pwd, '-p'])
+            if self.shell_script:
+                subprocess.call([self.shell_script, self._ip, self._login, self._pwd, '-p'])
             for f in self._func_list:
                 result = f()
                 print(f.__name__, ": ", result)
@@ -63,6 +67,24 @@ class test_template:
     def run_shell_script(self, cmd):
         cmd_l = cmd.split()
         subprocess.call(cmd_l)
+
+    def compare_host_dut_result(self, hl, dl):
+        """
+        Compare host and dut bytes.
+        :param hl: a list of host bytes.
+        :param dl: a list of dut bytes.
+        :return: -1-unmatch 0-mathced
+        """
+        if len(hl) != len(dl) or len(hl) == 0:
+            print("Host and dut have different number of bytes.\nhost:", hl, "\ndut:", dl)
+            return -1
+
+        for h, d in zip(hl, dl):
+            if h != d:
+                print("Host and dut have different bytes.\nhost:", hl, "\ndut:", dl)
+                return -1
+
+        return 0
 
     def clean(self):
         if os.path.isfile(self.rpi_output_file):
