11767a5209c903c30ad55b5510fdb59ebe76de9d
qiutongs <songqt01@gmail,com>
2019-11-26 10:30:49 -0800

    Implement I2C, UART and SPI on ST board

---
+++ vendors/st/boards/stm32l475_discovery/ports/common_io/test_iot_internal.c
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * IOT On-Target Unit Test Internal - LPC54018
+ *
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *******************************************************************************
+ */
+
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "test_iot_internal.h"
+
+/* UART */
+extern uint8_t ustestIotUartPort;
+extern uint32_t ultestIotUartFlowControl;
+extern uint32_t ultestIotUartParity;
+extern uint32_t ultestIotUartWordLength;
+extern uint32_t ultestIotUartStopBits;
+
+void SET_TEST_IOT_UART_CONFIG( int testSet )
+{
+    ustestIotUartPort = uartTestPort[ testSet ];
+    ultestIotUartFlowControl = uartIotUartFlowControl[ testSet ];
+    ultestIotUartParity = uartIotUartParity[ testSet ];
+    ultestIotUartWordLength = uartIotUartWordLength[ testSet ];
+    ultestIotUartStopBits = uartIotUartStopBits[ testSet ];
+}
+
+/* I2C */
+extern uint8_t uctestIotI2CSlaveAddr;
+extern uint8_t xtestIotI2CDeviceRegister;
+extern uint8_t uctestIotI2CWriteVal;
+extern uint8_t uctestIotI2CInstanceIdx;
+extern uint8_t uctestIotI2CInstanceNum;
+
+void SET_TEST_IOT_I2C_CONFIG( int testSet )
+{
+    uctestIotI2CSlaveAddr = i2cTestSlaveAddr[ testSet ];
+    xtestIotI2CDeviceRegister = i2cTestDeviceRegister[ testSet ];
+    uctestIotI2CWriteVal = i2cTestWriteVal[ testSet ];
+    uctestIotI2CInstanceIdx = i2cTestInstanceIdx[ testSet ];
+    uctestIotI2CInstanceNum = i2cTestInstanceNum[ testSet ];
+}
