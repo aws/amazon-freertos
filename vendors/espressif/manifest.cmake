94bdacb0e913711d8ce5fc09d0f3debd21e06c49
Carl Lundin <lundinc@amazon.com>
2019-11-27 14:18:17 -0800

    Created a single cmake file.

--- vendors/espressif/manifest.cmake
+++ vendors/espressif/manifest.cmake
@@ -10,5 +10,5 @@ set(
 set(AFR_MANIFEST_BOARD_DIR "boards")
 set(AFR_MANIFEST_BOARD_DIR_esp32_devkitc "boards/esp32")
 set(AFR_MANIFEST_BOARD_DIR_esp32_wrover_kit "boards/esp32")
-set(AFR_MANIFEST_BOARD_DIR_esp32_plus_ecc608a_devkitc "boards/esp32_plus_ecc608a")
-set(AFR_MANIFEST_BOARD_DIR_esp32_plus_ecc608a_wrover_kit "boards/esp32_plus_ecc608a")
+set(AFR_MANIFEST_BOARD_DIR_esp32_plus_ecc608a_devkitc "boards/esp32")
+set(AFR_MANIFEST_BOARD_DIR_esp32_plus_ecc608a_wrover_kit "boards/esp32")
