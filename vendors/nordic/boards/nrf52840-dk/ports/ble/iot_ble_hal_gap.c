c645293c45ea52ce86c214e826cad59306d90719
Bhagavandas <bhagavar@amazon.com>
2019-12-11 09:45:34 -0800

    Add comment in nordic hal

--- vendors/nordic/boards/nrf52840-dk/ports/ble/iot_ble_hal_gap.c
+++ vendors/nordic/boards/nrf52840-dk/ports/ble/iot_ble_hal_gap.c
@@ -839,7 +839,11 @@ ret_code_t prvBTAdvDataConvert( ble_advdata_t * xAdvData,
             else
             {
                 /* extract company_identifier from input manufacturer data
-                 * because nordic sdk has separate field for company_identifier */
+                 * because nordic sdk has separate field for company_identifier.
+                 *
+                 * company identifier is two bytes in size stored in little endian form.
++                * Exctract the bytes and store into a 16bit variable.
++                */
                 for( uint8_t i = 0; i < iot_ble_hal_gapADVERTISING_COMPANY_ID_SIZE; i++ )
                 {
                     companyId = companyId | ( ( uint16_t ) pcManufacturerData[ i ] << ( i * 8 ) );
