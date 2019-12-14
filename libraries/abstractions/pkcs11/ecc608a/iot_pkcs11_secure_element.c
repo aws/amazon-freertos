b4ad21a805527731f01b7cc7ca36c84b959e9cd2
Carl Lundin <lundinc@amazon.com>
2019-12-13 16:14:51 -0800

    Added HID configuration to common ecc608 file.

--- libraries/abstractions/pkcs11/ecc608a/iot_pkcs11_secure_element.c
+++ libraries/abstractions/pkcs11/ecc608a/iot_pkcs11_secure_element.c
@@ -204,10 +204,6 @@ const char * pcPkcs11GetThingName(void)
     return gcPkcs11ThingNameCache;
 }
 
-/** \defgroup config Configuration (cfg_)
- * \brief Logical device configurations describe the CryptoAuth device type and logical interface.
-   @{ */
-
 /** \brief default configuration for an ECCx08A device */
 ATCAIfaceCfg cfg_ateccx08a_i2c_default = {
     .iface_type             = ATCA_I2C_IFACE,
@@ -218,6 +214,17 @@ ATCAIfaceCfg cfg_ateccx08a_i2c_default = {
     //.atcai2c.baud = 100000,
     .wake_delay             = 1500,
     .rx_retries             = 20
+};
+
+/** \brief default configuration for Kit protocol over the device's async interface */
+ATCAIfaceCfg cfg_ateccx08a_kithid_default = {
+    .iface_type         = ATCA_HID_IFACE,
+    .devtype            = ATECC508A,
+    .atcahid.idx        = 0,
+    .atcahid.vid        = 0x03EB,
+    .atcahid.pid        = 0x2312,
+    .atcahid.packetsize = 64,
+    .atcahid.guid       = { 0x4d,        0x1e, 0x55, 0xb2, 0xf1, 0x6f, 0x11, 0xcf, 0x88, 0xcb, 0x00, 0x11, 0x11, 0x00, 0x00, 0x30 },
 };
 
 #ifdef AMAZON_FREERTOS_ENABLE_UNIT_TESTS
