a9c0a6e23e989c40991d2b4e8b63e09aee6b26a0
Carl Lundin <lundinc@amazon.com>
2019-12-16 14:07:13 -0800

    Add check to only compile one ECC608A configuration.

--- libraries/abstractions/pkcs11/ecc608a/iot_pkcs11_secure_element.c
+++ libraries/abstractions/pkcs11/ecc608a/iot_pkcs11_secure_element.c
@@ -204,6 +204,7 @@ const char * pcPkcs11GetThingName(void)
     return gcPkcs11ThingNameCache;
 }
 
+#ifdef ATCA_HAL_I2c
 /** \brief default configuration for an ECCx08A device */
 ATCAIfaceCfg cfg_ateccx08a_i2c_default = {
     .iface_type             = ATCA_I2C_IFACE,
@@ -215,17 +216,20 @@ ATCAIfaceCfg cfg_ateccx08a_i2c_default = {
     .wake_delay             = 1500,
     .rx_retries             = 20
 };
+#endif
 
+#ifdef ATCA_HAL_KIT_HID
 /** \brief default configuration for Kit protocol over the device's async interface */
 ATCAIfaceCfg cfg_ateccx08a_kithid_default = {
-    .iface_type         = ATCA_HID_IFACE,
-    .devtype            = ATECC508A,
-    .atcahid.idx        = 0,
-    .atcahid.vid        = 0x03EB,
-    .atcahid.pid        = 0x2312,
+    .iface_type = ATCA_HID_IFACE,
+    .devtype = ATECC608A,
+    .atcahid.idx = 0,
+    .atcahid.vid = 0x03EB,
+    .atcahid.pid = 0x2312,
     .atcahid.packetsize = 64,
-    .atcahid.guid       = { 0x4d,        0x1e, 0x55, 0xb2, 0xf1, 0x6f, 0x11, 0xcf, 0x88, 0xcb, 0x00, 0x11, 0x11, 0x00, 0x00, 0x30 },
+    .atcahid.guid = { 0x4d, 0x1e, 0x55, 0xb2, 0xf1, 0x6f, 0x11, 0xcf, 0x88, 0xcb, 0x00, 0x11, 0x11, 0x00, 0x00, 0x30 },
 };
+#endif
 
 #ifdef AMAZON_FREERTOS_ENABLE_UNIT_TESTS
 /* This configuration is only suitable for testing of a device and does not enforce good 
