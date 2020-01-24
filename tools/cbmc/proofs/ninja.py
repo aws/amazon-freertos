032a58d3eee544c9bac2aba0b778d65bad49b1ff
Mark R. Tuttle <mrtuttle@amazon.com>
2020-01-24 12:36:06 -0500

    Add python script to build ninja build file to generate cbmc proofs.

---
+++ tools/cbmc/proofs/ninja.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+
+"""
+Write a ninja build file to generate reports for cbmc proofs.
+
+Given a list of folders containing cbmc proofs, write a ninja build
+file the generate reports for these proofs.  The list of folders may
+be given on the command line, in a json file, or found in the file
+system.
+"""
+
+# Add task pool
+
+import os
+import argparse
+import json
+
+################################################################
+# The command line parser
+
+def argument_parser():
+    """Return the command line parser."""
+
+    parser = argparse.ArgumentParser(
+        description='Generate ninja build file for cbmc proofs.',
+        epilog="""
+            Folders containing cbmc proofs may be specified on the command line,
+            in a file containing a json dict mapping the key "proofs" to a list of folders,
+            or in the file system as all folders under the current directory
+            containing a file named 'cbmc-batch.yaml'.
+        """
+    )
+    parser.add_argument('folders', metavar='PROOF', nargs='*',
+                        help='Folder containing a cbmc proof')
+    parser.add_argument('--proofs', metavar='JSON',
+                        help='Json file listing folders containing cbmc proofs')
+    return parser
+
+################################################################
+# The list of folders containing proofs
+#
+# The list of folders will be taken from
+# 1. the list PROOFS defined here or
+# 2. the command line
+# 3. the json file specified on the command line containing a
+#    dict mapping the key JSON_KEY to a list of folders
+# 4. the folders below the current directory containing
+#    a file named FS_KEY
+#
+
+PROOFS = [
+]
+
+JSON_KEY = 'proofs'
+FS_KEY = 'cbmc-batch.yaml'
+
+def find_proofs_in_json_file(filename):
+    """Read the list of folders containing proofs from a json file."""
+
+    if not filename:
+        return []
+    try:
+        with open(filename) as proofs:
+            return json.load(proofs)[JSON_KEY]
+    except (FileNotFoundError, KeyError):
+        raise UserWarning("Can't find key {} in json file {}".format(JSON_KEY, filename))
+    except json.decoder.JSONDecodeError:
+        raise UserWarning("Can't parse json file {}".format(filename))
+
+def find_proofs_in_filesystem():
+    """Construct the list of folders in the filesystem containing proofs."""
+
+    proofs = []
+    for (root, _, files) in os.walk('.'):
+        if FS_KEY in files:
+            proofs.append(os.path.normpath(root))
+    return proofs
+
+################################################################
+# The strings used to write sections of the ninja file
+
+NINJA_RULES = """
+################################################################
+# task pool to force sequential builds of goto binaries
+
+pool goto_pool
+  depth = 1
+
+################################################################
+# proof target rules
+
+rule build_goto
+  command = make -C ${{folder}} goto
+  pool = goto_pool
+
+rule build_cbmc
+  command = make -C ${{folder}} cbmc
+
+rule build_coverage
+  command = make -C ${{folder}} coverage
+
+rule build_property
+  command = make -C ${{folder}} property
+
+rule build_report
+  command = make -C ${{folder}} report
+
+################################################################
+# global target rules
+
+rule clean_proofs
+  command = for folder in {folders}; do make -C $${{folder}} clean; done
+
+rule veryclean_proofs
+  command = for folder in {folders}; do make -C $${{folder}} veryclean; done
+
+rule open_proofs
+  command = for folder in {folders}; do open $${{folder}}/html/index.html; done
+"""
+
+NINJA_BUILDS = """
+################################################################
+# {folder} proof targets
+
+build {folder}/{entry}.goto: build_goto
+  folder={folder}
+
+build {folder}/cbmc.txt: build_cbmc {folder}/{entry}.goto
+  folder={folder}
+
+build {folder}/coverage.xml: build_coverage {folder}/{entry}.goto
+  folder={folder}
+
+build {folder}/property.xml: build_property {folder}/{entry}.goto
+  folder={folder}
+
+build {folder}/html/index.html: build_report {folder}/{entry}.goto {folder}/cbmc.txt {folder}/coverage.xml {folder}/property.xml
+  folder={folder}
+
+build {folder}: phony {folder}/html/index.html
+
+default {folder}
+"""
+
+NINJA_GLOBALS = """
+################################################################
+# global targets
+
+build clean_: clean_proofs
+
+build veryclean_: veryclean_proofs
+
+build open: open_proofs
+"""
+
+################################################################
+# The main function
+
+def get_entry(folder):
+    """Find the name of the proof in the proof Makefile."""
+
+    with open('{}/Makefile'.format(folder)) as makefile:
+        for line in makefile:
+            if line.strip().lower().startswith('entry'):
+                return line[line.find('=')+1:].strip()
+            if line.strip().lower().startswith('h_entry'):
+                return line[line.find('=')+1:].strip()
+    raise UserWarning("Can't find ENTRY in {}/Makefile".format(folder))
+
+def write_ninja_build_file():
+    """Write a ninja build file to generate proof results."""
+
+    args = argument_parser().parse_args()
+
+    proofs = (PROOFS or
+              args.folders or
+              find_proofs_in_json_file(args.proofs) or
+              find_proofs_in_filesystem())
+
+    with open('build.ninja', 'w') as ninja:
+        ninja.write(NINJA_RULES.format(folders=' '.join(proofs)))
+        for proof in proofs:
+            entry = get_entry(proof)
+            ninja.write(NINJA_BUILDS.format(folder=proof, entry=entry))
+        ninja.write(NINJA_GLOBALS)
+
+################################################################
+
+if __name__ == "__main__":
+    write_ninja_build_file()
