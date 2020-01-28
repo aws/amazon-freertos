93fcdf2f89075d440306ede694682d3cfbe3cbc9
Alfred Gedeon <gedeonag@amazon.com>
2020-01-27 19:52:55 -0800

    fix: add junit style report

--- tools/cmock/coverage.cmake
+++ tools/cmock/coverage.cmake
@@ -16,10 +16,21 @@ cmake_minimum_required(VERSION 3.13)
             )
     message("output base_Coverage .info")
     file(GLOB files "${CMAKE_BINARY_DIR}/bin/*")
+
     foreach(testname ${files})
-    message("Running ${testname}")
-        execute_process(COMMAND ${testname})
+
+        get_filename_component(test
+                               ${testname}
+                               NAME_WLE
+                        )
+        message("Running ${testname}")
+        execute_process(COMMAND ${testname} OUTPUT_FILE ${CMAKE_BINARY_DIR}/${test}_out.txt)
+        execute_process(COMMAND ruby
+                ${CMAKE_SOURCE_DIR}/../libraries/3rdparty/CMock/vendor/unity/auto/parse_output.rb -xml ${CMAKE_BINARY_DIR}/${test}_out.txt
+                OUTPUT_FILE ${CMAKE_BINARY_DIR}/${test}_out.xml
+                )
     endforeach()
+
     execute_process(
                 COMMAND lcov --capture
                              -b ${CMAKE_BINARY_DIR}
