7d39372848e560c300fe05c3ed2787a4b2988766
Carl Lundin <lundinc@amazon.com>
2020-02-07 14:16:25 -0800

    Reduce Microchip C Runtime Heap Size and Increase FreeRTOS Heap Size
    This change is necessary because FreeRTOS based projects should not use the C runtime heap. Instead
    they should use the FreeRTOS managed Heap. I have changed the C runtime heap allocation from
    170KB to 10KB, and in turn increased the FreeRTOS heap size by 160KB.

--- vendors/microchip/boards/curiosity_pic32mzef/aws_demos/config_files/FreeRTOSConfig.h
+++ vendors/microchip/boards/curiosity_pic32mzef/aws_demos/config_files/FreeRTOSConfig.h
@@ -57,7 +57,7 @@
 #define configISR_STACK_SIZE                       ( 512 )
 #define configSUPPORT_DYNAMIC_ALLOCATION           1
 #define configSUPPORT_STATIC_ALLOCATION            1
-#define configTOTAL_HEAP_SIZE                      ( ( size_t ) 256000 )
+#define configTOTAL_HEAP_SIZE                      ( ( size_t ) 416000 )
 #define configMAX_TASK_NAME_LEN                    ( 16 )
 #define configUSE_16_BIT_TICKS                     0
 #define configIDLE_SHOULD_YIELD                    1
