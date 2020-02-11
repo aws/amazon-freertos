8d1d6dd90d92a21b9a08e9d1e794443e9f11669e
Navin Soni <navinns@amazon.com>
2020-02-11 15:16:01 -0800

    Update version number of libraries and tests

--- libraries/abstractions/pkcs11/mbedtls/threading_alt.h
+++ libraries/abstractions/pkcs11/mbedtls/threading_alt.h
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS PKCS #11 V2.0.2
+ * FreeRTOS PKCS #11 V2.0.3
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
--- libraries/abstractions/platform/test/iot_test_platform_clock.c
+++ libraries/abstractions/platform/test/iot_test_platform_clock.c
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS Platform V1.1.0
+ * FreeRTOS Platform V1.1.1
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
