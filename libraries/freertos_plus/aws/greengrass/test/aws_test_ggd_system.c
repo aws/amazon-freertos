375dbb64914e01ab01cdb44d218cd068f68e9569
yourslab <abrinao@amazon.com>
2020-04-10 15:19:34 -0700

    docs: Change wording of num retries macro comment

--- libraries/freertos_plus/aws/greengrass/test/aws_test_ggd_system.c
+++ libraries/freertos_plus/aws/greengrass/test/aws_test_ggd_system.c
@@ -48,7 +48,7 @@
 #endif
 
 /**
- * @brief The amount of times to retry a connection if it fails with 1 being a single attempt.
+ * @brief The number of times to try a connection. Values greater than 1 will retry on failure with an exponential backoff.
  */
 #ifndef IOT_TEST_GGD_CONNECT_RETRY_COUNT
     #define IOT_TEST_GGD_CONNECT_RETRY_COUNT    ( 1 )
