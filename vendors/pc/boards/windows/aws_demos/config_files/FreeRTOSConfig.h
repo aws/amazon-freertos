5be5c40534ffdd8d4f332b76bc89c388cd60a1da
Archit Aggarwal <architag@amazon.com>
2020-05-04 15:38:20 -0700

    Fix some low-hanging fruit MSVC warnings

--- vendors/pc/boards/windows/aws_demos/config_files/FreeRTOSConfig.h
+++ vendors/pc/boards/windows/aws_demos/config_files/FreeRTOSConfig.h
@@ -135,8 +135,7 @@ extern void vLoggingPrintf( const char * pcFormat,
 #define configPRINTF( X )    vLoggingPrintf X
 
 /* Non-format version thread-safe print. */
-extern void vLoggingPrint(const char* pcMessage);
-#define configPRINT_STRING( X )    vLoggingPrint( X )
+#define configPRINT_STRING( X )    vLoggingPrintf( X )
 
 /* Application specific definitions follow. **********************************/
 
