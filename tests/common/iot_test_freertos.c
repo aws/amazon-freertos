14cb2f8778276241a57944b3f319f43e4bec8f6a
Archit Aggarwal <architag@amazon.com>
2020-05-04 23:30:14 +0000

    Fix unused parameter warnings in Defender, FreeRTOS+TCP tests

--- tests/common/iot_test_freertos.c
+++ tests/common/iot_test_freertos.c
@@ -73,6 +73,10 @@
     void vApplicationStackOverflowHook( TaskHandle_t xTask,
                                         char * pcTaskName )
     {
+        /* Disable unused parameter warnings. */
+        ( void ) xTask;
+        ( void ) pcTaskName;
+
         configPRINT_STRING( ( "ERROR: stack overflow with task \r\n" ) );
         portDISABLE_INTERRUPTS();
 
