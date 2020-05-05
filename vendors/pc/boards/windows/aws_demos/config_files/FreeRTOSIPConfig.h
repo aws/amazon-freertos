100ffbe95f35b4aabca1e1ee58279a7d1f4ada9b
Hein Tibosch <hein@htibosch.net>
2020-05-05 22:01:43 +0800

    Reverted changes to window's FreeRTOSIPConfig.h. Didn't mean to push the into this PR.

--- vendors/pc/boards/windows/aws_demos/config_files/FreeRTOSIPConfig.h
+++ vendors/pc/boards/windows/aws_demos/config_files/FreeRTOSIPConfig.h
@@ -79,11 +79,8 @@ extern void vLoggingPrintf( const char * pcFormatString,
  * socket has been destroyed, the result will be stored into the cache.  The next
  * call to FreeRTOS_gethostbyname() will return immediately, without even creating
  * a socket. */
-#define ipconfigUSE_DNS_CACHE						( 1 )
-#define ipconfigDNS_REQUEST_ATTEMPTS				( 2 )
-
-#define ipconfigUSE_LLMNR							( 1 )
-#define ipconfigUSE_NBNS							( 1 )
+#define ipconfigUSE_DNS_CACHE                      ( 1 )
+#define ipconfigDNS_REQUEST_ATTEMPTS               ( 2 )
 
 /* The IP stack executes it its own task (although any application task can make
  * use of its services through the published sockets API). ipconfigUDP_TASK_PRIORITY
