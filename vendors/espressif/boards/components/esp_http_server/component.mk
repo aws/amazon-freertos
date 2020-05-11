cc5530d5f79f7f515f770a91f2eff62cf41a785c
Shubham Kulkarni <shubham.kulkarni@espressif.com>
2020-05-11 17:39:45 +0530

    Move port and extra components directory to a common location shared by all boards.
    Remove mbedTLS port files and use ESP-IDF provided files.
    
    Changes in CMakeLists for mbedTLS  to support ESP32-S2.

---
+++ vendors/espressif/boards/components/esp_http_server/component.mk
@@ -0,0 +1,2 @@
+# Nothing to do, esp_http_server component can not be supported with FreeRTOS TCP stack
+COMPONENT_CONFIG_ONLY := 1
---
+++ vendors/espressif/boards/components/protocomm/component.mk
@@ -0,0 +1,2 @@
+# Nothing to do, protocomm component can not be supported with FreeRTOS TCP stack
+COMPONENT_CONFIG_ONLY := 1
