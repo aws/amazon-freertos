0abf3b46114e933481f02bf22481564043de7766
Ming Yue <mingyue86010@gmail.com>
2020-05-15 15:31:29 -0700

    Fix cypress43 and cypress54 build error.

--- vendors/cypress/WICED_SDK/apps/test/aws_test/aws_test.mk
+++ vendors/cypress/WICED_SDK/apps/test/aws_test/aws_test.mk
@@ -157,7 +157,6 @@ $(NAME)_SOURCES    := $(AMAZON_FREERTOS_PATH)vendors/cypress/boards/$(PLATFORM)/
                       $(AFR_C_SDK_AWS_PATH)defender/test/unit/aws_iot_tests_defender_unit.c \
                       $(AFR_FREERTOS_PLUS_STANDARD_PATH)crypto/test/iot_test_crypto.c \
                       $(AMAZON_FREERTOS_PATH)demos/dev_mode_key_provisioning/src/aws_dev_mode_key_provisioning.c \
-                      $(AMAZON_FREERTOS_PATH)demos/dev_mode_key_provisioning/src/pem2der.c \
                       $(AFR_C_SDK_STANDARD_PATH)common/taskpool/iot_taskpool.c \
                       $(AFR_ABSTRACTIONS_PATH)platform/freertos/iot_clock_freertos.c \
                       $(AFR_ABSTRACTIONS_PATH)platform/freertos/iot_network_freertos.c \
