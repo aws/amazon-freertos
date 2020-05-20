dd2197b766a7d558a5729289d674f070307a6e69
Hein Tibosch <hein_tibosch@yahoo.es>
2020-05-20 08:26:31 +0800

    FreeRTOS+TCP after merging with the multi branch v2 (#2015)
    * Initial commit of 85 files
    
    * Removing tools/cbmc/windows2/Windows.h (capital W), in order to create windows.h (small w)
    
    * Now adding windows2/windows.h with a small 'w'
    
    * FreeRTOS_DNS.c : always decrease 'uxSourceBytesRemaining' after using up bytes
    
    * Forgot to add these two header files
    
    * Changes after Gary's review
    
    * Removed suppressions comments for both Coverity and PC-lint in most cases
    
    Co-authored-by: Hein Tibosch <hein@htibosch.net>

--- libraries/freertos_plus/standard/freertos_plus_tcp/include/NetworkInterface.h
+++ libraries/freertos_plus/standard/freertos_plus_tcp/include/NetworkInterface.h
@@ -30,10 +30,16 @@
 extern "C" {
 #endif
 
-/* NOTE PUBLIC API FUNCTIONS. */
+/* INTERNAL API FUNCTIONS. */
 BaseType_t xNetworkInterfaceInitialise( void );
 BaseType_t xNetworkInterfaceOutput( NetworkBufferDescriptor_t * const pxNetworkBuffer, BaseType_t xReleaseAfterSend );
+/* coverity[misra_c_2012_rule_8_6_violation] */
+/* "vNetworkInterfaceAllocateRAMToBuffers" is declared but never defined.
+The following function is only used when BufferAllocation_1.c is linked in the project. */
 void vNetworkInterfaceAllocateRAMToBuffers( NetworkBufferDescriptor_t pxNetworkBuffers[ ipconfigNUM_NETWORK_BUFFER_DESCRIPTORS ] );
+
+/* "xGetPhyLinkStatus" is provided by the network driver. */
+/* coverity[misra_c_2012_rule_8_6_violation] */
 BaseType_t xGetPhyLinkStatus( void );
 
 #ifdef __cplusplus
