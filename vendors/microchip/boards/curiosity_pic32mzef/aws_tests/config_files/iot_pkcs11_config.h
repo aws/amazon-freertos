2f50fbec7e511371132e80c2fae28cf33213f17e
Carl Lundin <lundinc@amazon.com>
2020-05-29 12:42:59 -0700

    Add ESP and Microchip.

--- vendors/microchip/boards/curiosity_pic32mzef/aws_tests/config_files/iot_pkcs11_config.h
+++ vendors/microchip/boards/curiosity_pic32mzef/aws_tests/config_files/iot_pkcs11_config.h
@@ -74,6 +74,12 @@
 #define pkcs11configMAX_SESSIONS                           10
 
 /**
+ * @brief Maximum number of sessions that can be stored
+ * by the PKCS #11 module.
+ */
+#define pkcs11configMAX_SESSIONS                           10
+
+/**
  * @brief Set to 1 if a PAL destroy object is implemented.
  *
  * If set to 0, no PAL destroy object is implemented, and this functionality
