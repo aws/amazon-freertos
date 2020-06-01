4cebd681fdbc0d9d6eafae3993d6a73674ade86b
Archit Aggarwal <architag@amazon.com>
2020-06-01 19:21:47 +0000

    Minor updates

--- libraries/freertos_plus/standard/freertos_plus_posix/test/iot_test_posix_utils.c
+++ libraries/freertos_plus/standard/freertos_plus_posix/test/iot_test_posix_utils.c
@@ -46,7 +46,7 @@
 
 /*-----------------------------------------------------------*/
 #define TIME_T_SIGNED_MAX      ( time_t ) ( ( ( time_t ) 1 << ( ( sizeof( time_t ) << 3 ) - 1 ) ) - 1 )
-#define TIME_T_UNSIGNED_MAX    ( time_t ) ( ( ( time_t ) 1 << ( ( sizeof( time_t ) << 3 ) ) ) - 1 )
+#define TIME_T_UNSIGNED_MAX    ( ( time_t ) ULLONG_MAX )
 
 static const struct timespec signedCheck = { .tv_sec = 1, .tv_nsec = 1 };
 static uint8_t isTimespecTvSecSigned;
