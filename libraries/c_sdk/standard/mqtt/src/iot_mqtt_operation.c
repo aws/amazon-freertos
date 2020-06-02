37afe52b9cdf5cdb57a1922e27f7c2b8d2031400
Muneeb Ahmed <ahmemune@amazon.com>
2020-06-02 15:31:26 -0700

    Fix MQTT unit test

--- libraries/c_sdk/standard/mqtt/src/iot_mqtt_operation.c
+++ libraries/c_sdk/standard/mqtt/src/iot_mqtt_operation.c
@@ -779,16 +779,10 @@ void _IotMqtt_ProcessKeepAlive( IotTaskPool_t pTaskPool,
              * and Call 3 is KeepAliveMS. */
             pMqttConnection->nextKeepAliveMs = pMqttConnection->keepAliveMs;
 
-            /* Check if keep alive time is nonzero and greater than wait time. */
-            if( pMqttConnection->keepAliveMs > IOT_MQTT_RESPONSE_WAIT_MS )
-            {
-                /* Subtract time taken for PINGRESP check. */
-                scheduleDelay = pMqttConnection->keepAliveMs - IOT_MQTT_RESPONSE_WAIT_MS;
-            }
-            else
-            {
-                EMPTY_ELSE_MARKER;
-            }
+            IotMqtt_Assert( pMqttConnection->keepAliveMs > IOT_MQTT_RESPONSE_WAIT_MS );
+
+            /* Subtract time taken for PINGRESP check. */
+            scheduleDelay = pMqttConnection->keepAliveMs - IOT_MQTT_RESPONSE_WAIT_MS;
         }
         else
         {
