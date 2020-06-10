22190c71d1aa6fd047c9f51094e5c6f4ce71c91b
Gaurav Aggarwal <aggarg@amazon.com>
2020-06-09 22:13:29 -0700

    Fix version number inconsistencies in license headers
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- libraries/c_sdk/standard/mqtt/test/mock/iot_tests_mqtt_mock.h
+++ libraries/c_sdk/standard/mqtt/test/mock/iot_tests_mqtt_mock.h
@@ -1,6 +1,6 @@
 /*
- * IoT MQTT V2.1.0
- * Copyright (C) 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ * FreeRTOS MQTT V2.2.0
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -18,6 +18,9 @@
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * http://aws.amazon.com/freertos
+ * http://www.FreeRTOS.org
  */
 
 /**
--- tests/unit_test/linux/config_files/iot_ble_config.h
+++ tests/unit_test/linux/config_files/iot_ble_config.h
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS V1.4.2
+ * FreeRTOS V202006.00
  * Copyright (C) 2018 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
