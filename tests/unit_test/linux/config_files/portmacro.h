a86f64350ab76a71ef019d0fd9f84c524066b39c
Archit Aggarwal <architag@amazon.com>
2020-06-25 22:41:48 +0000

    Removing formatting change

--- tests/unit_test/linux/config_files/portmacro.h
+++ tests/unit_test/linux/config_files/portmacro.h
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS V202007.00
+ * FreeRTOS V202006.00
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -240,7 +240,7 @@
     #define portTASK_FUNCTION( vFunction, pvParameters )          void vFunction( void * pvParameters )
 
     #ifdef __cplusplus
-}
+        }
     #endif
 
 #endif /* PORTMACRO_H */
