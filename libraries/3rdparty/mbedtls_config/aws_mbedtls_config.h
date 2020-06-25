103b1d76fdc9009ae988081e5f94720a93c0d034
Carl Lundin <53273776+lundinc2@users.noreply.github.com>
2020-06-25 13:24:49 -0700

    Renable encrypt then MAC for CBC now that CBC is renabled. (#2174)

--- libraries/3rdparty/mbedtls_config/aws_mbedtls_config.h
+++ libraries/3rdparty/mbedtls_config/aws_mbedtls_config.h
@@ -1343,7 +1343,7 @@
  *
  * Comment this macro to disable support for Encrypt-then-MAC
  */
-//#define MBEDTLS_SSL_ENCRYPT_THEN_MAC
+#define MBEDTLS_SSL_ENCRYPT_THEN_MAC
 
 /** \def MBEDTLS_SSL_EXTENDED_MASTER_SECRET
  *
