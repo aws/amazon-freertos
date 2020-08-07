f119392e38fd637d48585d0e7fc04f2e7a012315
gshvang <gshvang@amazon.com>
2020-08-07 15:38:07 -0700

    Updated headers

--- libraries/c_sdk/standard/mqtt/src/iot_mqtt_mutex_wrapper.c
+++ libraries/c_sdk/standard/mqtt/src/iot_mqtt_mutex_wrapper.c
@@ -19,7 +19,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
- * http://aws.amazon.com/freertos
+ * https://github.com/freertos
  * http ://www.FreeRTOS.org
  */
 
