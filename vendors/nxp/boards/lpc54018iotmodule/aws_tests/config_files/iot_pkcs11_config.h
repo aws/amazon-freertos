18c1db52978efa2ce361d5b9f5fbb2fdc298c1b5
Carl Lundin <lundinc@amazon.com>
2020-08-19 12:06:20 -0700

    Rename MQTT to PKCS #11 in config.

--- vendors/nxp/boards/lpc54018iotmodule/aws_tests/config_files/iot_pkcs11_config.h
+++ vendors/nxp/boards/lpc54018iotmodule/aws_tests/config_files/iot_pkcs11_config.h
@@ -38,8 +38,8 @@
 /* Include logging header files and define logging macros in the following order:
  * 1. Include the header file "logging_levels.h".
  * 2. Define the LIBRARY_LOG_NAME and LIBRARY_LOG_LEVEL macros depending on
- * the logging configuration for MQTT.
- * 3. Include the header file "logging_stack.h", if logging is enabled for MQTT.
+ * the logging configuration for PKCS #11.
+ * 3. Include the header file "logging_stack.h", if logging is enabled for PKCS #11.
  */
 #include "logging_levels.h"
 
