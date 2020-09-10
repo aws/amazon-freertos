2b5206c18ef171866ad5c488ea3cda24786f30c7
ravibhagavandas <bhagavar@amazon.com>
2020-09-10 10:47:07 -0700

    Wrapping included svc definition in flag, move to beginning

--- libraries/abstractions/ble_hal/test/src/iot_test_ble_hal_common.c
+++ libraries/abstractions/ble_hal/test/src/iot_test_ble_hal_common.c
@@ -106,6 +106,16 @@ static const BTAttribute_t pxAttributeTableB[] =
     {
         .xServiceUUID = bletestsFREERTOS_SVC_B_UUID
     },
+#if ENABLE_TC_AFQP_ADD_INCLUDED_SERVICE
+    {
+        .xAttributeType = eBTDbIncludedService,
+        .xIncludedService =
+        {
+            .xUuid          = bletestsFREERTOS_SVC_A_UUID,
+            .pxPtrToService = &_xSrvcA
+        }
+    },
+#endif
     {
         .xAttributeType = eBTDbCharacteristic,
         .xCharacteristic =
@@ -206,14 +216,6 @@ static const BTAttribute_t pxAttributeTableB[] =
         {
             .xUuid        = bletestsFREERTOS_DESCR_D_UUID,
             .xPermissions = ( eBTPermRead )
-        }
-    },
-    {
-        .xAttributeType = eBTDbIncludedService,
-        .xIncludedService =
-        {
-            .xUuid          = bletestsFREERTOS_SVC_A_UUID,
-            .pxPtrToService = &_xSrvcA
         }
     }
 };
