0d878784ce93958d732d10bed9a9b621f2a577de
DanielYEHsieh <wnf007z@gmail.com>
2020-09-23 05:01:38 +0800

    Update libraries/abstractions/transport/secure_sockets/transport_secure_sockets.c
    Co-authored-by: Archit Aggarwal <architag@amazon.com>

--- libraries/abstractions/transport/secure_sockets/transport_secure_sockets.c
+++ libraries/abstractions/transport/secure_sockets/transport_secure_sockets.c
@@ -488,7 +488,9 @@ int32_t SecureSocketsTransport_Disconnect( const NetworkContext_t * pNetworkCont
             /* Call Secure Sockets close function to close socket. */
             if( SOCKETS_Close( xSocket ) != ( int32_t ) SOCKETS_ERROR_NONE )
             {
-                LogError( ( "Failed to close socket." ) );
+                LogError( ( "Failed to close connection: SOCKETS_Close call failed." ) );
+                
+                xSocket = TRANSPORT_SOCKET_STATUS_INTERNAL_ERROR;
             }
             else
             {
