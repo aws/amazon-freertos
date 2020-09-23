e43cb66e523d724201c621ea8dee822f2c83f21f
Daniel (Yung-En) Hsieh <dhs@amazon.com>
2020-09-23 16:22:57 +0800

    Run uncrustify

--- libraries/abstractions/transport/secure_sockets/transport_secure_sockets.c
+++ libraries/abstractions/transport/secure_sockets/transport_secure_sockets.c
@@ -358,7 +358,7 @@ static TransportSocketStatus_t establishConnect( NetworkContext_t * pNetworkCont
 
         if( tcpSocket == ( Socket_t ) SOCKETS_INVALID_SOCKET )
         {
-             LogError( ( "Failed to create new socket. tcpSocket %d\n", tcpSocket ) );
+            LogError( ( "Failed to create new socket. tcpSocket %d\n", tcpSocket ) );
             returnStatus = TRANSPORT_SOCKET_STATUS_INSUFFICIENT_MEMORY;
         }
     }
