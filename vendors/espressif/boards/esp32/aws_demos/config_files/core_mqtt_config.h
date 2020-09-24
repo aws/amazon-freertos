fd858115561da5246592dfd7b2bed21f7c5bee6b
Lee George Thomas <ltham@amazon.com>
2020-09-24 16:59:00 -0700

    Uncrustify changes

--- vendors/espressif/boards/esp32/aws_demos/config_files/core_mqtt_config.h
+++ vendors/espressif/boards/esp32/aws_demos/config_files/core_mqtt_config.h
@@ -1,5 +1,4 @@
 /*
- * AWS IoT Device SDK for Embedded C V202009.00
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -21,7 +20,7 @@
  */
 
 #ifndef CORE_MQTT_CONFIG_H_
-    #define CORE_MQTT_CONFIG_H_
+#define CORE_MQTT_CONFIG_H_
 
 /**************************************************/
 /******* DO NOT CHANGE the following order ********/
@@ -34,18 +33,18 @@
  * 3. Include the header file "logging_stack.h", if logging is enabled for MQTT.
  */
 
-    #include "logging_levels.h"
+#include "logging_levels.h"
 
 /* Logging configuration for the MQTT library. */
-    #ifndef LIBRARY_LOG_NAME
-        #define LIBRARY_LOG_NAME    "MQTT"
-    #endif
+#ifndef LIBRARY_LOG_NAME
+    #define LIBRARY_LOG_NAME    "MQTT"
+#endif
 
-    #ifndef LIBRARY_LOG_LEVEL
-        #define LIBRARY_LOG_LEVEL    LOG_INFO
-    #endif
+#ifndef LIBRARY_LOG_LEVEL
+    #define LIBRARY_LOG_LEVEL    LOG_INFO
+#endif
 
-    #include "logging_stack.h"
+#include "logging_stack.h"
 
 /************ End of logging configuration ****************/
 
@@ -59,7 +58,7 @@
  * macro sets the limit on how many simultaneous PUBLISH states an MQTT
  * context maintains.
  */
-    #define MQTT_STATE_ARRAY_MAX_COUNT    ( 10U )
+#define MQTT_STATE_ARRAY_MAX_COUNT    ( 10U )
 
 /**
  * @brief Number of milliseconds to wait for a ping response to a ping
@@ -68,6 +67,6 @@
  * If a ping response is not received before this timeout, then
  * #MQTT_ProcessLoop will return #MQTTKeepAliveTimeout.
  */
-    #define MQTT_PINGRESP_TIMEOUT_MS      ( 500U )
+#define MQTT_PINGRESP_TIMEOUT_MS      ( 500U )
 
 #endif /* ifndef CORE_MQTT_CONFIG_H_ */
