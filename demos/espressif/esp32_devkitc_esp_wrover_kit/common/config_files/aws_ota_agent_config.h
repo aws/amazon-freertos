80eb03b86f3bb6d77109155993df4dfbb6b23f58
Prasad Vyawahare <pvyawaha@amazon.com>
2020-09-29 13:04:22 -0700

    OTA: Increase the request block timeout to 5000 msec in Espressif

--- demos/espressif/esp32_devkitc_esp_wrover_kit/common/config_files/aws_ota_agent_config.h
+++ demos/espressif/esp32_devkitc_esp_wrover_kit/common/config_files/aws_ota_agent_config.h
@@ -54,7 +54,7 @@
  * The wait timer is reset whenever a data block is received from the OTA service so we will only send
  * the request message after being idle for this amount of time.
  */
-#define otaconfigFILE_REQUEST_WAIT_MS           2500U
+#define otaconfigFILE_REQUEST_WAIT_MS           5000U
 
 /**
  * @brief The OTA agent task priority. Normally it runs at a low priority.
