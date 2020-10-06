9b6e823fdc8ae0a8a2fc75eb92c0b992cd92c781
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2020-10-06 13:45:18 -0700

    forward slash and freertos_plus_tcp

--- vendors/espressif/boards/esp32/components/freertos_tcpip/freertos_plus_tcp/component.mk
+++ vendors/espressif/boards/esp32/components/freertos_tcpip/freertos_plus_tcp/component.mk
@@ -3,13 +3,13 @@ AMAZON_FREERTOS_AFR_PLUS_DIR := ../../../../../../../libraries/freertos_plus
 AMAZON_FREERTOS_DEMOS_DIR := ../../../../../../../demos
 AMAZON_FREERTOS_3RD_PARTY_DIR := ../../../../../../../libraries/3rdparty
 
-COMPONENT_SRCDIRS := $(AMAZON_FREERTOS_AFR_PLUS_DIR)/standard/freertos_plus_tcp/source \
-                    $(AMAZON_FREERTOS_AFR_PLUS_DIR)/standard/freertos_plus_tcp/source/portable/BufferManagement \
-                    $(AMAZON_FREERTOS_AFR_PLUS_DIR)/standard/freertos_plus_tcp/source/portable/NetworkInterface/esp32 \
+COMPONENT_SRCDIRS := $(AMAZON_FREERTOS_AFR_PLUS_DIR)/standard/freertos_plus_tcp \
+                    $(AMAZON_FREERTOS_AFR_PLUS_DIR)/standard/freertos_plus_tcp/portable/BufferManagement \
+                    $(AMAZON_FREERTOS_AFR_PLUS_DIR)/standard/freertos_plus_tcp/portable/NetworkInterface/esp32 \
                     $(AMAZON_FREERTOS_DEMOS_DIR)/tcp \
 
 COMPONENT_ADD_INCLUDEDIRS := $(AMAZON_FREERTOS_AFR_PLUS_DIR)/standard/freertos_plus_tcp/include \
-                            $(AMAZON_FREERTOS_AFR_PLUS_DIR)/standard/freertos_plus_tcp/source/portable/Compiler/GCC \
+                            $(AMAZON_FREERTOS_AFR_PLUS_DIR)/standard/freertos_plus_tcp/portable/Compiler/GCC \
                             $(AMAZON_FREERTOS_DEMOS_DIR)/common/include
 
 ifdef AMAZON_FREERTOS_ENABLE_UNIT_TEST
@@ -18,5 +18,5 @@ COMPONENT_PRIV_INCLUDEDIRS += $(AMAZON_FREERTOS_3RD_PARTY_DIR)/unity/extras/fixt
 COMPONENT_ADD_INCLUDEDIRS += ${AMAZON_FREERTOS_AFR_PLUS_DIR}/standard/freertos_plus_tcp/test
 endif
 
-COMPONENT_OBJEXCLUDE := $(AMAZON_FREERTOS_AFR_PLUS_DIR)/standard/freertos_plus_tcp/source/portable/BufferManagement/BufferAllocation_1.o
+COMPONENT_OBJEXCLUDE := $(AMAZON_FREERTOS_AFR_PLUS_DIR)/standard/freertos_plus_tcp/portable/BufferManagement/BufferAllocation_1.o
 demos/tcp/aws_tcp_echo_client_single_task.o: CFLAGS+=-Wno-format
