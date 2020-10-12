3c860ce216254d8f3db6a486e81b65748f8cfe1d
leegeth <51681119+leegeth@users.noreply.github.com>
2020-10-12 15:22:32 -0700

    OTA e2e test fix for ESP32 (#2556)
    Handle Network interface failure in transport send

--- libraries/c_sdk/standard/mqtt/src/iot_mqtt_api.c
+++ libraries/c_sdk/standard/mqtt/src/iot_mqtt_api.c
@@ -896,9 +896,9 @@ static int32_t transportSend( const NetworkContext_t * pNetworkContext,
     /* Sending the bytes on the network using Network Interface. */
     bytesSend = pNetworkContext->pNetworkInterface->send( pNetworkContext->pNetworkConnection, ( const uint8_t * ) pMessage, bytesToSend );
 
-    if( bytesSend < 0 )
+    if( bytesSend <= 0 )
     {
-        /* Network Send Interface return negative value in case of any socket error,
+        /* Network Send Interface returns a negative value or zero in case of any socket error,
          * unifying the error codes here for socket error and timeout to comply with the MQTT LTS Library.
          */
         bytesSend = -1;
