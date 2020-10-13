9764efa31afc74acee6336980870424dadb4b460
Andy Sun <smn@amazon.com>
2020-10-13 16:45:11 +0800

    Fix comments.

--- tests/integration_test/shadow_system_test.c
+++ tests/integration_test/shadow_system_test.c
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS
+ * FreeRTOS V202010.00
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -114,12 +114,7 @@
 /**
  * @brief Length of the client identifier.
  */
-#define TEST_CLIENT_IDENTIFIER_LENGTH       ( sizeof( TEST_CLIENT_IDENTIFIER ) - 1u )
-
-/**
- * @brief Length of the client identifier.
- */
-#define TEST_CLIENT_IDENTIFIER_LENGTH       ( sizeof( TEST_CLIENT_IDENTIFIER ) - 1u )
+#define TEST_CLIENT_IDENTIFIER_LENGTH       ( sizeof( TEST_CLIENT_IDENTIFIER ) - 1U )
 
 /**
  * @brief Transport timeout in milliseconds for transport send and receive.
