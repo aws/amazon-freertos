3fdc79cd54ef689d885e97472d79501b02910eae
Sarena Meas <sarem@amazon.com>
2020-10-14 13:23:03 -0700

    Fix all licenses in the config files.

--- vendors/infineon/boards/xmc4800_iotkit/aws_tests/config_files/shadow_config.h
+++ vendors/infineon/boards/xmc4800_iotkit/aws_tests/config_files/shadow_config.h
@@ -1,5 +1,4 @@
 /*
- * AWS IoT Device SDK for Embedded C V202009.00
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
