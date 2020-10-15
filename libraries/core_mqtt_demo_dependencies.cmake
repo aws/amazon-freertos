d6f1133aaa1bdfba208c4549fb1d30121579c0e1
Archit Aggarwal <architag@amazon.com>
2020-10-15 20:05:03 +0000

    Address feedback

--- libraries/core_mqtt_demo_dependencies.cmake
+++ libraries/core_mqtt_demo_dependencies.cmake
@@ -1,12 +1,5 @@
+# core_mqtt module.
 afr_module(NAME core_mqtt )
-
-afr_set_lib_metadata(ID "core_mqtt_demo_dependencies")
-afr_set_lib_metadata(DESCRIPTION "This library implements the MQTT protocol that enables \
-communication with AWS IoT. MQTT is an ISO standard publish-subscribe-based messaging protocol.")
-afr_set_lib_metadata(DISPLAY_NAME "Core MQTT")
-afr_set_lib_metadata(CATEGORY "Connectivity")
-afr_set_lib_metadata(VERSION "1.0.0")
-afr_set_lib_metadata(IS_VISIBLE "true")
 
 # Include MQTT library's source and header path variables.
 include("${CMAKE_CURRENT_LIST_DIR}/coreMQTT/mqttFilePaths.cmake")
@@ -30,7 +23,6 @@ afr_module_sources(
         # List of files added to the target so that these are available
         # in code downloaded from the FreeRTOS console.
         ${CMAKE_CURRENT_LIST_DIR}/coreMQTT/mqttFilePaths.cmake
-        ${CMAKE_CURRENT_LIST_DIR}/core_mqtt_demo_dependencies.cmake
         ${MQTT_HEADER_FILES}
 )
 
@@ -46,7 +38,9 @@ afr_module_dependencies(
     PUBLIC
         AFR::logging
 )
+################################################################
 
+# core_mqtt_demo_dependencies module.
 # Metadata module used for the MQTT library in the FreeRTOS console.
 # It represents a collection of module dependencies required 
 # by the coreMQTT demos. 
@@ -54,6 +48,22 @@ afr_module_dependencies(
 # library dependencies when selecting the MQTT library, so that the
 # coreMQTT demos can be downloaded.
 afr_module(NAME core_mqtt_demo_dependencies )
+
+afr_set_lib_metadata(ID "core_mqtt_demo_dependencies")
+afr_set_lib_metadata(DESCRIPTION "This library implements the MQTT protocol that enables \
+communication with AWS IoT. MQTT is an ISO standard publish-subscribe-based messaging protocol.")
+afr_set_lib_metadata(DISPLAY_NAME "Core MQTT")
+afr_set_lib_metadata(CATEGORY "Connectivity")
+afr_set_lib_metadata(VERSION "1.0.0")
+afr_set_lib_metadata(IS_VISIBLE "true")
+
+afr_module_sources(
+    ${AFR_CURRENT_MODULE}
+    PRIVATE
+        # This file is added to the target so that it is available
+        # in code downloaded from the FreeRTOS console.
+        ${CMAKE_CURRENT_LIST_DIR}/core_mqtt_demo_dependencies.cmake
+)
 
 # Add dependencies of the coreMQTT demos in this target
 # to support metadata required for FreeRTOS console.
