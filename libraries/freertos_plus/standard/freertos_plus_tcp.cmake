f3f70eea7162248173e475a0a9913243f718b353
Archit Aggarwal <architag@amazon.com>
2020-10-15 22:45:24 +0000

    Address comments

--- libraries/freertos_plus/standard/freertos_plus_tcp.cmake
+++ libraries/freertos_plus/standard/freertos_plus_tcp.cmake
@@ -2,13 +2,13 @@ afr_module(NAME "freertos_plus_tcp" INTERNAL)
 
 set(src_dir "${CMAKE_CURRENT_LIST_DIR}/freertos_plus_tcp")
 set(inc_dir "${CMAKE_CURRENT_LIST_DIR}/freertos_plus_tcp/include")
-set(test_dir "${CMAKE_CURRENT_LIST_DIR}")
+set(test_dir "${AFR_TESTS_DIR}/integration_test")
 
 # Create a list of all FReeRTOS+TCP headers files in the FreeRTOS+TCP
 # library.
 # The list of header files will be added to metadata required
 # for the FreeRTOS console.
-file(GLOB_RECURSE INCLUDE_HEADER_FILES "${inc_dir}/*.h")
+file(GLOB FREERTOS_PLUS_TCP_HEADER_FILES "${inc_dir}/*.h")
 
 afr_module_sources(
     ${AFR_CURRENT_MODULE}
@@ -25,7 +25,7 @@ afr_module_sources(
         # List of files added to the target so that these are available
         # in code downloaded from the FreeRTOS console.
         ${CMAKE_CURRENT_LIST_DIR}/freertos_plus_tcp.cmake
-        ${INCLUDE_HEADER_FILES}
+        ${FREERTOS_PLUS_TCP_HEADER_FILES}
 )
 
 afr_module_include_dirs(
