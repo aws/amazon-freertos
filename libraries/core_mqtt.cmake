8455f2b8b0d050b7fa30daad33f0d0a0c2c1b080
Archit Aggarwal <architag@amazon.com>
2020-10-14 18:22:00 -0700

    Add header and cmake files to CMake configuration for FreeRTOS console support (#2574)

--- libraries/core_mqtt.cmake
+++ libraries/core_mqtt.cmake
@@ -11,24 +11,37 @@ afr_set_lib_metadata(IS_VISIBLE "true")
 # Include MQTT library's source and header path variables.
 include("${CMAKE_CURRENT_LIST_DIR}/coreMQTT/mqttFilePaths.cmake")
 
+# Create a list of all header files in the coreMQTT library.
+# The list of header files will be added to metadata required
+# for the FreeRTOS console.
+set(MQTT_HEADER_FILES "")
+foreach(mqtt_public_include_dir ${MQTT_INCLUDE_PUBLIC_DIRS})
+    file(GLOB mqtt_public_include_header_files
+              LIST_DIRECTORIES false
+              ${mqtt_public_include_dir}/* )
+    list(APPEND MQTT_HEADER_FILES ${mqtt_public_include_header_files})
+endforeach()
+
 afr_module_sources(
     ${AFR_CURRENT_MODULE}
     PRIVATE
         ${MQTT_SOURCES}
         ${MQTT_SERIALIZER_SOURCES}
+        # List of files added to the target so that these are available
+        # in code downloaded from the FreeRTOS console.
+        ${CMAKE_CURRENT_LIST_DIR}/core_mqtt.cmake
+        ${MQTT_HEADER_FILES}
 )
 
 afr_module_include_dirs(
     ${AFR_CURRENT_MODULE}
     PUBLIC
         ${MQTT_INCLUDE_PUBLIC_DIRS}
-    PRIVATE
-        ${MQTT_INCLUDE_PRIVATE_DIRS}
 )
 
 # Dependency of module on logging stack.
 afr_module_dependencies(
     ${AFR_CURRENT_MODULE}
     PUBLIC
-        AFR::common
+        AFR::logging
 )
