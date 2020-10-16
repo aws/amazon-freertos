d1600f3f944b9226659317ac1a9f44468a9b615a
Muneeb Ahmed <54290492+muneebahmed10@users.noreply.github.com>
2020-10-16 10:40:30 -0700

    Fix demo bug allowing infinite subscribe loop (#2587)
    * Reset flag in subscribe loop
    * Fix typo and a format specifier

--- demos/coreMQTT/mqtt_demo_mutual_auth.c
+++ demos/coreMQTT/mqtt_demo_mutual_auth.c
@@ -762,6 +762,9 @@ static BaseType_t prvMQTTSubscribeWithBackoffRetries( MQTTContext_t * pxMQTTCont
 
         if( xStatus == pdPASS )
         {
+            /* Reset flag before checking suback responses. */
+            xFailedSubscribeToTopic = false;
+
             /* Check if recent subscription request has been rejected. #xTopicFilterContext is updated
              * in the event callback to reflect the status of the SUBACK sent by the broker. It represents
              * either the QoS level granted by the server upon subscription, or acknowledgement of
