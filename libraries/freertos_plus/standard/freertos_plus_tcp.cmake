ad11ce93473bcdcca493bcc47aa887b740a84e2e
Archit Aggarwal <architag@amazon.com>
2020-10-16 17:14:35 +0000

    Minor improvement in test include path dependency

--- libraries/freertos_plus/standard/freertos_plus_tcp.cmake
+++ libraries/freertos_plus/standard/freertos_plus_tcp.cmake
@@ -2,7 +2,6 @@ afr_module(NAME "freertos_plus_tcp" INTERNAL)
 
 set(src_dir "${CMAKE_CURRENT_LIST_DIR}/freertos_plus_tcp")
 set(inc_dir "${CMAKE_CURRENT_LIST_DIR}/freertos_plus_tcp/include")
-set(test_dir "${AFR_TESTS_DIR}/integration_test")
 
 # Create a list of all FReeRTOS+TCP headers files in the FreeRTOS+TCP
 # library.
@@ -31,8 +30,15 @@ afr_module_sources(
 afr_module_include_dirs(
     ${AFR_CURRENT_MODULE}
     PUBLIC "${inc_dir}"
-    PRIVATE "${test_dir}"
 )
+
+if(AFR_ENABLE_TESTS)
+    afr_module_include_dirs(
+        ${AFR_CURRENT_MODULE}
+        PRIVATE
+            "${AFR_TESTS_DIR}/integration_test"
+    )
+endif()
 
 afr_module_dependencies(
     ${AFR_CURRENT_MODULE}
