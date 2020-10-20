210dcbf224eb51205a6194677e3e465ca94d87b9
Archit Aggarwal <architag@amazon.com>
2020-10-20 02:03:35 +0000

    Remove metadata of JSON library and CLI demo

--- libraries/core_json.cmake
+++ libraries/core_json.cmake
@@ -1,12 +1,5 @@
 afr_module( NAME core_json )
 
-afr_set_lib_metadata(ID "core_json")
-afr_set_lib_metadata(DESCRIPTION "This library is a parser that enforces the ECMA-404 JSON standard.")
-afr_set_lib_metadata(DISPLAY_NAME "Core JSON")
-afr_set_lib_metadata(CATEGORY "Tool")
-afr_set_lib_metadata(VERSION "1.0.0")
-afr_set_lib_metadata(IS_VISIBLE "false")
-
 # Include Json library's source and header path variables.
 include("${CMAKE_CURRENT_LIST_DIR}/coreJSON/jsonFilePaths.cmake")
 
