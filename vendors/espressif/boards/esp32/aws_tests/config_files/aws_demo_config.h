03bf734ca63d48ed145dc9f0b55383da3f6beada
Archit Aggarwal <architag@amazon.com>
2020-10-28 19:13:54 -0700

    Remove old MQTT and Shadow demos (#2628)
    * Remove old MQTT and Shadow demos
    
    * Run IDE generator for removal of demo files
    
    * Remove old demo files form Cypress, Nuvoton Ethernet and Nordic projects
    
    * Remove .cmake files from Mediatek and Nuvoton project files
    
    * Remove all occurrences of CONFIG_MQTT_DEMO_ENABLED and CONFIG_SHADOW_DEMO_ENABLED
    
    * Remove CMakeLists.txt from Mediatek and Nuvoton project files
    
    * Minor hygiene of using spaces instead of tabs
    
    * Test Nordic build by adding dependency of AFR::ota_mqtt on OTA demo
    
    * Revert "Test Nordic build by adding dependency of AFR::ota_mqtt on OTA demo"
    
    This reverts commit b61113db3c97aaf02323a87217a5eb67db95aab4.
    
    * Add direct dependency on AFR::mqtt for OTA demo

--- vendors/espressif/boards/esp32/aws_tests/config_files/aws_demo_config.h
+++ vendors/espressif/boards/esp32/aws_tests/config_files/aws_demo_config.h
@@ -29,8 +29,6 @@
 /* To run a particular demo you need to define one of these.
  * Only one demo can be configured at a time
  *
- *          CONFIG_MQTT_DEMO_ENABLED
- *          CONFIG_SHADOW_DEMO_ENABLED
  *          CONFIG_MQTT_BLE_DEMO_ENABLED
  *          CONFIG_GREENGRASS_DISCOVERY_DEMO_ENABLED
  *          CONFIG_TCP_ECHO_CLIENT_DEMO_ENABLED
@@ -42,7 +40,7 @@
  *
  *  These defines are used in iot_demo_runner.h for demo selection */
 
-#define CONFIG_MQTT_DEMO_ENABLED
+#define CONFIG_CORE_MQTT_MUTUAL_AUTH_DEMO_ENABLED
 
 /* Default configuration for all demos. Individual demos can override these below */
 #define democonfigDEMO_STACKSIZE                                     ( configMINIMAL_STACK_SIZE * 8 )
