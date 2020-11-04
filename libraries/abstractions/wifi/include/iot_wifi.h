6644ec6cde30d2a19a5305c2435eadcae00d30b1
Eric Yeh <ericyeh@amazon.com>
2020-11-04 11:35:32 -0800

    Updated library versions for 202011.00 release

--- libraries/abstractions/wifi/include/iot_wifi.h
+++ libraries/abstractions/wifi/include/iot_wifi.h
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS WiFi V1.0.6
+ * FreeRTOS WiFi V2.0.0
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
