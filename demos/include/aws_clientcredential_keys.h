604ebe38e4c82d1d962867caad304660a0897625
sbammi <36212270+sbammi@users.noreply.github.com>
2020-11-05 22:00:08 -0800

    Update aws_clientcredential_keys.h
    Add more info on use of security keys in demo projects.

--- demos/include/aws_clientcredential_keys.h
+++ demos/include/aws_clientcredential_keys.h
@@ -65,6 +65,8 @@
  *
  * @todo If you are running one of the FreeRTOS demo projects, set this
  * to the private key that will be used for TLS client authentication.
+ * Please note that this is for demo purposes ONLY and NOT for production
+ * scenarios. NEVER use a genuine secure key like this.
  *
  * @note Must include the PEM header and footer:
  * "-----BEGIN RSA PRIVATE KEY-----\n"\
