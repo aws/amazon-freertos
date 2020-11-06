70d16df3196586549f5db0f7ca45064dbd7f950f
Archit Aggarwal <architag@amazon.com>
2020-11-06 22:22:25 +0000

    Remove more configs of absent demos

--- demos/include/iot_demo_runner.h
+++ demos/include/iot_demo_runner.h
@@ -38,17 +38,7 @@
  */
 
 /* Individual demo task entry definitions */
-#if defined( CONFIG_CORE_MQTT_BASIC_TLS_DEMO_ENABLED )
-    #define DEMO_entryFUNCTION              RunCoreMqttBasicTLSDemo
-    #if defined( democonfigMQTT_ECHO_TASK_STACK_SIZE )
-        #undef democonfigDEMO_STACKSIZE
-        #define democonfigDEMO_STACKSIZE    democonfigMQTT_ECHO_TASK_STACK_SIZE
-    #endif
-    #if defined( democonfigMQTT_ECHO_TASK_PRIORITY )
-        #undef democonfigDEMO_PRIORITY
-        #define democonfigDEMO_PRIORITY     democonfigMQTT_ECHO_TASK_PRIORITY
-    #endif
-#elif defined( CONFIG_CORE_MQTT_MUTUAL_AUTH_DEMO_ENABLED )
+#if defined( CONFIG_CORE_MQTT_MUTUAL_AUTH_DEMO_ENABLED )
     #define DEMO_entryFUNCTION              RunCoreMqttMutualAuthDemo
     #if defined( democonfigMQTT_ECHO_TASK_STACK_SIZE )
         #undef democonfigDEMO_STACKSIZE
