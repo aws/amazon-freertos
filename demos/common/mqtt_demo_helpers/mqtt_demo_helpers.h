d60b91bb22d5591aba8e14da1fef0695b1c91ef3
Archit Aggarwal <architag@amazon.com>
2020-11-10 19:59:56 +0000

    Update documentation of mqtt helper functions for the extra parameters

--- demos/common/mqtt_demo_helpers/mqtt_demo_helpers.h
+++ demos/common/mqtt_demo_helpers/mqtt_demo_helpers.h
@@ -71,6 +71,12 @@
 /**
  * @brief Establish a MQTT connection.
  *
+ * @param[in] pxContext The memory for the MQTTContext_t that will be used for the
+ * MQTT connection.
+ * @param[in] pxNetContext The memory for the NetworkContext_t required for the
+ * MQTT connection.
+ * @param[in] pxNetworkBuffer The buffer space for initializing the @p pxContext MQTT
+ * context used in the MQTT connection.
  * @param[in] appCallback The callback function used to receive incoming
  * publishes and incoming acks from MQTT library.
  *
@@ -93,6 +99,8 @@ void vHandleOtherIncomingPacket( MQTTPacketInfo_t * pxPacketInfo,
 /**
  * @brief Close the MQTT connection.
  *
+ * @param[in] pxContext The MQTT context for the MQTT connection to close.
+ *
  * @return pdPASS if DISCONNECT was successfully sent;
  * pdFAIL otherwise.
  */
@@ -102,6 +110,7 @@ BaseType_t DisconnectMqttSession( MQTTContext_t * pxContext,
 /**
  * @brief Subscribe to a MQTT topic filter.
  *
+ * @param[in] pxContext The MQTT context for the MQTT connection.
  * @param[in] pcTopicFilter Pointer to the shadow topic buffer.
  * @param[in] usTopicFilterLength Indicates the length of the shadow
  * topic buffer.
@@ -117,6 +126,7 @@ BaseType_t SubscribeToTopic( MQTTContext_t * pxContext,
  * @brief Sends an MQTT UNSUBSCRIBE to unsubscribe from the shadow
  * topic.
  *
+ * @param[in] pxContext The MQTT context for the MQTT connection.
  * @param[in] pcTopicFilter Pointer to the shadow topic buffer.
  * @param[in] usTopicFilterLength Indicates the length of the shadow
  * topic buffer.
@@ -131,6 +141,7 @@ BaseType_t UnsubscribeFromTopic( MQTTContext_t * pxContext,
 /**
  * @brief Publish a message to a MQTT topic.
  *
+ * @param[in] pxContext The MQTT context for the MQTT connection.
  * @param[in] pcTopicFilter Points to the topic.
  * @param[in] topicFilterLength The length of the topic.
  * @param[in] pcPayload Points to the payload.
