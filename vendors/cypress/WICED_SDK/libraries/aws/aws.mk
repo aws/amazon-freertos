7e3a50f8793d38f93b8b5d52c0bbc871aa7a7828
Oscar Abrina <abrinao@amazon.com>
2020-11-15 15:55:39 -0800

    Update project files for all other boards

--- vendors/cypress/WICED_SDK/libraries/aws/aws.mk
+++ vendors/cypress/WICED_SDK/libraries/aws/aws.mk
@@ -136,7 +136,7 @@ $(NAME)_SOURCES :=  $(AFR_FREERTOS_PLUS_AWS_PATH)greengrass/src/aws_greengrass_d
                     $(AFR_LIBRARIES_PATH)coreJSON/source/core_json.c                                                \
                     $(AFR_LIBRARIES_PATH)device_shadow_for_aws/source/shadow.c                     \
                     $(AFR_LIBRARIES_PATH)coreHTTP/source/core_http_client.c                                         \
-                    $(AFR_LIBRARIES_PATH)coreHTTP/source/3rdparty/http_parser/http_parser.c                         \
+                    $(AFR_LIBRARIES_PATH)coreHTTP/source/dependency/3rdparty/http_parser/http_parser.c                         \
                     $(AFR_LIBRARIES_PATH)coreJSON/source/core_json.c                                                \
                     $(AFR_LIBRARIES_PATH)device_defender_for_aws/source/defender.c                                  \
                     $(AFR_LIBRARIES_PATH)jobs_for_aws/source/jobs.c                                                 \
@@ -167,7 +167,7 @@ GLOBAL_INCLUDES := $(AFR_C_SDK_STANDARD_PATH)mqtt/include \
                     $(AFR_LIBRARIES_PATH)device_shadow_for_aws/source/include \
                     $(AFR_LIBRARIES_PATH)coreHTTP/source/include \
                     $(AFR_LIBRARIES_PATH)coreHTTP/source/interface \
-                    $(AFR_LIBRARIES_PATH)coreHTTP/source/3rdparty/http_parser \
+                    $(AFR_LIBRARIES_PATH)coreHTTP/source/dependency/3rdparty/http_parser \
                     $(AFR_LIBRARIES_PATH)coreJSON/source/include \
                     $(AFR_LIBRARIES_PATH)device_defender_for_aws/source/include \
                     $(AFR_LIBRARIES_PATH)jobs_for_aws/source/include \
