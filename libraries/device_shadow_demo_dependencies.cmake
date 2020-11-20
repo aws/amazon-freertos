8a314d55769d25dedd279e24863054a3cd7329b1
Archit Aggarwal <architag@amazon.com>
2020-11-20 17:41:59 +0000

    Fix Build Combination failures of Device Shadow and Jobs demo builds

--- libraries/device_shadow_demo_dependencies.cmake
+++ libraries/device_shadow_demo_dependencies.cmake
@@ -17,7 +17,7 @@ endforeach()
 
 # Add cmake files of module to metadata.
 afr_module_cmake_files(${AFR_CURRENT_MODULE} 
-    ${CMAKE_CURRENT_LIST_DIR}/device_shadow_for_aws_iot_embedded_sdk/shadowFilePaths.cmake
+    ${CMAKE_CURRENT_LIST_DIR}/device_shadow_for_aws/shadowFilePaths.cmake
 )
 
 afr_module_sources(
@@ -82,6 +82,7 @@ afr_module_dependencies(
     PUBLIC
         AFR::device_shadow
         AFR::core_json
+        AFR::mqtt_demo_helpers
         # Add dependency on core_mqtt_demo_dependencies module 
         # so that coreMQTT library is auto-included when selecting
         # Device Shadow library on the FreeRTOS console.
