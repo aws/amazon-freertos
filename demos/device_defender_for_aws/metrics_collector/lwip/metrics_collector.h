bb4b835ed8b4479f59a40b77e591705855e37d69
Ming Yue <mingyue86010@gmail.com>
2020-11-20 20:52:23 -0800

    Fix Typos.

--- demos/device_defender_for_aws/metrics_collector/lwip/metrics_collector.h
+++ demos/device_defender_for_aws/metrics_collector/lwip/metrics_collector.h
@@ -8,7 +8,7 @@
  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
  * the Software, and to permit persons to whom the Software is furnished to do so,
  * subject to the following conditions:
- *ƒ
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
  *
