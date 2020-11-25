008ef966ea8e7b5870ece726638a89663b746789
Gaurav Aggarwal <aggarg@amazon.com>
2020-11-25 11:41:26 -0800

    Add defender demo to platforms using FreeRTOS+TCP
    Added platforms are:
    - Espressif
    - Microchip
    - Marvell
    - Renesas
    - Xilinx
    
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

--- vendors/marvell/boards/mw300_rd/aws_demos/config_files/aws_demo_config.h
+++ vendors/marvell/boards/mw300_rd/aws_demos/config_files/aws_demo_config.h
@@ -44,6 +44,7 @@
  *          CONFIG_HTTPS_ASYNC_DOWNLOAD_DEMO_ENABLED
  *          CONFIG_HTTPS_SYNC_UPLOAD_DEMO_ENABLED
  *          CONFIG_HTTPS_ASYNC_UPLOAD_DEMO_ENABLED
+ *          CONFIG_DEVICE_DEFENDER_DEMO_ENABLED
  *
  *  These defines are used in iot_demo_runner.h for demo selection */
 
