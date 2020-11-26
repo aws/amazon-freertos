c79f25f699d9167e20504cffa8a852fbb4f3b5b5
Archit Aggarwal <architag@amazon.com>
2020-11-26 00:12:33 +0000

    Update dependency on AFR::pkcs11_helpers to be conditional for MQTT demo wrapper module

--- libraries/core_mqtt_demo_dependencies.cmake
+++ libraries/core_mqtt_demo_dependencies.cmake
@@ -83,6 +83,17 @@ afr_module_dependencies(
         AFR::pkcs11_helpers
 )
 
+# Add dependency on PKCS11 Helpers module, that is required
+# by the Secure Sockets based coreMQTT demo, ONLY if the board
+# supports the PKCS11 module.
+if(TARGET AFR::pkcs11_implementation::mcu_port)
+    afr_module_dependencies(
+        ${AFR_CURRENT_MODULE}
+        PUBLIC
+            AFR::pkcs11_helpers
+    )
+endif()
+
 # Add more dependencies for Secure Sockets based MQTT demo 
 # (at demos/coreMQTT folder) ONLY if the board supports 
 # the Secure Sockets library.
