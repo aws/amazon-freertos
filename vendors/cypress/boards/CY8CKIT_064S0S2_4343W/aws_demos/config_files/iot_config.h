bc5013a709af4b6010494d93523df2613a497e5b
Oscar Abrina <abrinao@amazon.com>
2020-12-01 17:30:17 -0800

    Fix comments

--- vendors/cypress/boards/CY8CKIT_064S0S2_4343W/aws_demos/config_files/iot_config.h
+++ vendors/cypress/boards/CY8CKIT_064S0S2_4343W/aws_demos/config_files/iot_config.h
@@ -60,7 +60,7 @@
 /* Platform thread priority. */
 #define IOT_THREAD_DEFAULT_PRIORITY             ( 5 )
 
-/* Platform stack size for HTTPS library. */
+/* Dispatch task and queue configurations for HTTPS library. */
 #define IOT_HTTPS_DISPATCH_TASK_COUNT           1
 #define IOT_HTTPS_DISPATCH_QUEUE_SIZE           2
 #define IOT_HTTPS_DISPATCH_USE_STATIC_MEMORY    1
