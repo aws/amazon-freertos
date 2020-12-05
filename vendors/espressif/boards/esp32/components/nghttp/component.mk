9533fd5a4f66f88f81ff695568b8ec53f878c6f4
David Chalco <david@chalco.io>
2020-12-04 17:27:11 -0800

    must override to keep http_parser struct interface consistent
    cleanup and refactoring
    
    handle more failure modes
    
    update relocated path
    
    allow protocomm for lwip
    
    tmp

--- vendors/espressif/boards/esp32/components/nghttp/component.mk
+++ vendors/espressif/boards/esp32/components/nghttp/component.mk
@@ -1,2 +1,9 @@
-# Nothing to be done here
-COMPONENT_CONFIG_ONLY := 1
+#
+# Component Makefile
+#
+
+AMAZON_FREERTOS_LIB_DIR := ../../../../../../libraries
+
+# Keep everything the same, EXCEPT prefer coreHTTP http_parser  
+COMPONENT_ADD_INCLUDEDIRS := ${AMAZON_FREERTOS_LIB_DIR}/libraries/coreHTTP/source/dependency/3rdparty/http_parser port/include nghttp2/lib/includes
+COMPONENT_SRCDIRS := ${AMAZON_FREERTOS_LIB_DIR}/libraries/coreHTTP/source/dependency/3rdparty/http_parser nghttp2/lib port
--- vendors/espressif/boards/esp32/components/protocomm/component.mk
+++
@@ -1,2 +0,0 @@
-# Nothing to do, protocomm component can not be supported with FreeRTOS TCP stack
-COMPONENT_CONFIG_ONLY := 1
