ab7f38b20931521037fbdc638bf688dc2259b4d8
David Chalco <david@chalco.io>
2020-12-07 14:07:59 -0800

    stop override esp_http_server and enable std version

--- vendors/microchip/boards/curiosity_pic32mzef/aws_demos/config_files/aws_demo_config.h
+++ vendors/microchip/boards/curiosity_pic32mzef/aws_demos/config_files/aws_demo_config.h
@@ -83,6 +83,4 @@
     #endif /* if( INCLUDE_uxTaskGetStackHighWaterMark == 1 ) */
 #endif /* democonfigMEMORY_ANALYSIS */
 
-#define IOT_WIFI_ENABLE_SOFTAP_PROVISIONING 1
-
 #endif /* _AWS_DEMO_CONFIG_H_ */
