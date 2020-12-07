50bd5045cddb0477ddaf564de843f9289cc01912
David Chalco <david@chalco.io>
2020-12-07 14:07:59 -0800

    stop override esp_http_server and enable std version

--- vendors/espressif/boards/components/nghttp/component.mk
+++ vendors/espressif/boards/components/nghttp/component.mk
@@ -5,5 +5,5 @@
 AMAZON_FREERTOS_LIB_DIR := ../../../../../../libraries
 
 # Keep everything the same, EXCEPT prefer coreHTTP http_parser  
-COMPONENT_ADD_INCLUDEDIRS := ${AMAZON_FREERTOS_LIB_DIR}/libraries/coreHTTP/source/dependency/3rdparty/http_parser port/include nghttp2/lib/includes
-COMPONENT_SRCDIRS := ${AMAZON_FREERTOS_LIB_DIR}/libraries/coreHTTP/source/dependency/3rdparty/http_parser nghttp2/lib port
+COMPONENT_ADD_INCLUDEDIRS := ${AMAZON_FREERTOS_LIB_DIR}/libraries/coreHTTP/source/dependency/3rdparty/http_parser
+COMPONENT_SRCDIRS := ${AMAZON_FREERTOS_LIB_DIR}/libraries/coreHTTP/source/dependency/3rdparty/http_parser 
