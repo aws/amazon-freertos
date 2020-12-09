531bda549acfccf1f1693e203745f6c1cee3d7d7
Pavan Madehalli Ranganath <rangpava@amazon.com>
2020-12-08 16:43:56 -0800

    fix secure sockets versioning

--- libraries/abstractions/transport/secure_sockets/transport_secure_sockets.c
+++ libraries/abstractions/transport/secure_sockets/transport_secure_sockets.c
@@ -1,5 +1,5 @@
 /*
- * FreeRTOS V202002.00
+ * FreeRTOS Transport Secure Sockets V1.0.0
  * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
