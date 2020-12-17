a57b63649e75ba9ac5f669386be58e6382e65962
DanielYEHsieh <wnf007z@example.com>
2020-12-17 11:38:44 +0800

    replace FreeRTOS-Plus/Source/Application-Protocols/network_transport. to amazon-freertos/libraries/abstractions/transport/secure_sockets/transport_secure_sockets.h and amazon-freertos/libraries/c_sdk/standard/ble/include/iot_ble_mqtt_transport.h
    replace amazon-freertos/libraries/c_sdk/standard/ble/include/iot_ble_mqtt_transport.h

--- tests/integration_test/shadow_system_test.c
+++ tests/integration_test/shadow_system_test.c
@@ -204,7 +204,7 @@
  * It should contain a single pointer to the type of your desired transport.
  * When using multiple transports in the same compilation unit, define this pointer as void *.
  *
- * @note Transport stacks are defined in FreeRTOS-Plus/Source/Application-Protocols/network_transport.
+ * @note Transport stacks are defined in amazon-freertos/libraries/abstractions/transport/secure_sockets/transport_secure_sockets.h.
  */
 struct NetworkContext
 {
