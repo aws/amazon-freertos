605762504ce48ddcdf14970a5c38f29da3187f61
Archit Aggarwal <architag@amazon.com>
2021-02-09 00:11:22 +0000

    doc: minor changes in comments for LOGGING_ENABLE_METADATA_WITH_C99_AND_GNU_EXTENSION macro

--- vendors/espressif/boards/esp32/aws_demos/config_files/FreeRTOSConfig.h
+++ vendors/espressif/boards/esp32/aws_demos/config_files/FreeRTOSConfig.h
@@ -422,12 +422,12 @@
     #define UNTESTED_FUNCTION()
 #endif
 
-/* This config enables extra verbosity in log messages with metadata information
- * about the source library and location of the log message.
- * This requires that the toolchain support C99 (for variadic macros) and the GNU
- * extension for comma elision in variadic macros (with ##__VA_ARGS__).
- * If this flag is enabled, you can change the metadata information from the logging_stack.h
- * file.*/
+/* This config enables extra metadata information of the source library and location
+ * in log messages.
+ * This config requires the toolchain to support ISO C99 and the GNU extension (for comma 
+ * elision in variadic macros i.e. with ##__VA_ARGS__).
+ * When using this flag, you can change the format or information of log messages in
+ * the logging_stack.h file.*/
 #define LOGGING_ENABLE_METADATA_WITH_C99_AND_GNU_EXTENSION    ( 1 )
 
 #endif /* #define FREERTOS_CONFIG_H */
