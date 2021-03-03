ec5afdc980abd7974769502887a73ca2be4c20a2
Carl Lundin <lundinc@amazon.com>
2021-03-03 14:51:58 -0800

    Uncrustify.

--- libraries/freertos_plus/standard/tls/src/iot_tls.c
+++ libraries/freertos_plus/standard/tls/src/iot_tls.c
@@ -908,6 +908,7 @@ BaseType_t TLS_Connect( void * pvContext )
     }
 
     #ifdef MBEDTLS_DEBUG_C
+
         /* If mbedTLS is being compiled with debug support, assume that the
          * runtime configuration should use verbose output. */
         mbedtls_ssl_conf_dbg( &pxCtx->xMbedSslConfig, prvTlsDebugPrint, NULL );
