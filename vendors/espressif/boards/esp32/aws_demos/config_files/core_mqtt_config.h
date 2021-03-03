3b33655325288011a4766c45c00e1f3e494a8fa3
Shubham Divekar <divekarshubham@gmail.com>
2021-03-02 17:20:42 -0800

    updating config files for esp32

--- vendors/espressif/boards/esp32/aws_demos/config_files/core_mqtt_config.h
+++ vendors/espressif/boards/esp32/aws_demos/config_files/core_mqtt_config.h
@@ -69,4 +69,54 @@
  */
 #define MQTT_PINGRESP_TIMEOUT_MS      ( 5000U )
 
+
+
+/*TODO: To document and add to the mqtt config defaults header file. */
+#define MQTT_AGENT_COMMAND_QUEUE_LENGTH     ( 25 )
+#define MQTT_COMMAND_CONTEXTS_POOL_SIZE     ( 10 )
+
+/**
+ * @brief The maximum number of subscriptions to track for a single connection.
+ *
+ * @note The MQTT agent keeps a record of all existing MQTT subscriptions.
+ * MQTT_AGENT_MAX_SIMULTANEOUS_SUBSCRIPTIONS sets the maximum number of
+ * subscriptions records that can be maintained at one time.  The higher this
+ * number is the greater the agent's RAM consumption will be.
+ */
+#define MQTT_AGENT_MAX_SIMULTANEOUS_SUBSCRIPTIONS    ( 10 )
+
+/**
+ * @brief Size of statically allocated buffers for holding subscription filters.
+ *
+ * @note Subscription filters are strings such as "/my/topicname/#".  These
+ * strings are limited to a maximum of MQTT_AGENT_MAX_SUBSCRIPTION_FILTER_LENGTH
+ * characters. The higher this number is the greater the agent's RAM consumption
+ * will be.
+ */
+#define MQTT_AGENT_MAX_SUBSCRIPTION_FILTER_LENGTH ( 100 )
+
+/**
+ * @brief Dimensions the buffer used to serialise and deserialise MQTT packets.
+ * @note Specified in bytes.  Must be large enough to hold the maximum
+ * anticipated MQTT payload.
+ */
+#define MQTT_AGENT_NETWORK_BUFFER_SIZE    ( 5000 )
+
+/**
+ * @breif Timeout for which MQTT library keeps polling the transport interface,
+ * when no byte is received.
+ * The timeout is honoured only after the first byte is read and while remaining bytes
+ * are read from network interface. Keeping this timeout to a sufficiently large value so
+ * as to account for delay of receipt of a large block of message.
+ */
+#define MQTT_RECV_POLLING_TIMEOUT_MS  ( 1000U )
+
+ /**
+  * @breif Maximum time MQTT agent waits in the queue for any pending MQTT operations.
+  * The wait time is kept smallest possible to increase the responsiveness of MQTT agent
+  * while processing  pending MQTT operations as well as receive packets from network.
+  */
+#define MQTT_AGENT_MAX_EVENT_QUEUE_WAIT_TIME ( 1U )
+
+
 #endif /* ifndef CORE_MQTT_CONFIG_H_ */
