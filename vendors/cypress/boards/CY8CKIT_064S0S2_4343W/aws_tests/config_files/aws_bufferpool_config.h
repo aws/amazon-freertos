e6832fb1c534a41eeafde140a159c227e2ac93f4
Fyall Ian (CYSC CSS ICW SW T 1) <Ian.Fyall@infineon.com>
2021-03-07 20:05:18 -0800

    Update 062 boards for PKCS11 changes from Amazon master. Update demo defaults for 062 boards from Amazon master. Regenerate GeneratedSource.

--- vendors/cypress/boards/CY8CKIT_064S0S2_4343W/aws_tests/config_files/aws_bufferpool_config.h
+++ vendors/cypress/boards/CY8CKIT_064S0S2_4343W/aws_tests/config_files/aws_bufferpool_config.h
@@ -1,5 +1,5 @@
 /*
- * Amazon FreeRTOS V1.4.8
+ * Amazon FreeRTOS V1.1.4
  * Copyright (C) 2017 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -39,6 +39,6 @@
 /**
  * @brief The size of each buffer in the static buffer pool.
  */
-#define bufferpoolconfigBUFFER_SIZE    ( 512 )
+#define bufferpoolconfigBUFFER_SIZE    ( 1024 )
 
 #endif /* _AWS_BUFFER_POOL_CONFIG_H_ */
