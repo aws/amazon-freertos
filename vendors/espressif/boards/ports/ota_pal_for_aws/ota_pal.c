35a40674408d54a3d8c47bde426aa8891cf8ffc3
Shubham Divekar <divekarshubham@gmail.com>
2021-03-10 11:42:15 -0800

    Removing configs from ota_pal

--- vendors/espressif/boards/ports/ota_pal_for_aws/ota_pal.c
+++ vendors/espressif/boards/ports/ota_pal_for_aws/ota_pal.c
@@ -25,17 +25,6 @@
 #include "ota_pal.h"
 #include "ota_interface_private.h"
 
-/* OTA_DO_NOT_USE_CUSTOM_CONFIG allows building the OTA library
- * without a custom config. If a custom config is provided, the
- * OTA_DO_NOT_USE_CUSTOM_CONFIG macro should not be defined. */
-#ifndef OTA_DO_NOT_USE_CUSTOM_CONFIG
-    #include "ota_config.h"
-#endif
-
-/* Include config defaults header to get default values of configs not defined
- * in ota_config.h file. */
-#include "ota_config_defaults.h"
-
 #include "types/iot_network_types.h"
 #include "aws_iot_network_config.h"
 #include "iot_crypto.h"
