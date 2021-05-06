73fbd4ea2e52259b602641978ac51b32888418ec
Shubham Divekar <divekarshubham@gmail.com>
2021-05-06 23:33:18 +0000

    Updating logging configs

--- tests/integration_test/ota_pal/aws_test_ota_pal.c
+++ tests/integration_test/ota_pal/aws_test_ota_pal.c
@@ -52,7 +52,7 @@
 #endif
 
 #ifndef LIBRARY_LOG_LEVEL
-    #define LIBRARY_LOG_LEVEL    LOG_INFO
+    #define LIBRARY_LOG_LEVEL    LOG_DEBUG
 #endif
 #include "logging_stack.h"
 
--- vendors/microchip/boards/ecc608a_plus_winsim/aws_tests/config_files/ota_config.h
+++ vendors/microchip/boards/ecc608a_plus_winsim/aws_tests/config_files/ota_config.h
@@ -31,6 +31,31 @@
 #ifndef OTA_CONFIG_H_
 #define OTA_CONFIG_H_
 
+/**************************************************/
+/******* DO NOT CHANGE the following order ********/
+/**************************************************/
+
+/* Logging related header files are required to be included in the following order:
+ * 1. Include the header file "logging_levels.h".
+ * 2. Define LIBRARY_LOG_NAME and  LIBRARY_LOG_LEVEL.
+ * 3. Include the header file "logging_stack.h".
+ */
+
+/* Include header that defines log levels. */
+#include "logging_levels.h"
+
+/* Configure name and log level for the OTA library. */
+#ifndef LIBRARY_LOG_NAME
+    #define LIBRARY_LOG_NAME     "OTA"
+#endif
+#ifndef LIBRARY_LOG_LEVEL
+    #define LIBRARY_LOG_LEVEL    LOG_DEBUG
+#endif
+
+#include "logging_stack.h"
+
+/************ End of logging configuration ****************/
+
 /* Include config file used for testing the OTA PAL. This config file defines
  * the otapalconfigCODE_SIGNING_CERTIFICATE macro. */
 #include "aws_test_ota_config.h"
