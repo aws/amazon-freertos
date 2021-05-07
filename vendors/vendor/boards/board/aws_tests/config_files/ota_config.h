e1069b8d2d07ccd9d7bfc0a4ce116cf76c5a90ed
Joshua Yan <52796499+yanjos-dev@users.noreply.github.com>
2021-05-06 18:17:41 -0700

    Update example vendor test ota_config.h file.

--- vendors/vendor/boards/board/aws_tests/config_files/ota_config.h
+++ vendors/vendor/boards/board/aws_tests/config_files/ota_config.h
@@ -58,125 +58,148 @@
 #include "aws_test_ota_config.h"
 
 /**
- * @brief Log base 2 of the size of the file data block message (excluding the header).
+ * @brief Log base 2 of the size of the file data block message (excluding the
+ * header).
  *
- * 10 bits yields a data block size of 1KB.
+ * <b>Possible values:</b> Any unsigned 32 integer. <br>
  */
 #define otaconfigLOG2_FILE_BLOCK_SIZE           12UL
 
 /**
  * @brief Size of the file data block message (excluding the header).
- *
  */
 #define otaconfigFILE_BLOCK_SIZE                ( 1UL << otaconfigLOG2_FILE_BLOCK_SIZE )
 
 /**
- * @brief Milliseconds to wait for the self test phase to succeed before we force reset.
+ * @brief Milliseconds to wait for the self test phase to succeed before we
+ * force reset.
+ *
+ * <b>Possible values:</b> Any unsigned 32 integer. <br>
  */
 #define otaconfigSELF_TEST_RESPONSE_WAIT_MS     16000U
 
 /**
- * @brief Milliseconds to wait before requesting data blocks from the OTA service if nothing is happening.
+ * @brief Milliseconds to wait before requesting data blocks from the OTA
+ * service if nothing is happening.
  *
- * The wait timer is reset whenever a data block is received from the OTA service so we will only send
- * the request message after being idle for this amount of time.
+ * @note The wait timer is reset whenever a data block is received from the OTA
+ * service so we will only send the request message after being idle for this
+ * amount of time.
+ *
+ * <b>Possible values:</b> Any unsigned 32 integer. <br>
  */
 #define otaconfigFILE_REQUEST_WAIT_MS           10000U
 
 /**
  * @brief The maximum allowed length of the thing name used by the OTA agent.
  *
- * AWS IoT requires Thing names to be unique for each device that connects to the broker.
- * Likewise, the OTA agent requires the developer to construct and pass in the Thing name when
- * initializing the OTA agent. The agent uses this size to allocate static storage for the
- * Thing name used in all OTA base topics. Namely $aws/things/<thingName>
+ * @note AWS IoT requires Thing names to be unique for each device that
+ * connects to the broker. Likewise, the OTA agent requires the developer to
+ * construct and pass in the Thing name when initializing the OTA agent. The
+ * agent uses this size to allocate static storage for the Thing name used in
+ * all OTA base topics. Namely $aws/things/thingName
+ *
+ * <b>Possible values:</b> Any unsigned 32 integer. <br>
  */
 #define otaconfigMAX_THINGNAME_LEN              128U
 
 /**
- * @brief The maximum number of data blocks requested from OTA streaming service.
+ * @brief The maximum number of data blocks requested from OTA streaming
+ * service.
  *
- *  This configuration parameter is sent with data requests and represents the maximum number of
- *  data blocks the service will send in response. The maximum limit for this must be calculated
- *  from the maximum data response limit (128 KB from service) divided by the block size.
- *  For example if block size is set as 1 KB then the maximum number of data blocks that we can
- *  request is 128/1 = 128 blocks. Configure this parameter to this maximum limit or lower based on
- *  how many data blocks response is expected for each data requests.
- *  Please note that this must be set larger than zero.
+ * @note This configuration parameter is sent with data requests and represents
+ * the maximum number of data blocks the service will send in response. The
+ * maximum limit for this must be calculated from the maximum data response
+ * limit (128 KB from service) divided by the block size. For example if block
+ * size is set as 1 KB then the maximum number of data blocks that we can
+ * request is 128/1 = 128 blocks. Configure this parameter to this maximum
+ * limit or lower based on how many data blocks response is expected for each
+ * data requests.
  *
+ * <b>Possible values:</b> Any unsigned 32 integer value greater than 0. <br>
  */
 #define otaconfigMAX_NUM_BLOCKS_REQUEST         1U
 
 /**
- * @brief The maximum number of requests allowed to send without a response before we abort.
+ * @brief The maximum number of requests allowed to send without a response
+ * before we abort.
  *
- * This configuration parameter sets the maximum number of times the requests are made over
- * the selected communication channel before aborting and returning error.
+ * @note This configuration parameter sets the maximum number of times the
+ * requests are made over the selected communication channel before aborting
+ * and returning error.
  *
+ * <b>Possible values:</b> Any unsigned 32 integer. <br>
  */
 #define otaconfigMAX_NUM_REQUEST_MOMENTUM       32U
 
 /**
+ * @brief How frequently the device will report its OTA progress to the cloud.
+ *
+ * @note Device will update the job status with the number of blocks it has
+ * received every certain number of blocks it receives. For example, 64 means
+ * device will update job status every 64 blocks it receives.
+ *
+ * <b>Possible values:</b> Any unsigned 32 integer. <br>
+ */
+#define otaconfigOTA_UPDATE_STATUS_FREQUENCY    25U
+
+/**
  * @brief The number of data buffers reserved by the OTA agent.
  *
- * This configurations parameter sets the maximum number of static data buffers used by
- * the OTA agent for job and file data blocks received.
+ * @note This configurations parameter sets the maximum number of static data
+ * buffers used by the OTA agent for job and file data blocks received.
+ *
+ * <b>Possible values:</b> Any unsigned 32 integer. <br>
  */
 #define otaconfigMAX_NUM_OTA_DATA_BUFFERS       4U
 
 /**
- * @brief How frequently the device will report its OTA progress to the cloud.
+ * @brief Flag to enable booting into updates that have an identical or lower
+ * version than the current version.
  *
- * Device will update the job status with the number of blocks it has received every certain
- * number of blocks it receives. For example, 25 means device will update job status every 25 blocks
- * it receives.
- */
-#define otaconfigOTA_UPDATE_STATUS_FREQUENCY    25U
-
-
-
-/**
- * @brief Allow update to same or lower version.
+ * @note Set this configuration parameter to '1' to disable version checks.
+ * This allows updates to an identical or lower version. This is provided for
+ * testing purpose and it's recommended to always update to higher version and
+ * keep this configuration disabled.
  *
- * Set this to 1 to allow downgrade or same version update.This configurations parameter
- * disables version check and allows update to a same or lower version.This is provided for
- * testing purpose and it is recommended to always update to higher version and keep this
- * configuration disabled.
+ * <b>Possible values:</b> Any unsigned 32 integer. <br>
  */
-#define otaconfigAllowDowngrade           0U
+#define otaconfigAllowDowngrade                 0U
 
 /**
  * @brief The protocol selected for OTA control operations.
  *
- * This configurations parameter sets the default protocol for all the OTA control
- * operations like requesting OTA job, updating the job status etc.
+ * @note This configurations parameter sets the default protocol for all the
+ * OTA control operations like requesting OTA job, updating the job status etc.
  *
- * Note - Only MQTT is supported at this time for control operations.
+ * @note Only MQTT is supported at this time for control operations.
+ *
+ * <b>Possible values:</b> OTA_CONTROL_OVER_MQTT <br>
  */
-#define configENABLED_CONTROL_PROTOCOL    ( OTA_CONTROL_OVER_MQTT )
+#define configENABLED_CONTROL_PROTOCOL          ( OTA_CONTROL_OVER_MQTT )
 
 /**
  * @brief The protocol selected for OTA data operations.
  *
- * This configurations parameter sets the protocols selected for the data operations
- * like requesting file blocks from the service.
+ * @note This configurations parameter sets the protocols selected for the data
+ * operations like requesting file blocks from the service.
  *
- * Note - Both MQTT and HTTP is supported for data transfer. This configuration parameter
- * can be set to following -
- * Enable data over MQTT - ( OTA_DATA_OVER_MQTT )
- * Enable data over HTTP - ( OTA_DATA_OVER_HTTP)
- * Enable data over both MQTT & HTTP ( OTA_DATA_OVER_MQTT | OTA_DATA_OVER_HTTP )
+ * <b>Possible values:</b><br>
+ * Enable data over MQTT - ( OTA_DATA_OVER_MQTT ) <br>
+ * Enable data over HTTP - ( OTA_DATA_OVER_HTTP ) <br>
+ * Enable data over both MQTT & HTTP - ( OTA_DATA_OVER_MQTT | OTA_DATA_OVER_HTTP ) <br>
  */
-#define configENABLED_DATA_PROTOCOLS      ( OTA_DATA_OVER_MQTT | OTA_DATA_OVER_HTTP )
+#define configENABLED_DATA_PROTOCOLS            ( OTA_DATA_OVER_MQTT | OTA_DATA_OVER_HTTP )
 
 /**
  * @brief The preferred protocol selected for OTA data operations.
  *
- * Primary data protocol will be the protocol used for downloading file if more than
- * one protocol is selected while creating OTA job. Default primary data protocol is MQTT
- * and following update here to switch to HTTP as primary.
+ * @note Primary data protocol will be the protocol used for downloading file
+ * if more than one protocol is selected while creating OTA job.
  *
- * Note - use OTA_DATA_OVER_HTTP for HTTP as primary data protocol.
+ * <b>Possible values:</b><br>
+ * Data over MQTT - ( OTA_DATA_OVER_MQTT ) <br>
+ * Data over HTTP - ( OTA_DATA_OVER_HTTP ) <br>
  */
 
 #define configOTA_PRIMARY_DATA_PROTOCOL    ( OTA_DATA_OVER_MQTT )
