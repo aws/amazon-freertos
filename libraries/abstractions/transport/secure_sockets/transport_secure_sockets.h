7442c3cfa70776ea80d1348b09584df33a2f2fa2
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2021-05-19 13:31:30 -0700

    Make all logging visible

--- libraries/abstractions/transport/secure_sockets/transport_secure_sockets.h
+++ libraries/abstractions/transport/secure_sockets/transport_secure_sockets.h
@@ -58,7 +58,7 @@
     #define LIBRARY_LOG_NAME     "Transport_Secure_Sockets"
 #endif
 #ifndef LIBRARY_LOG_LEVEL
-    #define LIBRARY_LOG_LEVEL    LOG_ERROR
+    #define LIBRARY_LOG_LEVEL    LOG_DEBUG
 #endif
 
 /* Logging implementation header include. */
