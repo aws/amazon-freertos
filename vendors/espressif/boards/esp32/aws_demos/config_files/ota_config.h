a485c919afafb552fcbc037c6d5dd26c19b85c2d
Shubham Divekar <divekarshubham@gmail.com>
2021-06-09 06:02:53 +0000

    Updating OTA configurations

--- vendors/espressif/boards/esp32/aws_demos/config_files/ota_config.h
+++ vendors/espressif/boards/esp32/aws_demos/config_files/ota_config.h
@@ -61,56 +61,61 @@
  *
  * 10 bits yields a data block size of 1KB.
  */
-#define otaconfigLOG2_FILE_BLOCK_SIZE          12UL
+#define otaconfigLOG2_FILE_BLOCK_SIZE           12UL
 
- /**
-  * @brief Size of the file data block message (excluding the header).
-  */
+/**
+ * @brief Size of the file data block message (excluding the header).
+ */
 #define otaconfigFILE_BLOCK_SIZE                ( 1UL << otaconfigLOG2_FILE_BLOCK_SIZE )
 
 /**
  * @brief Milliseconds to wait for the self test phase to succeed before we force reset.
  */
-#define otaconfigSELF_TEST_RESPONSE_WAIT_MS    16000U
+#define otaconfigSELF_TEST_RESPONSE_WAIT_MS     16000U
 
 /**
- * @brief Milliseconds to wait before requesting data blocks from the OTA service if nothing is happening.
+ * @brief Milliseconds to wait before requesting data blocks from the OTA service
+ * if nothing is happening.
  *
- * The wait timer is reset whenever a data block is received from the OTA service so we will only send
- * the request message after being idle for this amount of time.
+ * The wait timer is reset whenever a data block is received from the OTA
+ * service so we will only send the request message after being idle for this
+ * amount of time.
  */
-#define otaconfigFILE_REQUEST_WAIT_MS          10000U
+#define otaconfigFILE_REQUEST_WAIT_MS           10000U
 
 /**
  * @brief The maximum allowed length of the thing name used by the OTA agent.
  *
- * AWS IoT requires Thing names to be unique for each device that connects to the broker.
- * Likewise, the OTA agent requires the developer to construct and pass in the Thing name when
- * initializing the OTA agent. The agent uses this size to allocate static storage for the
- * Thing name used in all OTA base topics. Namely $aws/things/<thingName>
+ * AWS IoT requires Thing names to be unique for each device that connects to the
+ * broker. Likewise, the OTA agent requires the developer to construct and pass
+ * in the Thing name when initializing the OTA agent. The agent uses this size to
+ * allocate static storage for the Thing name used in all OTA base topics.
+ * Namely $aws/things/<thingName>
  */
-#define otaconfigMAX_THINGNAME_LEN             128U
+#define otaconfigMAX_THINGNAME_LEN              128U
 
 /**
  * @brief The maximum number of data blocks requested from OTA streaming service.
  *
- *  This configuration parameter is sent with data requests and represents the maximum number of
- *  data blocks the service will send in response. The maximum limit for this must be calculated
- *  from the maximum data response limit (128 KB from service) divided by the block size.
- *  For example if block size is set as 1 KB then the maximum number of data blocks that we can
- *  request is 128/1 = 128 blocks. Configure this parameter to this maximum limit or lower based on
- *  how many data blocks response is expected for each data requests.
- *  Please note that this must be set larger than zero.
+ * This configuration parameter is sent with data requests and represents the
+ * maximum number of data blocks the service will send in response. The maximum
+ * limit for this must be calculated from the maximum data response limit (128 KB
+ * from service) divided by the block size. For example if block size is set as 1
+ * KB then the maximum number of data blocks that we can request is
+ * 128/1 = 128 blocks. Configure this parameter to this maximum limit or lower
+ * based on how many data blocks response is expected for each data requests.
+ * @note This must be set larger than zero.
  */
-#define otaconfigMAX_NUM_BLOCKS_REQUEST        1U
+#define otaconfigMAX_NUM_BLOCKS_REQUEST         1U
 
 /**
- * @brief The maximum number of requests allowed to send without a response before we abort.
+ * @brief The maximum number of requests allowed to send without a response before
+ * we abort.
  *
- * This configuration parameter sets the maximum number of times the requests are made over
- * the selected communication channel before aborting and returning error.
+ * This configuration parameter sets the maximum number of times the requests are
+ * made over the selected communication channel before aborting and returning error.
  */
-#define otaconfigMAX_NUM_REQUEST_MOMENTUM      32U
+#define otaconfigMAX_NUM_REQUEST_MOMENTUM       32U
 
 /**
  * @brief The number of data buffers reserved by the OTA agent.
@@ -118,18 +123,16 @@
  * This configurations parameter sets the maximum number of static data buffers used by
  * the OTA agent for job and file data blocks received.
  */
-#define otaconfigMAX_NUM_OTA_DATA_BUFFERS      2U
+#define otaconfigMAX_NUM_OTA_DATA_BUFFERS       otaconfigMAX_NUM_BLOCKS_REQUEST + 1
 
- /**
-  * @brief How frequently the device will report its OTA progress to the cloud.
-  *
-  * Device will update the job status with the number of blocks it has received every certain
-  * number of blocks it receives. For example, 25 means device will update job status every 25 blocks
-  * it receives.
-  */
+/**
+ * @brief How frequently the device will report its OTA progress to the cloud.
+ *
+ * Device will update the job status with the number of blocks it has received every certain
+ * number of blocks it receives. For example, 25 means device will update job status every 25 blocks
+ * it receives.
+ */
 #define otaconfigOTA_UPDATE_STATUS_FREQUENCY    25U
-
-
 
 /**
  * @brief Allow update to same or lower version.
@@ -139,7 +142,7 @@
  * testing purpose and it is recommended to always update to higher version and keep this
  * configuration disabled.
  */
-#define otaconfigAllowDowngrade                0U
+#define otaconfigAllowDowngrade                 0U
 
 /**
  * @brief The protocol selected for OTA control operations.
@@ -147,9 +150,9 @@
  * This configurations parameter sets the default protocol for all the OTA control
  * operations like requesting OTA job, updating the job status etc.
  *
- * Note - Only MQTT is supported at this time for control operations.
+ * @note Only MQTT is supported at this time for control operations.
  */
-#define configENABLED_CONTROL_PROTOCOL         ( OTA_CONTROL_OVER_MQTT )
+#define configENABLED_CONTROL_PROTOCOL          ( OTA_CONTROL_OVER_MQTT )
 
 /**
  * @brief The protocol selected for OTA data operations.
@@ -157,13 +160,13 @@
  * This configurations parameter sets the protocols selected for the data operations
  * like requesting file blocks from the service.
  *
- * Note - Both MQTT and HTTP is supported for data transfer. This configuration parameter
+ * @note Both MQTT and HTTP is supported for data transfer. This configuration parameter
  * can be set to following -
  * Enable data over MQTT - ( OTA_DATA_OVER_MQTT )
  * Enable data over HTTP - ( OTA_DATA_OVER_HTTP)
  * Enable data over both MQTT & HTTP ( OTA_DATA_OVER_MQTT | OTA_DATA_OVER_HTTP )
  */
-#define configENABLED_DATA_PROTOCOLS           ( OTA_DATA_OVER_MQTT | OTA_DATA_OVER_HTTP  )
+#define configENABLED_DATA_PROTOCOLS            ( OTA_DATA_OVER_MQTT | OTA_DATA_OVER_HTTP )
 
 /**
  * @brief The preferred protocol selected for OTA data operations.
@@ -172,7 +175,7 @@
  * one protocol is selected while creating OTA job. Default primary data protocol is MQTT
  * and following update here to switch to HTTP as primary.
  *
- * Note - use OTA_DATA_OVER_HTTP for HTTP as primary data protocol.
+ * @note Use OTA_DATA_OVER_HTTP for HTTP as primary data protocol.
  */
 
 #define configOTA_PRIMARY_DATA_PROTOCOL    ( OTA_DATA_OVER_MQTT )
--- vendors/espressif/boards/esp32/aws_tests/config_files/aws_test_ota_config.h
+++ vendors/espressif/boards/esp32/aws_tests/config_files/aws_test_ota_config.h
@@ -91,6 +91,6 @@
  *        This is used in the OTA demo to set the appFirmwareVersion variable
  *        that is declared in the ota_appversion32.h file in the OTA library.
  */
-#define APP_VERSION_BUILD    0
+#define APP_VERSION_BUILD    2
 
 #endif /* ifndef _AWS_TEST_OTA_CONFIG_H_ */
