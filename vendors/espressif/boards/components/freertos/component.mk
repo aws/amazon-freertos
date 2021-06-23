698855d95c983ffb071ebce500ae2eae68dc0e2f
Muneeb Ahmed <ahmemune@amazon.com>
2021-06-23 14:25:52 -0700

    Update espressif files

--- vendors/espressif/boards/components/freertos/component.mk
+++ vendors/espressif/boards/components/freertos/component.mk
@@ -13,14 +13,11 @@ COMPONENT_ADD_INCLUDEDIRS := include \
                             ${AMAZON_FREERTOS_KERNEL_DIR}/include \
                             $(AMAZON_FREERTOS_LIB_DIR)/c_sdk/standard/common/include \
                             $(AMAZON_FREERTOS_LIB_DIR)/freertos_plus/standard/crypto/include \
-                            $(AMAZON_FREERTOS_LIB_DIR)/c_sdk/aws/defender/include \
                             $(AMAZON_FREERTOS_LIB_DIR)/freertos_plus/aws/greengrass/include \
-                            $(AMAZON_FREERTOS_LIB_DIR)/c_sdk/standard/mqtt/include \
                             $(AMAZON_FREERTOS_LIB_DIR)/freertos_plus/aws/ota/include \
                             $(AMAZON_FREERTOS_LIB_DIR)/freertos_plus/standard/pkcs11/include \
                             $(AMAZON_FREERTOS_LIB_DIR)/abstractions/pkcs11/include \
                             $(AMAZON_FREERTOS_LIB_DIR)/abstractions/secure_sockets/include \
-                            $(AMAZON_FREERTOS_LIB_DIR)/c_sdk/aws/shadow/include \
                             $(AMAZON_FREERTOS_LIB_DIR)/freertos_plus/standard/utils/include \
                             $(AMAZON_FREERTOS_LIB_DIR)/freertos_plus/standard/tls/include \
                             $(AMAZON_FREERTOS_LIB_DIR)/abstractions/wifi/include \
