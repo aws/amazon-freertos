8df4952cd239ff91b553fd3faf7822c816af2c00
Muneeb Ahmed <ahmemune@amazon.com>
2021-06-24 17:21:55 -0700

    Rename connection sharing demo to mqtt agent demo

--- demos/include/iot_demo_runner.h
+++ demos/include/iot_demo_runner.h
@@ -48,7 +48,7 @@
         #undef democonfigDEMO_PRIORITY
         #define democonfigDEMO_PRIORITY     democonfigMQTT_ECHO_TASK_PRIORITY
     #endif
-#elif defined( CONFIG_CORE_MQTT_CONNECTION_SHARING_DEMO_ENABLED )
+#elif defined( CONFIG_CORE_MQTT_AGENT_DEMO_ENABLED )
     #define DEMO_entryFUNCTION              RunCoreMqttAgentDemo
     #if defined( democonfigMQTT_ECHO_TASK_STACK_SIZE )
         #undef democonfigDEMO_STACKSIZE
--- vendors/espressif/boards/esp32/aws_tests/config_files/aws_demo_config.h
+++ vendors/espressif/boards/esp32/aws_tests/config_files/aws_demo_config.h
@@ -56,7 +56,7 @@
 #define democonfigMQTT_ECHO_TASK_PRIORITY                            ( tskIDLE_PRIORITY )
 
 /* MQTT Connection sharing demo task priority. */
-#define democonfigCORE_MQTT_CONNECTION_SHARING_DEMO_TASK_PRIORITY    ( tskIDLE_PRIORITY + 1 )
+#define democonfigCORE_MQTT_AGENT_DEMO_TASK_PRIORITY                 ( tskIDLE_PRIORITY + 1 )
 
 /* Timeout used when performing MQTT operations that do not need extra time
  * to perform a TLS negotiation. */
