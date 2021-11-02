708677c770860a7b4e0700002ff30fdbb481f84d
dachalco <david@chalco.io>
2021-11-02 13:31:58 -0700

    evaluate full shortened BLE name for esp32

--- vendors/espressif/boards/esp32/aws_demos/config_files/iot_ble_config.h
+++ vendors/espressif/boards/esp32/aws_demos/config_files/iot_ble_config.h
@@ -34,6 +34,7 @@
 
 /* Device name for this peripheral device. */
 #define IOT_BLE_DEVICE_COMPLETE_LOCAL_NAME                      "ESP32"
+#define IOT_BLE_DEVICE_SHORT_LOCAL_NAME_SIZE                    ( 5 )
 
 /* Include BLE default config at bottom to set the default values for the configurations which are not overridden */
 #include "iot_ble_config_defaults.h"
