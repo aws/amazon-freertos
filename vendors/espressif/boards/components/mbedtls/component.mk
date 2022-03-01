e22bca13f5505980ce9119c51b52ea1e9c835f3e
ActoryOu <ousc@amazon.com>
2022-03-01 10:51:46 +0800

    Espressif: update project makefile for mbedtls update

--- vendors/espressif/boards/components/mbedtls/component.mk
+++ vendors/espressif/boards/components/mbedtls/component.mk
@@ -4,7 +4,7 @@ MBEDTLS_DIR := ../../../esp-idf/components/mbedtls
 COMPONENT_SRCDIRS := $(AMAZON_FREERTOS_LIB_DIR)/3rdparty/mbedtls/library $(MBEDTLS_DIR)/port \
                       $(MBEDTLS_DIR)/port/esp32 $(AMAZON_FREERTOS_LIB_DIR)/3rdparty/mbedtls_utils
 
-COMPONENT_ADD_INCLUDEDIRS := $(MBEDTLS_DIR)/port/include $(AMAZON_FREERTOS_LIB_DIR)/3rdparty/mbedtls/include
+COMPONENT_ADD_INCLUDEDIRS := $(MBEDTLS_DIR)/port/include $(AMAZON_FREERTOS_LIB_DIR)/3rdparty/mbedtls/include $(AMAZON_FREERTOS_LIB_DIR)/3rdparty/mbedtls/library
 
 libraries/3rdparty/mbedtls/library/hmac_drbg.o: CFLAGS += -Wno-maybe-uninitialized
 libraries/3rdparty/mbedtls/library/x509_crt.o: CFLAGS += -Wno-maybe-uninitialized
