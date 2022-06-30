4392b8e7c774f6c639987f05f7dee408d4fbbde2
Paul Bartell <pbartell@amazon.com>
2022-06-30 11:37:08 -0700

    mw300_rd: Adjust FreeRTOSConfig.h for new kernel
    Define INCLUDE_xTaskGetCurrentTaskHandle to 1.

--- vendors/marvell/boards/mw300_rd/aws_demos/config_files/FreeRTOSConfig.h
+++ vendors/marvell/boards/mw300_rd/aws_demos/config_files/FreeRTOSConfig.h
@@ -37,17 +37,17 @@
  * application requirements.
  *
  * THESE PARAMETERS ARE DESCRIBED WITHIN THE 'CONFIGURATION' SECTION OF THE
- * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE. 
+ * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE.
  *
  * See http://www.freertos.org/a00110.html.
  *----------------------------------------------------------*/
 
 #define configUSE_PREEMPTION			1
-#define configUSE_IDLE_HOOK			0
-#define configUSE_TICK_HOOK			0
+#define configUSE_IDLE_HOOK			    0
+#define configUSE_TICK_HOOK			    0
 /* #define configCPU_CLOCK_HZ			( ( unsigned long ) 200000000 ) */
 #define configCPU_CLOCK_HZ              	( board_cpu_freq( ) )
-#define configTICK_RATE_HZ			( ( portTickType ) 1000 )
+#define configTICK_RATE_HZ			    ( ( portTickType ) 1000 )
 #define configMAX_PRIORITIES			( 7 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 256 )
 /* #define configTOTAL_HEAP_SIZE		( ( size_t ) ( 72 * 1024 ) ) */
@@ -63,7 +63,7 @@
 #define configCHECK_FOR_STACK_OVERFLOW  	0
 #endif
 #define configUSE_MALLOC_FAILED_HOOK		0
-#define configUSE_RECURSIVE_MUTEXES		1
+#define configUSE_RECURSIVE_MUTEXES		    1
 #define configUSE_COUNTING_SEMAPHORES		1
 
 /* Currently the TCP/IP stack is using dynamic allocation, and the MQTT task is
@@ -95,9 +95,9 @@ void portWMSDK_CONFIGURE_TIMER_FOR_RUN_TIME_STATS(void);
 unsigned long portWMSDK_GET_RUN_TIME_COUNTER_VALUE(void);
 
 #define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS		\
-	portWMSDK_CONFIGURE_TIMER_FOR_RUN_TIME_STATS
+    portWMSDK_CONFIGURE_TIMER_FOR_RUN_TIME_STATS
 #define portGET_RUN_TIME_COUNTER_VALUE		\
-	portWMSDK_GET_RUN_TIME_COUNTER_VALUE
+    portWMSDK_GET_RUN_TIME_COUNTER_VALUE
 #endif /* configGENERATE_RUN_TIME_STATS */
 
 #define configUSE_CO_ROUTINES 			0
@@ -114,17 +114,18 @@ unsigned long portWMSDK_GET_RUN_TIME_COUNTER_VALUE(void);
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
-#define INCLUDE_vTaskPrioritySet		1
-#define INCLUDE_uxTaskPriorityGet		1
-#define INCLUDE_vTaskDelete			1
+#define INCLUDE_vTaskPrioritySet		    1
+#define INCLUDE_uxTaskPriorityGet		    1
+#define INCLUDE_vTaskDelete			        1
 #define INCLUDE_vTaskCleanUpResources		0
-#define INCLUDE_vTaskSuspend			1
-#define INCLUDE_vTaskDelayUntil			1
-#define INCLUDE_vTaskDelay			1
+#define INCLUDE_vTaskSuspend			    1
+#define INCLUDE_vTaskDelayUntil			    1
+#define INCLUDE_vTaskDelay			        1
 #define INCLUDE_uxTaskGetStackHighWaterMark	0
-#define INCLUDE_pcTaskGetTaskName		1
+#define INCLUDE_pcTaskGetTaskName		    1
+#define INCLUDE_xTaskGetCurrentTaskHandle	1
 
-#define configKERNEL_INTERRUPT_PRIORITY 	0xf0
+#define configKERNEL_INTERRUPT_PRIORITY 	    0xf0
 #define configMAX_SYSCALL_INTERRUPT_PRIORITY 	0xa0 /* equivalent to 0xa0, or priority 5. */
 
 #define xPortPendSVHandler	PendSV_IRQHandler
--- vendors/marvell/boards/mw300_rd/aws_tests/config_files/FreeRTOSConfig.h
+++ vendors/marvell/boards/mw300_rd/aws_tests/config_files/FreeRTOSConfig.h
@@ -40,17 +40,17 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * application requirements.
  *
  * THESE PARAMETERS ARE DESCRIBED WITHIN THE 'CONFIGURATION' SECTION OF THE
- * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE. 
+ * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE.
  *
  * See http://www.freertos.org/a00110.html.
  *----------------------------------------------------------*/
 
 #define configUSE_PREEMPTION		1
-#define configUSE_IDLE_HOOK		0
-#define configUSE_TICK_HOOK		0
+#define configUSE_IDLE_HOOK		    0
+#define configUSE_TICK_HOOK		    0
 /* #define configCPU_CLOCK_HZ		( ( unsigned long ) 200000000 ) */
 #define configCPU_CLOCK_HZ           ( board_cpu_freq( ) )
-#define configTICK_RATE_HZ		( ( portTickType ) 1000 )
+#define configTICK_RATE_HZ		    ( ( portTickType ) 1000 )
 #define configMAX_PRIORITIES		(  7 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 256 )
 /* #define configTOTAL_HEAP_SIZE		( ( size_t ) ( 72 * 1024 ) ) */
@@ -59,14 +59,14 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define configUSE_STATS_FORMATTING_FUNCTIONS	1
 #define configUSE_16_BIT_TICKS		0
 #define configIDLE_SHOULD_YIELD		1
-#define configUSE_MUTEXES		1
+#define configUSE_MUTEXES		    1
 #if ( CONFIG_ENABLE_STACK_OVERFLOW_CHECK == 1 )
 #define configCHECK_FOR_STACK_OVERFLOW	2
 #else
 #define configCHECK_FOR_STACK_OVERFLOW  0
 #endif
 #define configUSE_MALLOC_FAILED_HOOK	0
-#define configUSE_RECURSIVE_MUTEXES	1
+#define configUSE_RECURSIVE_MUTEXES	    1
 #define configUSE_COUNTING_SEMAPHORES	1
 
 /* Currently the TCP/IP stack is using dynamic allocation, and the MQTT task is
@@ -98,34 +98,35 @@ void portWMSDK_CONFIGURE_TIMER_FOR_RUN_TIME_STATS(void);
 unsigned long portWMSDK_GET_RUN_TIME_COUNTER_VALUE(void);
 
 #define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS		\
-	portWMSDK_CONFIGURE_TIMER_FOR_RUN_TIME_STATS
+    portWMSDK_CONFIGURE_TIMER_FOR_RUN_TIME_STATS
 #define portGET_RUN_TIME_COUNTER_VALUE		\
-	portWMSDK_GET_RUN_TIME_COUNTER_VALUE
+    portWMSDK_GET_RUN_TIME_COUNTER_VALUE
 #endif /* configGENERATE_RUN_TIME_STATS */
 
-#define configUSE_CO_ROUTINES 		0
-#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
+#define configUSE_CO_ROUTINES 		        0
+#define configMAX_CO_ROUTINE_PRIORITIES     ( 2 )
 
 /* Use application defined heap region */
 #define configAPPLICATION_ALLOCATED_HEAP	1
 
 /*  Software timer definitions. */
-#define configUSE_TIMERS		1
-#define configTIMER_TASK_PRIORITY	( configMAX_PRIORITIES - 1  )
-#define configTIMER_QUEUE_LENGTH	5
+#define configUSE_TIMERS		        1
+#define configTIMER_TASK_PRIORITY	    ( configMAX_PRIORITIES - 1  )
+#define configTIMER_QUEUE_LENGTH	    5
 #define configTIMER_TASK_STACK_DEPTH	( configMINIMAL_STACK_SIZE )
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
-#define INCLUDE_vTaskPrioritySet	1
-#define INCLUDE_uxTaskPriorityGet	1
-#define INCLUDE_vTaskDelete		1
-#define INCLUDE_vTaskCleanUpResources	0
-#define INCLUDE_vTaskSuspend		1
-#define INCLUDE_vTaskDelayUntil		1
-#define INCLUDE_vTaskDelay		1
+#define INCLUDE_vTaskPrioritySet	        1
+#define INCLUDE_uxTaskPriorityGet	        1
+#define INCLUDE_vTaskDelete		            1
+#define INCLUDE_vTaskCleanUpResources	    0
+#define INCLUDE_vTaskSuspend		        1
+#define INCLUDE_vTaskDelayUntil		        1
+#define INCLUDE_vTaskDelay		            1
 #define INCLUDE_uxTaskGetStackHighWaterMark	1
-#define INCLUDE_pcTaskGetTaskName	1
+#define INCLUDE_pcTaskGetTaskName	        1
+#define INCLUDE_xTaskGetCurrentTaskHandle	1
 
 #define configKERNEL_INTERRUPT_PRIORITY 	0xf0
 #define configMAX_SYSCALL_INTERRUPT_PRIORITY 	0xa0 /* equivalent to 0xa0, or priority 5. */
