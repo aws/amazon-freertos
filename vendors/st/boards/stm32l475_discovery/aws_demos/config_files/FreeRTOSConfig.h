7f6a71a17b26290b6b337d4b0ad05c5f909a7761
Paul Bartell <pbartell@amazon.com>
2022-06-30 10:11:38 -0700

    stm32l475: Adjust FreeRTOSConfig.h for new kernel.
    Define INCLUDE_xTaskGetCurrentTaskHandle to 1.

--- vendors/st/boards/stm32l475_discovery/aws_demos/config_files/FreeRTOSConfig.h
+++ vendors/st/boards/stm32l475_discovery/aws_demos/config_files/FreeRTOSConfig.h
@@ -94,6 +94,7 @@
 #define INCLUDE_vTaskDelay                           1
 #define INCLUDE_uxTaskGetStackHighWaterMark          1
 #define INCLUDE_xTaskGetSchedulerState               1
+#define INCLUDE_xTaskGetCurrentTaskHandle            1
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
